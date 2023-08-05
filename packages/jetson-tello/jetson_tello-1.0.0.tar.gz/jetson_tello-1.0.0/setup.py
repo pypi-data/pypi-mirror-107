ddaa5a2a3175c39c6b249d7f8bb888b94c94ef96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package jetson-tello/1.0.0/jetson_tello-1.0.0.tar.gz

---
+++ packages/jetson-tello/jetson_tello-1.0.0.tar.gz/jetson_tello-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='jetson_tello',
+      version='1.0.0',
+      description='Tello drone integration with Jetson',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/robagar/jetson-tello',
+      project_urls={
+        'Documentation': 'https://jetson-tello.readthedocs.io/en/latest/'
+      },
+      author='Rob Agar',
+      author_email='tello_asyncio@fastmail.net',
+      license='LGPL',
+      packages=['jetson_tello'],
+      zip_safe=False,
+      python_requires=">=3.6",
+      install_requires=[
+        'tello-asyncio >= 1.6.0'
+      ])
