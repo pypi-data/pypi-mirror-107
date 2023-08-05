262dd8b9af55626df87120e7bde882d2226bda7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:08 +0000

    Add package valuta/0.1.5/valuta-0.1.5-py2.py3-none-any.whl

---
+++ packages/valuta/valuta-0.1.5-py2.py3-none-any.whl/valuta/exceptions.py
@@ -0,0 +1,11 @@
+__author__ = "Artur Barseghyan"
+__copyright__ = "2021 Artur Barseghyan"
+__license__ = "GPL-2.0-only OR LGPL-2.1-or-later"
+__all__ = ("ImproperlyConfigured",)
+
+
+class ImproperlyConfigured(Exception):
+    """ImproperlyConfigured.
+
+    Supposed to be thrown when code is improperly configured.
+    """
