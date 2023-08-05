1a22ef43c51ebfa3aab6a5c308823160985952ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:27 +0000

    Add package django-graze-tools/0.0.1/django-graze-tools-0.0.1.tar.gz

---
+++ packages/django-graze-tools/django-graze-tools-0.0.1.tar.gz/django-graze-tools-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-graze-tools',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.graze.ai',
+    author='me',
+    author_email='webmaster@graze.ai',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
