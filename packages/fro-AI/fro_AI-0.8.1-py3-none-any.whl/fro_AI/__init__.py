f269b2aa9f8e667008f0b6085836bac92ec5af47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:58 +0000

    Add package fro-AI/0.8.1/fro_AI-0.8.1-py3-none-any.whl

---
+++ packages/fro-AI/fro_AI-0.8.1-py3-none-any.whl/fro_AI/__init__.py
@@ -0,0 +1,6 @@
+from .face import FaceRecognizer
+from .face_detect import FaceDetector
+from .classifier import Classifier
+from .color import ColorTracker, ColorRecognizer
+
+__version__ = '0.8.1'
