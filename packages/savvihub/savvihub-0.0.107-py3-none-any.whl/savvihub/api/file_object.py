08dd3d1bd781e462e3c19593a80a32de43e68492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:43 +0000

    Add package savvihub/0.0.107/savvihub-0.0.107-py3-none-any.whl

---
+++ packages/savvihub/savvihub-0.0.107-py3-none-any.whl/savvihub/api/file_object.py
@@ -0,0 +1,83 @@
+import os
+
+import requests
+
+from savvihub.common.utils import read_in_chunks
+
+
+class UploadableFileObject:
+    def __init__(self, url, base_path, path):
+        self.url = url
+        self.base_path = base_path
+        self.full_path = os.path.join(base_path, path)
+        self.path = path
+
+    def upload_chunks(self, *, callback=None):
+        return read_in_chunks(self.full_path, callback=callback)
+
+    def upload_hooks(self, *, log=None):
+        def fn(resp, **kwargs):
+            if resp.status_code != 200:
+                print(f'Upload for {resp.request.url} failed. Detail: {resp.data}')
+        return {
+            'response': fn,
+        }
+
+    def upload(self, session=requests.Session(), progressable=None):
+        file_size = os.path.getsize(self.full_path)
+        progress_callback = None
+        if progressable:
+            progress = progressable(length=file_size, label=self.path)
+            progress_callback = lambda data: progress.update(len(data))
+
+        # send empty data when file is empty
+        if os.stat(self.full_path).st_size == 0:
+            future = session.put(
+                self.url,
+                data='',
+                headers={'content-type': 'application/octet-stream'},
+                hooks=self.upload_hooks(),
+            )
+            return future
+
+        # TODO: streamed upload to show a progress bar
+        with open(self.full_path, 'rb') as f:
+            future = session.put(
+                self.url,
+                data=f,
+                headers={'content-type': 'application/octet-stream'},
+                hooks=self.upload_hooks(),
+            )
+            return future
+
+
+class DownloadableFileObject:
+    def __init__(self, url, base_path, path, size=None):
+        self.url = url
+        self.full_path = os.path.join(base_path, path)
+        self.size = size
+
+    def download_hooks(self, *, callback=None):
+        def fn(resp, **kwargs):
+            os.makedirs(os.path.dirname(self.full_path), exist_ok=True)
+            with open(self.full_path, 'wb') as f:
+                for chunk in resp.iter_content(chunk_size=8192):
+                    f.write(chunk)
+                    if callback:
+                        callback(chunk)
+        return {
+            'response': fn,
+        }
+
+    def download(self, session=requests.Session(), progressable=None):
+        progress_callback = None
+        if progressable and self.size:
+            progress = progressable(length=self.size, label=self.full_path)
+            progress_callback = lambda data: progress.update(len(data))
+
+        future = session.get(
+            self.url,
+            stream=True,
+            hooks=self.download_hooks(callback=progress_callback)
+        )
+        return future
