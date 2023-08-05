383bc1343e9a5877e7f8f65e9f0b7239c7eb0975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:48 +0000

    Add package notejob/0.2.18/notejob-0.2.18-py3-none-any.whl

---
+++ packages/notejob/notejob-0.2.18-py3-none-any.whl/notejob/center/manage.py
@@ -0,0 +1,43 @@
+
+import os
+
+from flask_migrate import Migrate
+from flask_script import Manager, Shell
+from notejob.center.app.core import create_app, db
+from notejob.center.datas.model import CronInfos, JobLog, JobLogItems
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+db.create_all()
+
+def make_shell_context():
+    return dict(
+        app=app,
+        JobLog=JobLog,
+        CronInfos=CronInfos,
+        JobLogItems=JobLogItems
+    )
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+#manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    # print("Hello world")
+    # d = configs('job_log_counts')
+    # print(d)
+    # print(uuid.uuid1())
+    '''
+    sudo docker exec -it $DOCKER_ID /bin/bash -c 'cd /packages/detectron && python tools/train.py'
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    # gunicorn -b 0.0.0.0:8445 -w 1 -k gevent manage:app
+    manager.run()
