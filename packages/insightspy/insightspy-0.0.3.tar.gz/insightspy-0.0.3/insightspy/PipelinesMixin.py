4191385c2963bf0c74493329da0e3ae0d0833b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:57 +0000

    Add package insightspy/0.0.3/insightspy-0.0.3.tar.gz

---
+++ packages/insightspy/insightspy-0.0.3.tar.gz/insightspy-0.0.3/insightspy/PipelinesMixin.py
@@ -0,0 +1,17 @@
+from insightspy.SessionCore import RequestCore
+import pandas as pd
+
+
+class PipelinesMixin(RequestCore):
+    def pipeline_revisions(self):
+        """List available pipelines
+
+        Lists pipelines accessible within the current portal session. Pipeline access is
+        limited by user id and the current session project if one is set.
+
+        Returns:
+            DataFrame: table of pipeline metadata
+        """
+        return pd.DataFrame.from_dict(
+            self._get("pipelinesDb/project_pipelines_revisions")["response"]["data"]
+        )
