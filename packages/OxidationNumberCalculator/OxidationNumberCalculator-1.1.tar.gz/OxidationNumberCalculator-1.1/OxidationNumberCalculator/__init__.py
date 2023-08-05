3521f0b06df5176165ebc87fee25e116394f3c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:24 +0000

    Add package OxidationNumberCalculator/1.1/OxidationNumberCalculator-1.1.tar.gz

---
+++ packages/OxidationNumberCalculator/OxidationNumberCalculator-1.1.tar.gz/OxidationNumberCalculator-1.1/OxidationNumberCalculator/__init__.py
@@ -0,0 +1,1 @@
+from .OxidationNumber import processI
---
+++ packages/OxidationNumberCalculator/OxidationNumberCalculator-1.1.tar.gz/OxidationNumberCalculator-1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import pathlib
+here = pathlib.Path('README.md').parent.resolve()
+
+setup(
+    name = 'OxidationNumberCalculator',
+    packages = ['OxidationNumberCalculator'],
+    description='An alpha version of an oxidation number calculator that can be used on compounds/Elements or Chemical Equations.',
+    long_description=(here / 'README.md').read_text(encoding='utf-8'),
+    long_description_content_type='text/markdown',
+    version = '1.1',
+    license='MIT',
+    author = 'Harold J. Iwen',
+    author_email = 'inventorsniche349@gmail.com',
+    url = 'https://www.inventorsniche.com',
+    download_url = 'https://github.com/Hiwen-STEM/OxidationNumberCalculator',
+    keywords = ['Oxidation','Number','Oxidation_Number','oxidation_number','stoichiometry','chemical','equation','chemical_equation'],
+    install_requires=[
+        'mendeleev'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',     
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
