a5970c83906428f3f959d2e83cbc07134e8789ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:23 +0000

    Add package mongohelper/0.0.1/mongohelper-0.0.1.tar.gz

---
+++ packages/mongohelper/mongohelper-0.0.1.tar.gz/mongohelper-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python MongoDB helper'
+LONG_DESCRIPTION = 'Python MongoDB wrapper that has functionalities to find by id and return lists and dictionaries.'
+
+# Setting up
+setup(
+    name="mongohelper",
+    version=VERSION,
+    author="Alan Ngo",
+    author_email="<alanngo673@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pymongo', 'dnspython'],
+    keywords=['pymongo', 'dnspython'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
