acf7e35d82b9d49ee5c9979208bec13a0bc64b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:05 +0000

    Add package anoapycore/0.1.12/anoapycore-0.1.12.tar.gz

---
+++ packages/anoapycore/anoapycore-0.1.12.tar.gz/anoapycore-0.1.12/src/anoapycore/data/column/__init__.py
@@ -0,0 +1,14 @@
+def count (a_dataframe) :
+    return len(a_dataframe.columns)    
+    
+def delete (a_dataframe,a_column) :
+    loc_dataframe = a_dataframe.drop([a_column], axis = 1)    
+    return loc_dataframe
+
+def delete (a_dataframe,a_column) :
+    loc_dataframe = a_dataframe.drop(a_column, axis = 1)    
+    return loc_dataframe
+    
+def list_ (a_dataframe) :
+    return list(a_dataframe)
+    
