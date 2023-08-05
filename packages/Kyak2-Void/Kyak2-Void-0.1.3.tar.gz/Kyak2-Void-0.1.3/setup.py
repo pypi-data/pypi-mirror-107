4b0f9d54896420fd57ddc1cbd87445638164a5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:12 +0000

    Add package Kyak2-Void/0.1.3/Kyak2-Void-0.1.3.tar.gz

---
+++ packages/Kyak2-Void/Kyak2-Void-0.1.3.tar.gz/Kyak2-Void-0.1.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Kyak2-Void",
+    version="0.1.3",
+    author="UnbalancedSkunk",
+    author_email="turkishanhur@gmail.com",
+    description="Void Linux Openbox(WM) Logout screen",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UnbalancedSkunk/Kyak2",
+    project_urls={
+        "Bug Tracker": "https://github.com/UnbalancedSkunk/Kyak2",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
