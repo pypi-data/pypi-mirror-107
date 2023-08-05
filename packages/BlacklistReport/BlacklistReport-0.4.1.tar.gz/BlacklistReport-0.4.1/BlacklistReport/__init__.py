7533c1dc50a4d2747d78454ba023cbab18d39ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:42 +0000

    Add package BlacklistReport/0.4.1/BlacklistReport-0.4.1.tar.gz

---
+++ packages/BlacklistReport/BlacklistReport-0.4.1.tar.gz/BlacklistReport-0.4.1/BlacklistReport/__init__.py
@@ -0,0 +1,9 @@
+from BlacklistReport.BlacklistReport import generate_blacklist_report
+from BlacklistReport.BlacklistReport import BlacklistReport
+from BlacklistReport.BlacklistEntry import BlacklistEntry
+
+import logging
+
+
+logging.basicConfig(level=logging.WARNING,
+                    format='%(name)s - %(levelname)s - %(message)s')
