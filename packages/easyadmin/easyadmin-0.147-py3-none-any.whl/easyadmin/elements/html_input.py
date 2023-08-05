26a3544dd6dcf7da54f907232ef1d6abe0615471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:59 +0000

    Add package easyadmin/0.147/easyadmin-0.147-py3-none-any.whl

---
+++ packages/easyadmin/easyadmin-0.147-py3-none-any.whl/easyadmin/elements/html_input.py
@@ -0,0 +1,53 @@
+from . import card
+
+def get_text_input(
+    name: str = 'input text name',
+    input_type: str = 'text', # 'text', 'password', 'email',
+    value: str = None,
+    size: int = 6
+):
+    value = f' value="{value}"' if value else ''
+    return f"""
+<div class="col-sm-{size}">
+    <input type="{input_type}"{value} class="form-control" name="{name}" form-control-user" id="exampleFirstName"
+        placeholder="{name}">
+</div>
+
+"""
+
+def get_checkbox_item(
+    checkbox_name: str,
+    label: str,
+    checked=False
+):
+    checked= ' checked="true"' if checked else ''
+    return f"""
+<div class="col-sm-12 col-md-8 col-lg-6">
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" name="{checkbox_name}" value="{label}" id="{label}"{checked}>
+        <label class="form-check-label" for="{label}">
+        {label}
+        </label>
+    </div>
+</div>
+"""
+
+def get_checkbox(
+    name: str,
+    choices: set,
+    size: int = 4
+):
+    choices = set(choices)
+    choices = ''.join(
+        [get_checkbox_item(name, label=choice[0], checked=choice[1]) for choice in choices]
+    )
+    check_box =  f"""
+<div class="row">
+    {choices}
+</div>
+"""
+    return card.get_card(
+        name,
+        body=check_box,
+        size=size
+    )
