b7410c76ef4ce24b1cf4f4b61e8e507d8170b997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:48 +0000

    Add package plotmanx/0.4.92/plotmanx-0.4.92-py3-none-any.whl

---
+++ packages/plotmanx/plotmanx-0.4.92-py3-none-any.whl/plotmanx/api.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+#
+
+import sqlite3
+import urllib
+from datetime import datetime
+
+import requests
+from tornado import web, ioloop, httpserver
+
+from .configuration import PlotmanConfig, get_db_path
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+
+def PostDat(dp: dict, cfg: PlotmanConfig):
+    # sending post request and saving response as response object
+    payload = json.dumps(dp)
+    requests.post(url=f'http://{cfg.apis.target}:{cfg.apis.port}/report', data=payload)
+
+
+"""
+
+from prometheus_client.core import GaugeMetricFamily
+
+class PlotmanCollector:
+
+    def collect(self):
+        cfg = configuration.get_validated_configs()
+        jobs = Job.genTasks(cfg.directories.log)
+        count = len(sorted(jobs, key=job.Job.get_time_wall))
+        yield GaugeMetricFamily("plotman_jobs_count", "Number of plotting jobs running", value=count)
+
+if __name__ == "__main__":
+    start_http_server(8001)
+    REGISTRY.register(PlotmanCollector())
+    while True:
+        time.sleep(1)
+"""
+
+
+class MainHandler(web.RequestHandler):
+    def initialize(self, *args, **kwargs):
+        self.remote_ip = self.request.headers.get('X-Forwarded-For', self.request.headers.get('X-Real-Ip', self.request.remote_ip))
+        self.using_ssl = (self.request.headers.get('X-Scheme', 'http') == 'https')
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        req_body = self.request.body
+        jsondata = dict()
+        try:
+            jsondata = json.loads(req_body)
+        except:
+            jsondata = json.loads(urllib.unquote_plus(req_body))
+
+        ts = datetime.now().strftime('%m-%d %H:%M:%S')
+
+        con = sqlite3.connect(get_db_path())
+        cur = con.cursor()
+
+        cur.execute('''CREATE TABLE IF NOT EXISTS systemchia (
+                           tid text PRIMARY KEY,
+                           block text NOT NULL,
+                           ip text NOT NULL
+                   );''')
+
+        if req_body is not None:
+            # Insert a row of data
+            cur.execute(f"INSERT INTO systemchia VALUES ('{ts}','{req_body}','{self.remote_ip}')")
+
+        con.commit()
+        con.close()
+
+        print(jsondata)
+        print(req_body)
+
+
+def apiOpen(cfg: PlotmanConfig):
+    print(f"api port {cfg.apis.port} is now listening")
+    application = web.Application([(r"/report", MainHandler)])
+    http_server = httpserver.HTTPServer(application)
+    http_server.listen(cfg.apis.port)
+    ioloop.IOLoop.current().start()
