931da67d5f1a3523a68cb88150bfe2c59e43e2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:13 +0000

    Add package sciplot/0.8.0/Sciplot-0.8.0.tar.gz

---
+++ packages/sciplot/Sciplot-0.8.0.tar.gz/Sciplot-0.8.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Sciplot',
+    version='0.8.0',
+    author='Andreas Führ',
+    author_email='andreas.fuhr@outlook.com',
+    license='MIT',
+    description='Format Matplotlib scientific plots',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/andreasfuhr/sciplot',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Framework :: Matplotlib',
+        'Topic :: Scientific/Engineering'
+    ],
+    packages=['sciplot'],
+    package_data={'sciplot': ['parameters/*.yml', '../README.md']},
+    include_package_data=True,
+    python_requires='>=3.7',
+    install_requires=['matplotlib>=3.3.4', 'pyyaml', 'seaborn'],
+)
