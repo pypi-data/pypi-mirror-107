786bdebdd7788b8bf21fa95cfb495d5a30b400e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:17 +0000

    Add package aurcore/1.2.13/aurcore-1.2.13.tar.gz

---
+++ packages/aurcore/aurcore-1.2.13.tar.gz/aurcore-1.2.13/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aurcore', 'aurcore.event', 'aurcore.log', 'aurcore.persist', 'aurcore.util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyyaml>=5.3.1,<6.0.0']
+
+setup_kwargs = {
+    'name': 'aurcore',
+    'version': '1.2.13',
+    'description': 'Aurcore!',
+    'long_description': None,
+    'author': 'Zenith',
+    'author_email': 'z@zenith.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
