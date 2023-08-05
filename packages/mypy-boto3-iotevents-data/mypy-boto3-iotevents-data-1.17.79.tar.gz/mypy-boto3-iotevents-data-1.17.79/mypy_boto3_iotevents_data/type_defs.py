634c36198555d55ba5064f7350b33638846c0588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:33 +0000

    Add package mypy-boto3-iotevents-data/1.17.79/mypy-boto3-iotevents-data-1.17.79.tar.gz

---
+++ packages/mypy-boto3-iotevents-data/mypy-boto3-iotevents-data-1.17.79.tar.gz/mypy-boto3-iotevents-data-1.17.79/mypy_boto3_iotevents_data/type_defs.py
@@ -0,0 +1,213 @@
+"""
+Type annotations for iotevents-data service type definitions.
+
+[Open documentation](./type_defs.md)
+
+Usage::
+
+    ```python
+    from mypy_boto3_iotevents_data.type_defs import BatchPutMessageErrorEntryTypeDef
+
+    data: BatchPutMessageErrorEntryTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import IO, List, Union
+
+from .literals import ErrorCodeType
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "BatchPutMessageErrorEntryTypeDef",
+    "BatchPutMessageResponseTypeDef",
+    "BatchUpdateDetectorErrorEntryTypeDef",
+    "BatchUpdateDetectorResponseTypeDef",
+    "DescribeDetectorResponseTypeDef",
+    "DetectorStateDefinitionTypeDef",
+    "DetectorStateSummaryTypeDef",
+    "DetectorStateTypeDef",
+    "DetectorSummaryTypeDef",
+    "DetectorTypeDef",
+    "ListDetectorsResponseTypeDef",
+    "MessageTypeDef",
+    "TimerDefinitionTypeDef",
+    "TimerTypeDef",
+    "UpdateDetectorRequestTypeDef",
+    "VariableDefinitionTypeDef",
+    "VariableTypeDef",
+)
+
+BatchPutMessageErrorEntryTypeDef = TypedDict(
+    "BatchPutMessageErrorEntryTypeDef",
+    {
+        "messageId": str,
+        "errorCode": ErrorCodeType,
+        "errorMessage": str,
+    },
+    total=False,
+)
+
+BatchPutMessageResponseTypeDef = TypedDict(
+    "BatchPutMessageResponseTypeDef",
+    {
+        "BatchPutMessageErrorEntries": List["BatchPutMessageErrorEntryTypeDef"],
+    },
+    total=False,
+)
+
+BatchUpdateDetectorErrorEntryTypeDef = TypedDict(
+    "BatchUpdateDetectorErrorEntryTypeDef",
+    {
+        "messageId": str,
+        "errorCode": ErrorCodeType,
+        "errorMessage": str,
+    },
+    total=False,
+)
+
+BatchUpdateDetectorResponseTypeDef = TypedDict(
+    "BatchUpdateDetectorResponseTypeDef",
+    {
+        "batchUpdateDetectorErrorEntries": List["BatchUpdateDetectorErrorEntryTypeDef"],
+    },
+    total=False,
+)
+
+DescribeDetectorResponseTypeDef = TypedDict(
+    "DescribeDetectorResponseTypeDef",
+    {
+        "detector": "DetectorTypeDef",
+    },
+    total=False,
+)
+
+DetectorStateDefinitionTypeDef = TypedDict(
+    "DetectorStateDefinitionTypeDef",
+    {
+        "stateName": str,
+        "variables": List["VariableDefinitionTypeDef"],
+        "timers": List["TimerDefinitionTypeDef"],
+    },
+)
+
+DetectorStateSummaryTypeDef = TypedDict(
+    "DetectorStateSummaryTypeDef",
+    {
+        "stateName": str,
+    },
+    total=False,
+)
+
+DetectorStateTypeDef = TypedDict(
+    "DetectorStateTypeDef",
+    {
+        "stateName": str,
+        "variables": List["VariableTypeDef"],
+        "timers": List["TimerTypeDef"],
+    },
+)
+
+DetectorSummaryTypeDef = TypedDict(
+    "DetectorSummaryTypeDef",
+    {
+        "detectorModelName": str,
+        "keyValue": str,
+        "detectorModelVersion": str,
+        "state": "DetectorStateSummaryTypeDef",
+        "creationTime": datetime,
+        "lastUpdateTime": datetime,
+    },
+    total=False,
+)
+
+DetectorTypeDef = TypedDict(
+    "DetectorTypeDef",
+    {
+        "detectorModelName": str,
+        "keyValue": str,
+        "detectorModelVersion": str,
+        "state": "DetectorStateTypeDef",
+        "creationTime": datetime,
+        "lastUpdateTime": datetime,
+    },
+    total=False,
+)
+
+ListDetectorsResponseTypeDef = TypedDict(
+    "ListDetectorsResponseTypeDef",
+    {
+        "detectorSummaries": List["DetectorSummaryTypeDef"],
+        "nextToken": str,
+    },
+    total=False,
+)
+
+MessageTypeDef = TypedDict(
+    "MessageTypeDef",
+    {
+        "messageId": str,
+        "inputName": str,
+        "payload": Union[bytes, IO[bytes]],
+    },
+)
+
+TimerDefinitionTypeDef = TypedDict(
+    "TimerDefinitionTypeDef",
+    {
+        "name": str,
+        "seconds": int,
+    },
+)
+
+TimerTypeDef = TypedDict(
+    "TimerTypeDef",
+    {
+        "name": str,
+        "timestamp": datetime,
+    },
+)
+
+_RequiredUpdateDetectorRequestTypeDef = TypedDict(
+    "_RequiredUpdateDetectorRequestTypeDef",
+    {
+        "messageId": str,
+        "detectorModelName": str,
+        "state": "DetectorStateDefinitionTypeDef",
+    },
+)
+_OptionalUpdateDetectorRequestTypeDef = TypedDict(
+    "_OptionalUpdateDetectorRequestTypeDef",
+    {
+        "keyValue": str,
+    },
+    total=False,
+)
+
+
+class UpdateDetectorRequestTypeDef(
+    _RequiredUpdateDetectorRequestTypeDef, _OptionalUpdateDetectorRequestTypeDef
+):
+    pass
+
+
+VariableDefinitionTypeDef = TypedDict(
+    "VariableDefinitionTypeDef",
+    {
+        "name": str,
+        "value": str,
+    },
+)
+
+VariableTypeDef = TypedDict(
+    "VariableTypeDef",
+    {
+        "name": str,
+        "value": str,
+    },
+)
