5f0224fae33b27c0216b2aaaba0981e5292228b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:25 +0000

    Add package randomstuff.py/1.3.1/randomstuff.py-1.3.1-py3-none-any.whl

---
+++ packages/randomstuff.py/randomstuff.py-1.3.1-py3-none-any.whl/randomstuff/constants.py
@@ -0,0 +1,15 @@
+def _warn(client, warning):
+	if client.suppress_warnings == True:
+		return
+	else:
+		print("[WARNING] "+"\n"+warning)
+
+BASE_URL = 'https://api.pgamerx.com'
+IMAGE_TYPES = ['aww', 'duck', 
+	'dog', 'cat', 
+	'memes', 'dankmemes', 
+	'holup', 'art', 
+	'harrypottermemes','facepalm']
+JOKE_TYPES = ['any', 'dev', 'spooky', 'pun']
+PLANS = [None, 'pro', 'ultra', 'biz', 'mega']
+VERSIONS = ['2', '3']
