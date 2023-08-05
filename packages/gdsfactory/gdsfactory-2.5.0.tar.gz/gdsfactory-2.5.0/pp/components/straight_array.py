302294ae60eabb2c78b9682d04e32540d168b938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:50 +0000

    Add package gdsfactory/2.5.0/gdsfactory-2.5.0.tar.gz

---
+++ packages/gdsfactory/gdsfactory-2.5.0.tar.gz/gdsfactory-2.5.0/pp/components/straight_array.py
@@ -0,0 +1,40 @@
+import pp
+from pp.component import Component
+from pp.components.straight import straight as straight_function
+from pp.port import auto_rename_ports
+from pp.types import ComponentOrFactory
+
+
+@pp.cell_with_validator
+def straight_array(
+    n_straights: int = 4,
+    spacing: float = 4.0,
+    straigth: ComponentOrFactory = straight_function,
+    **straigth_settings
+) -> Component:
+    """Array of straights connected with grating couplers.
+
+    useful to align the 4 corners of the chip
+
+    Args:
+        n_straights: number of straights
+        spacing: edge to edge straight spacing
+        straigth: straigth straight Component or factory
+        **straigth_settings
+    """
+
+    c = Component()
+    wg = straigth(**straigth_settings) if callable(straigth) else straigth
+
+    for i in range(n_straights):
+        wref = c.add_ref(wg)
+        wref.y += i * (spacing + wg.width)
+        c.ports["E" + str(i)] = wref.ports["E0"]
+        c.ports["W" + str(i)] = wref.ports["W0"]
+    auto_rename_ports(c)
+    return c
+
+
+if __name__ == "__main__":
+    c = straight_array(waveguide="nitride")
+    c.show()
