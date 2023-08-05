c9b5a809733bd1418b0706bf4a9123a9f3ff127a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:44 +0000

    Add package smail3/0.0.0/smail3-0.0.0.tar.gz

---
+++ packages/smail3/smail3-0.0.0.tar.gz/smail3-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'smail3',
+    version = '0.0.0',
+    keywords='email sender',
+    description = 'a library for sending information to a email address',
+    license = 'MIT License',
+    url = 'https://github.com/dwtenis/smail',
+    author = 'Syrah Y',
+    author_email = 'dw313@126.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
