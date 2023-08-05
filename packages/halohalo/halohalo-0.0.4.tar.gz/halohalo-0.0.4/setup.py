7d1650d32fa4f1fb5c1d53faa809724442e3fe4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:57 +0000

    Add package halohalo/0.0.4/halohalo-0.0.4.tar.gz

---
+++ packages/halohalo/halohalo-0.0.4.tar.gz/halohalo-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'halohalo',
+    packages = find_packages(include = ['halohalo']),
+    version = '0.0.4',
+    description = 'Randomized AB Testing Library used for integrating with ML Deployments',
+    author = 'Miles Ong',
+    license = 'MIT',
+    install_requires = []
+    # setup_requires = ['pytest-runner'],
+    # tests_require = ['pytest==4.4.1'],
+    # test_suite = 'tests'
+)
