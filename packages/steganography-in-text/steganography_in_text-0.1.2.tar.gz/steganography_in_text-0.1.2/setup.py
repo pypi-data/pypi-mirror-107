93e21755274a5eac0366a7004cfa9e340d6dd8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:32 +0000

    Add package steganography-in-text/0.1.2/steganography_in_text-0.1.2.tar.gz

---
+++ packages/steganography-in-text/steganography_in_text-0.1.2.tar.gz/steganography_in_text-0.1.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+"""
+:authors: Lexinus
+:license: Apache License, version 2.0, see LICENSE file
+:copyright: (c) 2021 Lexinus
+"""
+
+version = '0.1.2'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="steganography_in_text",
+    version=version,
+    author="Lexinus",
+    license='Apache License, version 2.0, see LICENSE file',
+    author_email="starshenkov3@gmail.com",
+    description="Это модуль, умеющий скрывать текст в тексте",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/red-lexinus/steganography_in_text",
+    project_urls={
+        "Bug Tracker": "https://github.com/red-lexinus/steganography_in_text",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={},
+    packages=['steganography_in_text'],
+    python_requires=">=3.6",
+)
