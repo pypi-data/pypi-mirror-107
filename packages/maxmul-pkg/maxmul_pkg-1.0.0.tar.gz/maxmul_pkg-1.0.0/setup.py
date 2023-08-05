51d8b9e8cab75dc931308e666deda64d38ba9751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:44 +0000

    Add package maxmul-pkg/1.0.0/maxmul_pkg-1.0.0.tar.gz

---
+++ packages/maxmul-pkg/maxmul_pkg-1.0.0.tar.gz/maxmul_pkg-1.0.0/setup.py
@@ -0,0 +1,23 @@
+﻿#配置发布信息
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="maxmul_pkg",
+    version="1.0.0",
+    author="jobi",
+    authoremail="jobi@gmail.com", 
+    description="an example",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),#返回一个需要但是目录没有的包的list
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+        ],
+    )
+
