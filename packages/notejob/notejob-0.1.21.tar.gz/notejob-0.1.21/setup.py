3368a0a31c86ee44053bd29143f8b39fdd5d42ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:58 +0000

    Add package notejob/0.1.21/notejob-0.1.21.tar.gz

---
+++ packages/notejob/notejob-0.1.21.tar.gz/notejob-0.1.21/setup.py
@@ -0,0 +1,24 @@
+from os import path
+
+from notebuild.tool import read_version
+from setuptools import find_packages, setup
+
+version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
+
+
+install_requires = ['apscheduler', 'gunicorn', 'flask-migrate', 'flask-script']
+
+setup(name='notejob',
+      version=read_version(version_path),
+      description='notejob',
+      author='niuliangtao',
+      author_email='1007530194@qq.com',
+      url='https://github.com/1007530194',
+      entry_points={'console_scripts': ['notejob = notejob.core:notejob']},
+      packages=find_packages(),
+      #package_data={"notejob": ["*", "*.html", "*.js", "*.*"]},
+      package_data={"": ["*.js"]},
+      #include_package_data=True,
+      install_requires=install_requires
+      
+      )
