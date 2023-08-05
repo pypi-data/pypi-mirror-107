99e91cd9d934d1c324f2cbddd88f4b0afd301e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:43 +0000

    Add package splaxa/1.0/splaxa-1.0.tar.gz

---
+++ packages/splaxa/splaxa-1.0.tar.gz/splaxa-1.0/splax/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'aminonf'
+__author__ = 'NfrXDRA'
+__license__ = 'MIT'
+__version__ = '1.2'
+from requests import get
+from json import loads
+
+__newest__ = loads(get("https://pypi.python.org/pypi/aminonf/json").text)["info"]["version"]
+
+if __version__ != __newest__:
+	print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
