9ad08aa4f2737588a28c20e8e78ca19c7d9b830f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:16 +0000

    Add package board-game-bot/0.1.1/board-game-bot-0.1.1.tar.gz

---
+++ packages/board-game-bot/board-game-bot-0.1.1.tar.gz/board-game-bot-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['board_game_bot']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['board-game-utils>=0.1.1,<0.2.0',
+ 'pytility>=0.3.0,<0.4.0',
+ 'tweepy>=3.10.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'board-game-bot',
+    'version': '0.1.1',
+    'description': 'Board game recommender bots 🤖',
+    'long_description': '# 🎲 Board Game Bot 🤖\n',
+    'author': 'Markus Shepherd',
+    'author_email': 'markus@recommend.games',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://recommend.games/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
