0c63c3760a9df9e38b25fde5d1fe20397f4aa1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:44 +0000

    Add package myt/0.5.5/myt-0.5.5.tar.gz

---
+++ packages/myt/myt-0.5.5.tar.gz/myt-0.5.5/README.md
@@ -0,0 +1,175 @@
+# t
+
+Command line tool for showing time in different time zones and other temporal information.
+
+```
+pipx install --user myt
+```
+
+```
+Usage: t [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  -J, --json  Output in JSON
+  --help      Show this message and exit.
+
+Commands:
+  m      Moon matrix
+  s      Sun and moon
+  t      Time in different zones
+  z      24hrs in time zones (on date)
+  zones  List common time zones and UTC offset (on date)
+```
+
+## t m
+
+```
+Usage: t m [OPTIONS]
+
+Options:
+  -y, --year TEXT  Year for calculation
+  --help           Show this message and exit.
+```
+
+Example (alignment looks OK in terminal): 
+```
+t m
+     Moon phases for year: 2021
+     01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+Jan  🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖
+Feb  🌖 🌖 🌗 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕
+Mar  🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌖 🌖
+Apr  🌖 🌖 🌗 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖
+May  🌖 🌖 🌗 🌗 🌗 🌘 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖
+Jun  🌗 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌖 🌖 🌖 🌖 🌗
+Jul  🌗 🌗 🌗 🌘 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌗 🌗
+Aug  🌗 🌗 🌘 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌖 🌗 🌗 🌗
+Sep  🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌗
+Oct  🌘 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌗 🌘
+Nov  🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌘 🌘
+Dec  🌘 🌘 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌘 🌘 🌘
+```
+
+## t s
+
+```
+Usage: t s [OPTIONS]
+
+Options:
+  -l, --location TEXT  Location as longitude,latitude (WGS84, dd)
+  -t, --date TEXT      Date for calculation
+  -f, --format TEXT    Output time format
+  --help               Show this message and exit.
+```
+
+Example. Rise times are dawn, sunrise, end of golden hour, Set are emas:
+```
+t s -t "2021-04-24"
+Location: -76.6963,39.0715
+Rise: 05:51:28  06:19:41  06:56:02
+Set:  19:16:19  19:52:39  20:20:52
+Moon: 🌔 (0.40)
+```
+
+## t t
+
+```
+Usage: t t [OPTIONS]
+
+  Time in different zones
+
+Options:
+  -t, --date TEXT   Date for calculation
+  -z, --zones TEXT  Comma separated list of timezones
+  --help            Show this message and exit.
+```
+
+Example:
+```
+t t -t "1 April 8pm"
+Local             2021-04-01T20:00:00-0400
+Europe/Copenhagen 2021-04-02T02:00:00+0200
+UTC               2021-04-02T00:00:00+0000
+US/Eastern        2021-04-01T20:00:00-0400
+US/Central        2021-04-01T19:00:00-0500
+US/Mountain       2021-04-01T18:00:00-0600
+America/Phoenix   2021-04-01T17:00:00-0700
+US/Pacific        2021-04-01T17:00:00-0700
+US/Alaska         2021-04-01T16:00:00-0800
+Pacific/Tahiti    2021-04-01T14:00:00-1000
+Pacific/Auckland  2021-04-02T13:00:00+1300
+Australia/Sydney  2021-04-02T11:00:00+1100
+```
+
+## t z
+
+```
+Usage: t z [OPTIONS]
+
+  24hrs in time zones (on date)
+
+Options:
+  -f, --format TEXT  Output time format
+  -t, --date TEXT    Date for calculation
+  -z, --zones TEXT   Comma separated list of timezones
+  --help             Show this message and exit.
+```
+
+Example:
+```
+t z
+Local             07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05 06
+Europe/Copenhagen 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05 06 07 08 09 10 11
+UTC               11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05 06 07 08 09 10
+US/Eastern        07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05 06
+US/Central        06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05
+US/Mountain       05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04
+America/Phoenix   04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03
+US/Pacific        04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03
+US/Alaska         03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02
+Pacific/Tahiti    01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00
+Pacific/Auckland  00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+Australia/Sydney  22 23 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21
+```
+
+## t zones
+
+```
+Usage: t zones [OPTIONS]
+
+  List common time zones and UTC offset (on date)
+
+Options:
+  -t, --date TEXT  Date for calculation
+  --help           Show this message and exit.
+```
+
+Example:
+```
+t zones -t "2021-12-21"
+-11:00 Pacific/Midway
+-11:00 Pacific/Niue
+-11:00 Pacific/Pago_Pago
+-10:00 America/Adak
+-10:00 Pacific/Honolulu
+-10:00 Pacific/Rarotonga
+-10:00 Pacific/Tahiti
+-10:00 US/Hawaii
+-09:30 Pacific/Marquesas
+...
++00:00 Europe/London
++00:00 GMT
++00:00 UTC
++01:00 Africa/Algiers
+...
++13:45 Pacific/Chatham
++14:00 Pacific/Apia
++14:00 Pacific/Kiritimati
+```
+
+
+## Change Log
+
+v0.5.5 
+- Added T_LOCATION and T_LOGLEVEL environment variable options
+- Cleaned up some math in suncalc
---
+++ packages/myt/myt-0.5.5.tar.gz/myt-0.5.5/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['t']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0',
+ 'dateparser>=1.0.0,<2.0.0',
+ 'pytz>=2021.1,<2022.0',
+ 'requests>=2.25.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['t = t.__main__:main']}
+
+setup_kwargs = {
+    'name': 'myt',
+    'version': '0.5.5',
+    'description': 'Time, zones',
+    'long_description': '# t\n\nCommand line tool for showing time in different time zones and other temporal information.\n\n```\npipx install --user myt\n```\n\n```\nUsage: t [OPTIONS] COMMAND [ARGS]...\n\nOptions:\n  -J, --json  Output in JSON\n  --help      Show this message and exit.\n\nCommands:\n  m      Moon matrix\n  s      Sun and moon\n  t      Time in different zones\n  z      24hrs in time zones (on date)\n  zones  List common time zones and UTC offset (on date)\n```\n\n## t m\n\n```\nUsage: t m [OPTIONS]\n\nOptions:\n  -y, --year TEXT  Year for calculation\n  --help           Show this message and exit.\n```\n\nExample (alignment looks OK in terminal): \n```\nt m\n     Moon phases for year: 2021\n     01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31\nJan  🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖\nFeb  🌖 🌖 🌗 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕\nMar  🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌖 🌖\nApr  🌖 🌖 🌗 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖\nMay  🌖 🌖 🌗 🌗 🌗 🌘 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖\nJun  🌗 🌗 🌗 🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌖 🌖 🌖 🌖 🌗\nJul  🌗 🌗 🌗 🌘 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌗 🌗\nAug  🌗 🌗 🌘 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌖 🌗 🌗 🌗\nSep  🌗 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌗\nOct  🌘 🌘 🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌗 🌘\nNov  🌘 🌘 🌑 🌑 🌑 🌑 🌒 🌒 🌒 🌓 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌘 🌘\nDec  🌘 🌘 🌑 🌑 🌑 🌒 🌒 🌒 🌒 🌓 🌓 🌓 🌔 🌔 🌔 🌔 🌔 🌕 🌕 🌕 🌖 🌖 🌖 🌖 🌖 🌗 🌗 🌗 🌘 🌘 🌘\n```\n\n## t s\n\n```\nUsage: t s [OPTIONS]\n\nOptions:\n  -l, --location TEXT  Location as longitude,latitude (WGS84, dd)\n  -t, --date TEXT      Date for calculation\n  -f, --format TEXT    Output time format\n  --help               Show this message and exit.\n```\n\nExample. Rise times are dawn, sunrise, end of golden hour, Set are emas:\n```\nt s -t "2021-04-24"\nLocation: -76.6963,39.0715\nRise: 05:51:28  06:19:41  06:56:02\nSet:  19:16:19  19:52:39  20:20:52\nMoon: 🌔 (0.40)\n```\n\n## t t\n\n```\nUsage: t t [OPTIONS]\n\n  Time in different zones\n\nOptions:\n  -t, --date TEXT   Date for calculation\n  -z, --zones TEXT  Comma separated list of timezones\n  --help            Show this message and exit.\n```\n\nExample:\n```\nt t -t "1 April 8pm"\nLocal             2021-04-01T20:00:00-0400\nEurope/Copenhagen 2021-04-02T02:00:00+0200\nUTC               2021-04-02T00:00:00+0000\nUS/Eastern        2021-04-01T20:00:00-0400\nUS/Central        2021-04-01T19:00:00-0500\nUS/Mountain       2021-04-01T18:00:00-0600\nAmerica/Phoenix   2021-04-01T17:00:00-0700\nUS/Pacific        2021-04-01T17:00:00-0700\nUS/Alaska         2021-04-01T16:00:00-0800\nPacific/Tahiti    2021-04-01T14:00:00-1000\nPacific/Auckland  2021-04-02T13:00:00+1300\nAustralia/Sydney  2021-04-02T11:00:00+1100\n```\n\n## t z\n\n```\nUsage: t z [OPTIONS]\n\n  24hrs in time zones (on date)\n\nOptions:\n  -f, --format TEXT  Output time format\n  -t, --date TEXT    Date for calculation\n  -z, --zones TEXT   Comma separated list of timezones\n  --help             Show this message and exit.\n```\n\nExample:\n```\nt z\nLocal             07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05 06\nEurope/Copenhagen 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05 06 07 08 09 10 11\nUTC               11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05 06 07 08 09 10\nUS/Eastern        07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05 06\nUS/Central        06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04 05\nUS/Mountain       05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03 04\nAmerica/Phoenix   04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03\nUS/Pacific        04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02 03\nUS/Alaska         03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00 01 02\nPacific/Tahiti    01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 00\nPacific/Auckland  00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23\nAustralia/Sydney  22 23 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21\n```\n\n## t zones\n\n```\nUsage: t zones [OPTIONS]\n\n  List common time zones and UTC offset (on date)\n\nOptions:\n  -t, --date TEXT  Date for calculation\n  --help           Show this message and exit.\n```\n\nExample:\n```\nt zones -t "2021-12-21"\n-11:00 Pacific/Midway\n-11:00 Pacific/Niue\n-11:00 Pacific/Pago_Pago\n-10:00 America/Adak\n-10:00 Pacific/Honolulu\n-10:00 Pacific/Rarotonga\n-10:00 Pacific/Tahiti\n-10:00 US/Hawaii\n-09:30 Pacific/Marquesas\n...\n+00:00 Europe/London\n+00:00 GMT\n+00:00 UTC\n+01:00 Africa/Algiers\n...\n+13:45 Pacific/Chatham\n+14:00 Pacific/Apia\n+14:00 Pacific/Kiritimati\n```\n\n\n## Change Log\n\nv0.5.5 \n- Added T_LOCATION and T_LOGLEVEL environment variable options\n- Cleaned up some math in suncalc\n',
+    'author': 'datadavev',
+    'author_email': '605409+datadavev@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/datadavev/t',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
