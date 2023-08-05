1366b263e9bffbef6ab8b715476bcd6b72f5e39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:00 +0000

    Add package eye-exam/0.1.0/eye_exam-0.1.0.tar.gz

---
+++ packages/eye-exam/eye_exam-0.1.0.tar.gz/eye_exam-0.1.0/eye_exam/eye_exam.py
@@ -0,0 +1,3 @@
+def eye_exam():
+  print("visit:  https://www.personaleyes.com.au/online-eye-test/index.php")
+  print(" 1m away, 6 questions are asked  ")
---
+++ packages/eye-exam/eye_exam-0.1.0.tar.gz/eye_exam-0.1.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(
+name='eye_exam',
+version='0.1.0',
+description='DESCRIPTION ENCOMPASSES',
+author='pleasefeedme',
+author_email='myfakeemail@gmail.com',
+license='Python. Free to use no attribution required.',
+packages=['eye_exam'],
+install_requires=[])
