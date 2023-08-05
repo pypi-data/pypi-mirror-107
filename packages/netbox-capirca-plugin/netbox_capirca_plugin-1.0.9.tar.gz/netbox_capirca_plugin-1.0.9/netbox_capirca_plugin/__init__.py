a82fd9b1c87e19b381395fe776ffa6485867838c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:43 +0000

    Add package netbox-capirca-plugin/1.0.9/netbox_capirca_plugin-1.0.9.tar.gz

---
+++ packages/netbox-capirca-plugin/netbox_capirca_plugin-1.0.9.tar.gz/netbox_capirca_plugin-1.0.9/netbox_capirca_plugin/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "1.0.9"
+
+from extras.plugins import PluginConfig
+
+
+class NetboxACLPluginConfig(PluginConfig):
+    name = 'netbox_capirca_plugin'
+    verbose_name = 'netbox Capirca Plugin'
+    description = 'A plugin for ACL management with capirca',
+    version = __version__
+    author = 'Johannes Erwerle'
+    author_email = 'erwerle@belwue.de'
+    required_settings = []
+    base_url = "netbox_capirca_plugin"
+    default_settings = {}
+    required_settings = ["default_policy_template",
+                         "definitions_path",
+                         "policy_base_path"]
+
+
+config = NetboxACLPluginConfig
