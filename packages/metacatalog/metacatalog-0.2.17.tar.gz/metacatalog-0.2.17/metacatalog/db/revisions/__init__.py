a007ce0fcf50b9e76630ea13d0c54814bb796143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:34 +0000

    Add package metacatalog/0.2.17/metacatalog-0.2.17.tar.gz

---
+++ packages/metacatalog/metacatalog-0.2.17.tar.gz/metacatalog-0.2.17/metacatalog/db/revisions/__init__.py
@@ -0,0 +1,17 @@
+from . import (
+    rev0,
+    rev1,
+    rev2,
+    rev3,
+    rev4,
+    rev5,
+)
+
+revisions = {
+    0: rev0,
+    1: rev1,
+    2: rev2,
+    3: rev3,
+    4: rev4,
+    5: rev5,
+}
