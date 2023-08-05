ddb7ddd8b81f314448b20e2bea4abe3c715a95ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:04 +0000

    Add package opensesame-extension-omm/0.3.0/opensesame_extension_omm-0.3.0-py2.py3-none-any.whl

---
+++ packages/opensesame-extension-omm/opensesame_extension_omm-0.3.0-py2.py3-none-any.whl/openmonkeymind/_exceptions.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+from libopensesame.py3compat import *
+
+class ParticipantNotFound(Exception): pass
+class NoJobsForParticipant(Exception): pass
+class FailedToSendJobResults(Exception): pass
+class InvalidJSON(Exception): pass
+class FailedToDownloadExperiment(Exception): pass
+class FailedToSetJobStates(Exception): pass
+class FailedToDeleteJobs(Exception): pass
+class FailedToInsertJobs(Exception): pass
+class FailedToGetJobs(Exception): pass
