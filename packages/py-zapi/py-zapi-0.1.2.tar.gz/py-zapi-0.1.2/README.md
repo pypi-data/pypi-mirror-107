da44a90b72f1b5dc4143a3bd8cf28c68cc74fde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:27 +0000

    Add package py-zapi/0.1.2/py-zapi-0.1.2.tar.gz

---
+++ packages/py-zapi/py-zapi-0.1.2.tar.gz/py-zapi-0.1.2/README.md
@@ -0,0 +1,8 @@
+# Python ZAPI Library
+
+Pre-alpha state, this will change without warning.
+
+ZeroMQ devices used in `plantd`.
+
+* MDP broker
+* MDP client and worker
---
+++ packages/py-zapi/py-zapi-0.1.2.tar.gz/py-zapi-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zapi', 'zapi.mdp']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyzmq>=22.0']
+
+setup_kwargs = {
+    'name': 'py-zapi',
+    'version': '0.1.2',
+    'description': 'ZeroMQ Devices API',
+    'long_description': '# Python ZAPI Library\n\nPre-alpha state, this will change without warning.\n\nZeroMQ devices used in `plantd`.\n\n* MDP broker\n* MDP client and worker\n',
+    'author': 'Geoff Johnson',
+    'author_email': 'geoff.jay@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/plantd/py-zapi',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
