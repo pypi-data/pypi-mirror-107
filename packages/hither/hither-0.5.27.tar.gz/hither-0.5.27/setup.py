cae6c50836b3a4179bf13d337bb6416ad8ab755a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:38 +0000

    Add package hither/0.5.27/hither-0.5.27.tar.gz

---
+++ packages/hither/hither-0.5.27.tar.gz/hither-0.5.27/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+pkg_name = "hither"
+
+setuptools.setup(
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    scripts=[
+        "bin/hither-compute-resource",
+        "bin/hither-scriptdir-runner",
+        "bin/hither-log"
+    ],
+    install_requires=[
+        "click",
+        "inquirer",
+        "pyyaml",
+        "dockerfile-parse",
+        "kachery_p2p>=0.8.10",
+        "docker"
+        # non-explicit dependencies: numpy
+    ]
+)
