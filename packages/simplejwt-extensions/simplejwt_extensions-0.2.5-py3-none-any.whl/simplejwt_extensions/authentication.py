72a8c702839ba3cca92eab7dd3348aefb728c555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:34 +0000

    Add package simplejwt-extensions/0.2.5/simplejwt_extensions-0.2.5-py3-none-any.whl

---
+++ packages/simplejwt-extensions/simplejwt_extensions-0.2.5-py3-none-any.whl/simplejwt_extensions/authentication.py
@@ -0,0 +1,34 @@
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.authentication import JWTAuthentication as SimpleJWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, AuthenticationFailed
+
+from .settings import api_settings
+
+
+class JWTAuthentication(SimpleJWTAuthentication):
+    def authenticate(self, request):
+        self.request = request
+        return super().authenticate(request)
+
+    def get_user(self, validated_token):
+        try:
+            user_id = validated_token[api_settings.USER_ID_CLAIM]
+        except KeyError:
+            raise InvalidToken(_('Token contained no recognizable user identification'))
+
+        User = get_user_model()
+
+        try:
+            user = User.objects.get(**{api_settings.USER_ID_FIELD: user_id})
+        except User.DoesNotExist:
+            user = None
+            if api_settings.NEW_USER_CALLBACK:
+                user = api_settings.NEW_USER_CALLBACK(self.request, user_id)
+            if user is None:
+                raise AuthenticationFailed(_('User not found'), code='user_not_found')
+
+        if not user.is_active:
+            raise AuthenticationFailed(_('User is inactive'), code='user_inactive')
+
+        return user
