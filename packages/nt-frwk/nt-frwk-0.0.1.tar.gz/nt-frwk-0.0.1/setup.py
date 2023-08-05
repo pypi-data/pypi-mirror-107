301c40b280ab0500038c1ad904a2445df3496cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:28 +0000

    Add package nt-frwk/0.0.1/nt-frwk-0.0.1.tar.gz

---
+++ packages/nt-frwk/nt-frwk-0.0.1.tar.gz/nt-frwk-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="nt-frwk",  # Replace with your own username
+    version="0.0.1",
+    author="newstower",
+    author_email="kj2945@gmail.com",
+    description="newstower",
+    # long_description='',
+    # long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
