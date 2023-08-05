a27bba9250c56f22bb81fbc7f821ad9a752602e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:30 +0000

    Add package colocationship/0.0.3/colocationship-0.0.3.tar.gz

---
+++ packages/colocationship/colocationship-0.0.3.tar.gz/colocationship-0.0.3/colocationship/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Class for co-locationship, meetup_strategy
+# (c) Zexun Chen, 2020-12-16
+# sxtpy2010@gmail.com
+
+from . import util
+from .meetup_strategy import *
+from .meetup_comparison import *
