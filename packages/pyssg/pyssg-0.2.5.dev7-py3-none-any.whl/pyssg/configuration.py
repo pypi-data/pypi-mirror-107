f2bc1a8565848ba34baf85ae949333830f95b0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:20 +0000

    Add package pyssg/0.2.5.dev7/pyssg-0.2.5.dev7-py3-none-any.whl

---
+++ packages/pyssg/pyssg-0.2.5.dev7-py3-none-any.whl/pyssg/configuration.py
@@ -0,0 +1,99 @@
+import os
+from typing import Union
+
+
+class Configuration:
+    def __init__(self, path: str):
+        self.path: str = path
+        self.src: str = None
+        self.dst: str = None
+        self.base_url: str = None
+        self.title: str = None
+        self.dformat: str = None
+        self.l_dformat: str = None
+        self.lsep_dformat: str = None
+        self.force: bool = None
+
+
+    def read(self):
+        try:
+            lines: list[str] = None
+            with open(self.path, 'r') as f:
+                lines = f.readlines()
+
+            opts: dict[str, Union[str, bool]] = dict()
+            for l in lines:
+                kv: list[str] = l.split('=', 1)
+                if len(kv) != 2:
+                    raise Exception('wrong config syntax')
+
+                k: str = kv[0].strip()
+                v_temp: str = kv[1].strip()
+                # check if value should be a boolean true
+                v: Union[str, bool] = v_temp\
+                    if v_temp.lower() not in ['true', '1', 'yes']\
+                    else True
+
+                opts[k] = v
+
+            try:
+                self.src = opts['SRC_PATH']
+            except KeyError: pass
+
+            try:
+                self.dst = opts['DST_PATH']
+            except KeyError: pass
+
+            try:
+                self.base_url = opts['BASE_URL']
+            except KeyError: pass
+
+            try:
+                self.title = opts['TITLE']
+            except KeyError: pass
+
+            try:
+                self.dformat = opts['DATE_FORMAT']
+            except KeyError: pass
+
+            try:
+                self.l_dformat = opts['LIST_DATE_FORMAT']
+            except KeyError: pass
+
+            try:
+                self.lsep_dformat = opts['LIST_SEP_DATE_FORMAT']
+            except KeyError: pass
+
+            try:
+                # if the parser above didn't read a boolean true, then take it
+                # as a false anyways
+                self.force = opts['FORCE'] if opts['FORCE'] is True else False
+            except KeyError: pass
+
+        except OSError: pass
+
+
+    def fill_missing(self, opts: dict[str, Union[str, bool]]) -> None:
+        if self.src is None:
+            self.src = opts['src']
+
+        if self.dst is None:
+            self.dst = opts['dst']
+
+        if self.base_url is None:
+            self.base_url = opts['url']
+
+        if self.title is None:
+            self.title = opts['title']
+
+        if self.dformat is None:
+            self.dformat = opts['date_format']
+
+        if self.l_dformat is None:
+            self.l_dformat = opts['list_date_format']
+
+        if self.lsep_dformat is None:
+            self.lsep_dformat = opts['list_sep_date_format']
+
+        if self.force is None:
+            self.force = opts['force']
---
+++ packages/pyssg/pyssg-0.2.5.dev7-py3-none-any.whl/pyssg/rss.py
@@ -0,0 +1,52 @@
+import os
+import importlib.metadata
+from datetime import datetime, timezone
+
+from .page import Page
+from .configuration import Configuration
+
+
+VERSION = importlib.metadata.version('pyssg')
+DFORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+
+
+class RSSBuilder:
+    def __init__(self, config: Configuration,
+                 template: str,
+                 pages: list[Page]):
+        self.config: Configuration = config
+        self.rss: str = template
+        self.pages: list[Page] = pages
+
+
+    def build(self):
+        # initial base replacements
+        self.rss = self.rss.replace('$$TITLE', self.config.title)
+        self.rss = self.rss.replace('$$LINK', self.config.base_url)
+        self.rss = self.rss.replace('$$PYSSGVERSION', VERSION)
+        items_formatted: str = self.__get_items_formatted()
+        self.rss = self.rss.replace('$$ITEMS', items_formatted)
+
+        current_date: str = datetime.now(tz=timezone.utc).strftime(DFORMAT)
+        self.rss = self.rss.replace('$$CURRENTDATE', current_date)
+
+        with open(os.path.join(self.config.dst, 'rss.xml'), 'w') as f:
+            f.write(self.rss)
+
+
+    def __get_items_formatted(self) -> str:
+        # i_f=items formatted for short
+        i_f: str = ''
+        for p in self.pages:
+            url: str = f'{self.config.base_url}/{p.name.replace(".md", ".html")}'
+            date: str = p.c_datetime.strftime(DFORMAT)
+
+            i_f = f'{i_f}    <item>\n'
+            i_f = f'{i_f}      <title>{p.title}</title>\n'
+            i_f = f'{i_f}      <link>{url}</link>\n'
+            i_f = f'{i_f}      <description>{p.summary}</description>\n'
+            i_f = f'{i_f}      <guid isPermaLink="true">{url}</guid>\n'
+            i_f = f'{i_f}      <pubDate>{date}</pubDate>\n'
+            i_f = f'{i_f}    </item>\n'
+
+        return i_f
