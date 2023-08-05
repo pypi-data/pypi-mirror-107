e04b1443ce75499b64d08170733a21bb93858d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:18 +0000

    Add package FastCNN2/1.21.525.1416/FastCNN2-1.21.525.1416.tar.gz

---
+++ packages/FastCNN2/FastCNN2-1.21.525.1416.tar.gz/FastCNN2-1.21.525.1416/FastCNN/entry/gongda.py
@@ -0,0 +1,73 @@
+"""
+import below
+---------------------------
+"""
+import os,sys
+
+
+debug = False
+if os.path.exists("./debug"):
+    sys.path.insert(0,os.path.abspath("../../"))
+    debug = True
+from FastCNN.prx.GongDaProxy import predictPicture
+"""
+function below
+--------------------------
+"""
+def getArg():
+    arg = None
+    if len(sys.argv) > 1:
+        arg = sys.argv.pop(1)
+    return arg
+
+def getKwargs():
+    """
+    get kwargs by formatter["key=value"]
+    call before getArg
+    """
+    kwargs = {}
+    for i in range(1,len(sys.argv)):
+        arg = sys.argv[i]
+        arg_split = arg.split('=')
+        if len(arg_split) > 1:
+            kwargs[arg_split[0].strip()] = kwargs[arg_split[1].strip()]
+            sys.argv.pop(i)
+    return kwargs
+
+"""
+class below(if need)
+---------------------------
+"""
+def printHelp():
+    print("--help below"+"-"*20)
+    print("predictPicture modelpath picturepath")
+
+def callPredictPicture():
+    modelpath = getArg()
+    picturepath = getArg()
+    
+    if modelpath:
+        if picturepath:
+            rtn = predictPicture(modelpath,picturepath)
+            print(rtn)
+            return
+        
+    
+    print("call predict error")
+
+"""
+main below
+---------------------------
+"""
+
+def main():
+    if len(sys.argv) == 1:
+        printHelp()
+        return
+    cmd = getArg()
+    if cmd == "predictPicture":
+        callPredictPicture()
+    pass
+
+if __name__ == "__main__":
+    main()
