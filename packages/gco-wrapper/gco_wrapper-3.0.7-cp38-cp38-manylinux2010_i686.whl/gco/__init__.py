99955c54a4738982051db43d51c38599b7c52654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:20 +0000

    Add package gco-wrapper/3.0.7/gco_wrapper-3.0.7-cp38-cp38-manylinux2010_i686.whl

---
+++ packages/gco-wrapper/gco_wrapper-3.0.7-cp38-cp38-manylinux2010_i686.whl/gco/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '3.0.7'
+__version_info__ = tuple([int(i) for i in __version__.split('.')])
+
+try:
+    from pygco import *  # noqa: F401 F403
+except ImportError:
+    from gco.pygco import *  # noqa: F401 F403
