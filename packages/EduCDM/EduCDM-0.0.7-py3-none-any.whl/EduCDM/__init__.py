17f07ecd3de25d225e6df3e1fb564de67668ed3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:40 +0000

    Add package EduCDM/0.0.7/EduCDM-0.0.7-py3-none-any.whl

---
+++ packages/EduCDM/EduCDM-0.0.7-py3-none-any.whl/EduCDM/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# 2021/3/17 @ tongshiwei
+
+
+from .meta import CDM
+from .MCD import MCD
+from .DINA import DINA
+from .FuzzyCDF import FuzzyCDF
+from .NCDM import NCDM
+from .IRT import GDIRT
+from .IRT import EMIRT
