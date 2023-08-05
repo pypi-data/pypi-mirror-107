9e508ebd68cb998333e9273ea8b9bb41a2c0509e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:01 +0000

    Add package u3driver/1.0.38/u3driver-1.0.38.tar.gz

---
+++ packages/u3driver/u3driver-1.0.38.tar.gz/u3driver-1.0.38/u3driver/commands/FindObjects/find_object_in_range_where_text_contains.py
@@ -0,0 +1,17 @@
+import json
+
+from u3driver.commands.command_returning_alt_elements import CommandReturningAltElements
+
+
+class FindObjectInRangeWhereTextContains(CommandReturningAltElements):
+    def __init__(self, socket,request_separator,request_end,appium_driver,value,text,range_path):
+        super(FindObjectInRangeWhereTextContains, self).__init__(socket,request_separator,request_end,appium_driver)
+        self.value=value
+        self.text = text
+        self.range_path = range_path
+
+    
+    def execute(self):
+        data = self.send_data(self.create_command('findObjectInRangeWhereTextContains', self.value,self.text,self.range_path))
+        return json.loads(data)['name']
+    
