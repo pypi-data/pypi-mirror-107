06d782fb42faab067a88afef15e1bceb291eeac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:35 +0000

    Add package Socialsearch/0.0.7/Socialsearch-0.0.7.tar.gz

---
+++ packages/Socialsearch/Socialsearch-0.0.7.tar.gz/Socialsearch-0.0.7/Socialsearch/Facebook/__init__.py
@@ -0,0 +1,3 @@
+def search(Search):
+    link = 'https://www.facebook.com/search/top?q={}'.format(Search)
+    return link
---
+++ packages/Socialsearch/Socialsearch-0.0.7.tar.gz/Socialsearch-0.0.7/Socialsearch/Google/__init__.py
@@ -0,0 +1,3 @@
+def search(Search):
+    link = 'https://www.google.com/search?q={}'.format(Search)
+    return link
