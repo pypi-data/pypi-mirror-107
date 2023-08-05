9a7c04e84b17b45e6d06bd29c22229369af6d695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:08 +0000

    Add package shelxfile/1/shelxfile-1.tar.gz

---
+++ packages/shelxfile/shelxfile-1.tar.gz/shelxfile-1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='shelxfile',
+    version='1',
+    url='https://github.com/dkratzert/ShelXFile',
+    license='Beerware License',
+    author='Daniel Kratzert',
+    author_email='dkratzert@gmx.de',
+    description='A parser for SHELXL results files.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir={"": "shelxfile"},
+    packages=find_packages(where="shelxfile", exclude=['tests']),
+    python_requires=">=3.5",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Environment :: Console",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
