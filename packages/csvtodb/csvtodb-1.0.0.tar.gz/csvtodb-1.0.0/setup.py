b38094e81571eb4e830a11be13c6d4ff44db1bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:32 +0000

    Add package csvtodb/1.0.0/csvtodb-1.0.0.tar.gz

---
+++ packages/csvtodb/csvtodb-1.0.0.tar.gz/csvtodb-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csvtodb",
+    version="1.0.0",
+    author="akk0ga",
+    author_email="ttn.glock@orange.fr",
+    description="Make it easier to build database/table/seeders for DBMS or framework like laravel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akk0ga/CsvToDB",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    packages=setuptools.find_packages(),
+    keywords=['csv', 'db', 'database', 'sql', 'framework', 'mysql'],
+    python_requires=">=3.6",
+)
