45a534bd212426e7db1018dbaa44dbf285642d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:09 +0000

    Add package atmos/0.2.6/atmos-0.2.6.tar.gz

---
+++ packages/atmos/atmos-0.2.6.tar.gz/atmos-0.2.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def parse_requirements(filename):
+    requirements = []
+    with open(filename) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line != "":
+                requirements.append(line)
+    return requirements
+
+install_reqs = parse_requirements('requirements.txt')
+
+setup(
+    name='atmos',
+    packages=['atmos'],
+    version='0.2.6',
+    description='Atmospheric sciences utility library',
+    author='Jeremy McGibbon',
+    author_email='mcgibbon@uw.edu',
+    install_requires=install_reqs,
+    url='https://github.com/mcgibbon/atmos',
+    keywords=['atmos', 'atmospheric', 'equations', 'geoscience', 'science'],
+    classifiers=[],
+    license='MIT',
+)
