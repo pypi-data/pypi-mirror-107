c7a361132853b865d816be36ea09935fd4a9ee14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:33 +0000

    Add package pybins/1.0.1/pybins-1.0.1-py3-none-any.whl

---
+++ packages/pybins/pybins-1.0.1-py3-none-any.whl/pybins/gtfo.py
@@ -0,0 +1,20 @@
+import argparse
+from tinydb import TinyDB, Query
+from rich.console import Console
+from rich.table import Table
+from rich.columns import Columns
+from rich.syntax import Syntax
+
+def get_binaries_functions(platform, binary):
+    print(f"{platform}\n{binary}")
+    pass
+
+def get_binaries_with_function(platform, function):
+    pass
+
+def get_binariy_commands(platform, function, binary):
+    pass
+
+
+def get_platform_functions(platform):
+    pass
