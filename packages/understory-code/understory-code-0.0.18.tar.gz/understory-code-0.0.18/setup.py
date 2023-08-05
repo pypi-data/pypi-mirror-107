745b4825f8ea368cd3d06f2d24b47762abf083ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:23 +0000

    Add package understory-code/0.0.18/understory-code-0.0.18.tar.gz

---
+++ packages/understory-code/understory-code-0.0.18.tar.gz/understory-code-0.0.18/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['understory', 'understory.pkg', 'understory.pkg.licensing', 'understory.src']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pendulum>=2.1.2,<3.0.0',
+ 'radon>=4.5.1,<5.0.0',
+ 'semver>=2.13.0,<3.0.0',
+ 'sh>=1.14.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'understory-code',
+    'version': '0.0.18',
+    'description': 'Tools for metamodern software development',
+    'long_description': '# understory-code\nTools for metamodern software development\n',
+    'author': 'Angelo Gladding',
+    'author_email': 'angelo@lahacker.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
