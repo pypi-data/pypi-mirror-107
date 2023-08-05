fb5ed8f4cba9689ad68266f9704dab282e9480d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:31 +0000

    Add package Augmentext/0.1.3/Augmentext-0.1.3.tar.gz

---
+++ packages/Augmentext/Augmentext-0.1.3.tar.gz/Augmentext-0.1.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='Augmentext',
+    packages=['Augmentext'],
+    version='0.1.3',
+    description='Text augmentation library for NLP applications.',
+    long_description='Text augmentation library for NLP applications.',
+    license='MIT',
+    author='Marcus D. Bloice',
+    author_email='marcus.bloice@medunigraz.at',
+    url='https://github.com/mdbloice/Augmentext',  # URL to GitHub repo
+    # download_url='https://github.com/mdbloice/Augmentext/tarball/0.1.0',  # Get this using git tag
+    keywords=['text', 'augmentation', 'generation', 'NLP', 'machine', 'learning'],
+    include_package_data=True,  # This will include all files in MANIFEST.in in the package when installing.
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+    # install_requires=[
+    #   'future>=0.16.0',
+    #   'futures>=3.2.0; python_version == "2.7"'
+    # ]
+    # zip_safe=False # Check this later.
+)
