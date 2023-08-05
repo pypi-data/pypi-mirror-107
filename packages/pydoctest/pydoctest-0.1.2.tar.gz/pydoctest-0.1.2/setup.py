d27264da786671102495786cae579bdba81ab159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:44 +0000

    Add package pydoctest/0.1.2/pydoctest-0.1.2.tar.gz

---
+++ packages/pydoctest/pydoctest-0.1.2.tar.gz/pydoctest-0.1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydoctest",
+    version="0.1.2",
+    author="Jeppe Rask",
+    author_email="jepperaskdk@gmail.com",
+    description="Test if doctype types match signatures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jepperaskdk/pydoctest",
+    project_urls={
+        "Bug Tracker": "https://github.com/jepperaskdk/pydoctest/issues",
+    },
+    package_dir={"": "."},
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pydoctest=pydoctest.main:main'
+        ]
+    },
+    python_requires=">=3.6",
+)
