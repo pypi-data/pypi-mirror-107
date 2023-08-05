96b5eb70bbdb5f8af2b64fa308574ae1f5b6d35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:37 +0000

    Add package web3-erc20-predefined/0.0.3/web3_erc20_predefined-0.0.3.tar.gz

---
+++ packages/web3-erc20-predefined/web3_erc20_predefined-0.0.3.tar.gz/web3_erc20_predefined-0.0.3/web3_erc20_predefined/core/predefined_erc20.py
@@ -0,0 +1,52 @@
+# ------------------------------------------------------------ Imports ----------------------------------------------------------- #
+
+# System
+from typing import Optional
+
+# Pip
+from web3.eth import Eth
+from eth_account.signers.local import LocalAccount
+
+from web3_erc20 import ERC20
+
+# Local
+from .predefined_erc20_constants import PredefinedERC20Constants
+
+# -------------------------------------------------------------------------------------------------------------------------------- #
+
+
+
+# ---------------------------------------------------- class: PredefinedERC20 ---------------------------------------------------- #
+
+class PredefinedERC20(ERC20):
+
+    # --------------------------------------------------------- Init --------------------------------------------------------- #
+
+    def __init__(
+        self,
+        eth: Eth,
+        costants: PredefinedERC20Constants,
+        account: Optional[LocalAccount] = None
+    ):
+        super().__init__(
+            eth=eth,
+            address=costants.address,
+            account=account
+        )
+
+        self.__costants = costants
+
+
+    # ---------------------------------------------------- Public methods ---------------------------------------------------- #
+
+    def name(self) -> str:
+        return self.__costants.name
+
+    def symbol(self) -> str:
+        return self.__costants.symbol
+
+    def decimals(self) -> int:
+        return self.__costants.decimals
+
+
+# -------------------------------------------------------------------------------------------------------------------------------- #
