9e09e10f3106874c9fd3a641d1c7126df635f16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package py4geo/1.0.4/py4geo-1.0.4.tar.gz

---
+++ packages/py4geo/py4geo-1.0.4.tar.gz/py4geo-1.0.4/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py4geo",
+    version="1.0.4",
+    author="Manuele Pesenti",
+    author_email="manuele@inventati.org",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/manuelep/py4vtile",
+    project_urls={
+        "Bug Tracker": "https://github.com/manuelep/py4vtile/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    package_data={"": ['*.sql']},
+    python_requires=">=3.6",
+    install_requires=[
+        "mptools",
+        "overpy",
+        "shapely",
+        "ujson",
+        "pyproj",
+        "mercantile",
+        "lxml",
+        "geojson",
+        "hashids",
+        "tqdm",
+        "geomet",
+        "h3",
+        "py4vtile",
+        "supermercado",
+        "osm2geojson"
+    ]
+)
