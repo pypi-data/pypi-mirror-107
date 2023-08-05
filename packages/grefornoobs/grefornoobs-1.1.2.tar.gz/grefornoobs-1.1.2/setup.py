a7519e3a5c711a54b7f5394e6c17c0a4d12ca303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:23 +0000

    Add package grefornoobs/1.1.2/grefornoobs-1.1.2.tar.gz

---
+++ packages/grefornoobs/grefornoobs-1.1.2.tar.gz/grefornoobs-1.1.2/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="grefornoobs",
+    version="1.1.2",
+    description="Your python GRE preparation tool",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abdussamiakanda/grefornoobs",
+    author="Md. Abdus Sami Akanda",
+    author_email="abdussamiakanda@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    py_modules=["gre"],
+    package_dir={'': 'src'},
+    install_requires=[],
+)
