c66e87632921afe46fde36d76a293254d7ce3292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:41 +0000

    Add package mathics-pygments/1.0.2/mathics_pygments-1.0.2-py2.py3-none-any.whl

---
+++ packages/mathics-pygments/mathics_pygments-1.0.2-py2.py3-none-any.whl/mathics_pygments/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016 rsmenon
+# Licensed under the MIT License (https://opensource.org/licenses/MIT)
+
+from mathics_pygments.lexer import MathematicaLexer
+from mathics_pygments.style import MathematicaNotebookStyle, MathematicaStyle
