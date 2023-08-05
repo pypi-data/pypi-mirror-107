e9875b97f2f7614c306c2f21261828516217ab7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:52 +0000

    Add package encodedecode/0.0.1/encodedecode-0.0.1.tar.gz

---
+++ packages/encodedecode/encodedecode-0.0.1.tar.gz/encodedecode-0.0.1/encodedecode/__init__.py
@@ -0,0 +1,24 @@
+def encode(string):
+    Encoded_string = ""
+    length_of_the_string = len(string)
+    if length_of_the_string > 1:
+        for variable in string:
+            ans = str(ord(variable)) + " " + \
+                str(length_of_the_string+ord(variable)) + "&"
+            Encoded_string += ans
+    else:
+        ans = str(ord(string)) + "&" + str(ord(string))
+        Encoded_string += ans
+    return Encoded_string
+
+
+def decode(string):
+    ans = string.split("&")
+    ans1 = ""
+    value = ""
+    ans.pop()
+    for ans in ans:
+        x = ans.split()
+        ans1 = chr(int(x[0]))
+        value += ans1
+    return value
