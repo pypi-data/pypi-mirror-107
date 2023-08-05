5774cf647ece4f256cf8c05b4b955bd9e41ab5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package nester-yq/1.0.0/nester_yq-1.0.0.tar.gz

---
+++ packages/nester-yq/nester_yq-1.0.0.tar.gz/nester_yq-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester_yq',
+    version='1.0.0',
+    py_modules=['nester_yq'],
+    author='JackChen9527',
+    author_email='21630566@qq.com',
+    url='http://www.qq.com',
+    description='A simple printer of nester list.',
+    )
