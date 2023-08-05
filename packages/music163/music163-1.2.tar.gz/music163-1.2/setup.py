d1aae5761f3ab45f110df64e84158675dc73df3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:21 +0000

    Add package music163/1.2/music163-1.2.tar.gz

---
+++ packages/music163/music163-1.2.tar.gz/music163-1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='music163',# 需要打包的名字,即本模块要发布的名字
+    version='v1.2',#版本
+    description='Download the 163 music for free!', # 简要描述
+    py_modules=['music163.music163'],   #  需要打包的模块
+    author='DY_XiaoDong', # 作者名
+    packages=['music163'],
+    author_email='xiaodong@indouyin.cn',   # 作者邮件
+    url='https://xiaodong.indouyin.cn/python/modules/music163/index.html', # 项目地址,一般是代码托管的网站
+    requires=['requests','pylucky','urllib3','requests','bs4'], # 依赖包,如果没有,可以不要
+    install_requires=['requests','pylucky','urllib3','requests','bs4'], # 强制下载的依赖包
+    license='MIT',
+)
