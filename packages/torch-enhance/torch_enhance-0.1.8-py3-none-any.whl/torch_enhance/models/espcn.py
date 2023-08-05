8720bd71b7d7ab1f1127f3e5b4f79ea556efc1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:15 +0000

    Add package torch-enhance/0.1.8/torch_enhance-0.1.8-py3-none-any.whl

---
+++ packages/torch-enhance/torch_enhance-0.1.8-py3-none-any.whl/torch_enhance/models/espcn.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+
+from .base import BaseModel
+
+
+class ESPCN(BaseModel):
+    """Efficient Sub-Pixel Convolutional Neural Network
+    https://arxiv.org/pdf/1609.05158v2.pdf
+
+    Parameters
+    ----------
+    scale_factor : int
+        Super-Resolution scale factor. Determines Low-Resolution downsampling.
+    channels: int
+        Number of input and output channels
+
+    """
+
+    def __init__(self, scale_factor: int, channels: int = 3):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=64,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=channels * scale_factor ** 2,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.PixelShuffle(scale_factor),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Super-resolve Low-Resolution input tensor
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input Low-Resolution image as tensor
+
+        Returns
+        -------
+        torch.Tensor
+            Super-Resolved image as tensor
+
+        """
+        return self.model(x)
---
+++ packages/torch-enhance/torch_enhance-0.1.8-py3-none-any.whl/torch_enhance/models/vdsr.py
@@ -0,0 +1,82 @@
+import torch.nn as nn
+
+from .base import BaseModel
+from .baseline import Bicubic
+
+
+class VDSR(BaseModel):
+    """Very Deep Super Resolution
+    https://arxiv.org/pdf/1511.04587.pdf
+
+    Parameters
+    ----------
+    scale_factor : int
+        Super-Resolution scale factor. Determines Low-Resolution downsampling.
+    channels: int
+        Number of input and output channels
+    num_layers: int
+        Number of stacked conv layers
+    """
+
+    def __init__(
+        self, scale_factor: int, channels: int = 3, num_layers: int = 20
+    ):
+        super().__init__()
+
+        self.upsample = Bicubic(scale_factor)
+
+        # Initial layer
+        layers = [
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.ReLU(),
+        ]
+
+        # Residual reconstruction
+        for i in range(num_layers - 2):
+            layers.append(
+                nn.Conv2d(
+                    in_channels=64,
+                    out_channels=64,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                )
+            )
+            layers.append(nn.ReLU())
+
+        # Output reconstruction layer
+        layers.append(
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        """Super-resolve Low-Resolution input tensor
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input Low-Resolution image as tensor
+
+        Returns
+        -------
+        torch.Tensor
+            Super-Resolved image as tensor
+
+        """
+        x = self.upsample(x)
+        x = x + self.model(x)
+        return x
