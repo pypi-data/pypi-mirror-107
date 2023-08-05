a0c2d9bb9a5de6bc9ea846ffcd1553e5c0f6bb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:48 +0000

    Add package fibery/0.3.0/fibery-0.3.0.tar.gz

---
+++ packages/fibery/fibery-0.3.0.tar.gz/fibery-0.3.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fibery', 'fibery.client', 'fibery.console', 'fibery.console.commands']
+
+package_data = \
+{'': ['*'], 'fibery.console.commands': ['js/*']}
+
+install_requires = \
+['aiohttp>=3.7.4,<4.0.0',
+ 'pydantic>=1.8.1,<2.0.0',
+ 'pyppeteer>=0.2.5,<0.3.0',
+ 'requests>=2.25.1,<3.0.0',
+ 'tabulate>=0.8.9,<0.9.0',
+ 'typer>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['fibery = fibery.console.main:main']}
+
+setup_kwargs = {
+    'name': 'fibery',
+    'version': '0.3.0',
+    'description': 'Python SDK for working with Fibery.io',
+    'long_description': None,
+    'author': 'Sergio Bershadsky',
+    'author_email': 'sergio.bershadsky@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/sergio-bershadsky/fibery',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
