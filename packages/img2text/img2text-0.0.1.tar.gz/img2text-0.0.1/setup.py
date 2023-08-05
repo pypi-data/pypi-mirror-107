644ec777b4905cf72f665aa8a7d8166a925e5645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:07 +0000

    Add package img2text/0.0.1/img2text-0.0.1.tar.gz

---
+++ packages/img2text/img2text-0.0.1.tar.gz/img2text-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().split('\n')
+
+setuptools.setup(
+    name="img2text",
+    version="0.0.1",
+    author="Dhia HMILA",
+    author_email="dhiahmila@gmail.com",
+    description="A lightweight module to generate colorful ascii art from images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/hmiladhia/img2text",
+    py_modules=['img2text'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': ['img2text=img2text:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    keywords=['ascii', 'art', 'ascii-art', 'image', 'text', 'color', 'colour'],
+)
