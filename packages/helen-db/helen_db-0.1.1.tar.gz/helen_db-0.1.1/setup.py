fd6d31830ac11b4147fe7c7208d791a53495e186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:52 +0000

    Add package helen-db/0.1.1/helen_db-0.1.1.tar.gz

---
+++ packages/helen-db/helen_db-0.1.1.tar.gz/helen_db-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['helen_db']
+setup_kwargs = {
+    'name': 'helen-db',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Andrew',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
