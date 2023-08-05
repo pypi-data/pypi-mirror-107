1db889f7e745d27c6f2e8e1af19df5b156b9c417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:51 +0000

    Add package coqui-stt-server/0.0.2/coqui_stt_server-0.0.2.tar.gz

---
+++ packages/coqui-stt-server/coqui_stt_server-0.0.2.tar.gz/coqui_stt_server-0.0.2/coqui_stt_server/__main__.py
@@ -0,0 +1,19 @@
+"""Entry point. Run the server and open a browser pointing to it."""
+import webbrowser
+from threading import Thread
+
+from .server import get_server_hostport, start_app
+
+
+def main():
+    thread = Thread(target=start_app, daemon=True)
+    thread.start()
+    host, port = get_server_hostport()
+    addr = f"http://{host}:{port}"
+    print(f"Started server listening on {addr}...")
+    webbrowser.open(addr)
+    thread.join()
+
+
+if __name__ == "__main__":
+    main()
