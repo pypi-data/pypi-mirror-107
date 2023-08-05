6126c57abaf35145c15f62e46aa7c034f98953e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:20 +0000

    Add package anyio-mqtt/0.1.1/anyio-mqtt-0.1.1.tar.gz

---
+++ packages/anyio-mqtt/anyio-mqtt-0.1.1.tar.gz/anyio-mqtt-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['anyio_mqtt']
+install_requires = \
+['anyio>=3.0.1,<4.0.0', 'paho-mqtt>=1.5.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'anyio-mqtt',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Ellis Percival',
+    'author_email': 'anyio-mqtt@failcode.co.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
