aaea88508361ce1fc2ce62026ca70f91ffafc723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:06 +0000

    Add package dorotheumypython/0.1.7/dorotheumypython-0.1.7-py3-none-any.whl

---
+++ packages/dorotheumypython/dorotheumypython-0.1.7-py3-none-any.whl/dorotheumypython/mymodule/util.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def show_data():
+    data = {
+        'Country': ['Belgium',  'India',  'Brazil'],
+        'Capital': ['Brussels',  'New Delhi',  'Brasilia'],
+        'Population': [11190846, 1303171035, 207847528]
+    }
+    df = pd.DataFrame(data, columns=['Country',  'Capital',  'Population'])
+
+    return df
---
+++ packages/dorotheumypython/dorotheumypython-0.1.7-py3-none-any.whl/dorotheumypython/wrapper.py
@@ -0,0 +1,8 @@
+from mymodule import util
+
+def show_data():
+    return util.show_data()
+
+if __name__ == '__main__':
+    val = show_data()
+    print(val)
