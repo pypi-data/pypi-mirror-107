040e80d1d122dfa42dc74f6810780930a0d33892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:44 +0000

    Add package kernel-driver/0.0.6/kernel_driver-0.0.6.tar.gz

---
+++ packages/kernel-driver/kernel_driver-0.0.6.tar.gz/kernel_driver-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="kernel_driver",
+    version="0.0.6",
+    url="https://github.com/davidbrochart/kernel_driver.git",
+    author="David Brochart",
+    author_email="david.brochart@gmail.com",
+    description="A Jupyter kernel driver",
+    packages=["kernel_driver"],
+    python_requires=">=3.7",
+    install_requires=[
+        "pyzmq",
+        "python-dateutil",
+        "rich",
+    ],
+    extras_require={
+        "test": [
+            "xeus-python",
+            "ipykernel",
+            "pytest",
+            "pytest-asyncio",
+            "mypy",
+            "flake8",
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
