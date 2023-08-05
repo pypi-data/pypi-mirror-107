e074dbb27d33b35b0651ee67d5dfd6ad2d002c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:02 +0000

    Add package projetodorotheu/0.1.0/projetodorotheu-0.1.0-py3-none-any.whl

---
+++ packages/projetodorotheu/projetodorotheu-0.1.0-py3-none-any.whl/projetodorotheu-0.1.0.data/scripts/cap_script.py
@@ -0,0 +1,13 @@
+#!python
+
+"""
+A really simple script just to demonstrate packaging
+"""
+
+import sys, os
+from projetodorotheu import capital_mod
+from projetodorotheu import main
+
+
+if __name__ == "__main__":
+    main.main()    
