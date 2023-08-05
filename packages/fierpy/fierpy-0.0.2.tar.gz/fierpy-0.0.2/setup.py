bc2f0e764c95b371ab76ae3e614f686c2d937006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:37 +0000

    Add package fierpy/0.0.2/fierpy-0.0.2.tar.gz

---
+++ packages/fierpy/fierpy-0.0.2.tar.gz/fierpy-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='fierpy',
+    version='0.0.2',
+    description='Python implementation of the Forecasting Inundation Extents using REOF method',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/servir/fierpy',
+    packages=setuptools.find_packages(),
+    author='Kel Markert',
+    author_email='kel.markert@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pandas',
+        'xarray',
+        'scikit-learn>=0.24',
+        'geoglows',
+        'eofs',
+        'netcdf4',
+        'pyqt'
+    ],
+    extras_require = {
+        'opensarlab':  [
+            'kernda',
+            'jupyter',
+
+        ]
+    }
+)
