cf9576372b44cca6f7cae424c4ac903dc5c2fc09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package pimodelmanager/0.4.5/PIModelManager-0.4.5-py3-none-any.whl

---
+++ packages/pimodelmanager/PIModelManager-0.4.5-py3-none-any.whl/PIModelManager/pandora_model.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+
+class PandoraModel:
+    """PandoraModel contains information about a machine learning model."""
+
+    language: str
+    name: str
+    client: str
+    version: str
+    unzip: bool
+    filename: str
+    path: Path
+
+    def __init__(
+        self,
+        type: str,
+        language: str,
+        size: str,
+        name: str,
+        client: str,
+        version: str,
+        path: Path,
+        filename: str = None
+    ):
+        self.language = language
+        self.type = type
+        self.size = size
+        self.name = name
+        self.client = client
+        self.version = version
+        #self.unzip = unzip
+        #self.filename = filename
+        self.path = path
+        self.filename = filename
+
+    def __eq__(self, other):
+        return (
+            self.type == other.type
+            and self.language == other.language
+            and self.size == other.size
+            and self.name == other.name
+            and self.client == other.client
+            and self.version == other.version
+            #and self.unzip == other.unzip
+            #and self.path == other.path
+        )
+
+    def __ne__(self, other):
+        return (
+            self.type != other.type
+            or self.language != other.language
+            or self.size != other.size
+            or self.name != other.name
+            or self.client != other.client
+            or self.version != other.version
+            #or self.unzip != other.unzip
+            #or self.filename != other.filename
+        )
