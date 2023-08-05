65cd49047b3291290fa0b8fd93d5f969c6dd562c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:53 +0000

    Add package ground/5.0.0/ground-5.0.0.tar.gz

---
+++ packages/ground/ground-5.0.0.tar.gz/ground-5.0.0/ground/core/centroidal/plain/multipoint.py
@@ -0,0 +1,19 @@
+from fractions import Fraction
+from typing import (Callable,
+                    Type)
+
+from ground.core.hints import (Multipoint,
+                               Point)
+
+
+def centroid(multipoint: Multipoint,
+             point_cls: Type[Point],
+             inverse: Callable[[int], Fraction] = Fraction(1).__truediv__
+             ) -> Point:
+    result_x = result_y = 0
+    for point in multipoint.points:
+        result_x += point.x
+        result_y += point.y
+    inverted_points_count = inverse(len(multipoint.points))
+    return point_cls(result_x * inverted_points_count,
+                     result_y * inverted_points_count)
