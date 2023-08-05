517f220b3d84c4441155ce74ae0d04005ce1e3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:06 +0000

    Add package joshlutils/0.0.7/joshlutils-0.0.7.tar.gz

---
+++ packages/joshlutils/joshlutils-0.0.7.tar.gz/joshlutils-0.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.7"
+URL = "https://github.com/JoshLoecker/utils"
+DOWNLOAD_URL = f"https://github.com/JoshLoecker/utils/archive/refs/tags/{VERSION}.tar.gz"
+
+with open("README.md", "r") as i_stream:
+    LONG_DESCRIPTION = i_stream.read()
+
+setup(
+    name='joshlutils',
+    packages=find_packages(),
+    version=VERSION,
+    install_requires=[
+        "keyring",
+        "imap_tools"
+    ],
+    description='A simple utility package for various tasks I commonly perform',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author='Josh Loecker',
+    author_email='joshloecker@icloud.com',
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    keywords=["Python", "Python 3", "Email"],
+    license="MIT",
+    classifiers=[],
+)
