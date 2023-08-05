d0ee7de9249bce8902f6bbcf7d9c4cc10a62962c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:16 +0000

    Add package mqtt-asyncio/0.1/mqtt_asyncio-0.1.tar.gz

---
+++ packages/mqtt-asyncio/mqtt_asyncio-0.1.tar.gz/mqtt_asyncio-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'mqtt_asyncio',
+    packages = ['mqtt_asyncio'],
+    version = '0.1',
+    description = '',
+    author = 'Hamid Rostami',
+    author_email = 'hamid.r1988@gmail.com',
+    url = 'https://github.com/hamid-rostami/mqtt-asyncio',
+    download_url = 'https://github.com/hamid-rostami/mqtt-asyncio/archive/refs/tags/0.1.tar.gz',
+    keywords = ['tag01'],
+    classifiers = [],
+)
