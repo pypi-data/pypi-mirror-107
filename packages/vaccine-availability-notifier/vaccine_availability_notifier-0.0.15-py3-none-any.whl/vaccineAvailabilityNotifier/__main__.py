6dbf872bb916f4e4b7bd890c6e760493c326994a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:38 +0000

    Add package vaccine-availability-notifier/0.0.15/vaccine_availability_notifier-0.0.15-py3-none-any.whl

---
+++ packages/vaccine-availability-notifier/vaccine_availability_notifier-0.0.15-py3-none-any.whl/vaccineAvailabilityNotifier/__main__.py
@@ -0,0 +1,9 @@
+from vaccineAvailabilityNotifier.commands import router
+
+
+def main():
+    router.safe_entry_point()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/vaccine-availability-notifier/vaccine_availability_notifier-0.0.15-py3-none-any.whl/vaccineAvailabilityNotifier/client/actionsImpl.py
@@ -0,0 +1,40 @@
+import time
+
+import requests
+
+from vaccineAvailabilityNotifier.client.actions import Actions
+
+
+def get_headers(params=None):
+    # 'accept': 'application/json',
+    # 'Accept-Language': 'hi_IN'
+    return {
+        "user-agent": "Mozilla/5.0 (Linux; Android 9; Redmi Note 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36",
+        'Accept-Language': 'hi_IN',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
+    }
+
+
+class ActionsImpl(Actions):
+
+    def get(self, url, params={}):
+        r = None
+        try:
+            r = requests.get(url, {}, headers=get_headers())
+        except Exception as e:
+            # if there is any exception print that and wait for 100 sec
+            print(e)
+            time.sleep(10)
+        return r
+
+    def create(self, uri, payload):
+        r = None
+        try:
+            url = build_url()
+            r = requests.post(url, data=payload, headers=get_headers())
+        except requests.exceptions.RequestException as e:
+            exit()
+        return r
+
+    def delete(self, uri, payload=None):
+        return self.create(uri, payload)
