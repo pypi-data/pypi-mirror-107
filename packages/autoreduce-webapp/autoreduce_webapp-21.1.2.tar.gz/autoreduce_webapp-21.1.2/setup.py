7069c9032ca74caacf9fdb3819e7b388e25d6e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:26 +0000

    Add package autoreduce-webapp/21.1.2/autoreduce_webapp-21.1.2.tar.gz

---
+++ packages/autoreduce-webapp/autoreduce_webapp-21.1.2.tar.gz/autoreduce_webapp-21.1.2/setup.py
@@ -0,0 +1,21 @@
+# pylint:skip-file
+"""
+Wrapper for the functionality for various installation and project setup commands
+see:
+    `python setup.py help`
+for more details
+"""
+from setuptools import setup, find_packages
+
+setup(name="autoreduce_webapp",
+      version="21.1.2",
+      description="The frontend of the ISIS Autoreduction service",
+      author="ISIS Autoreduction Team",
+      url="https://github.com/ISISScientificComputing/autoreduce-frontend/",
+      install_requires=[
+          "autoreduce_utils==0.1.3", "autoreduce_db==0.1.4", "autoreduce_qp==21.1.3", "Django==3.2.2",
+          "django_extensions==3.1.3", "django-user-agents==0.4.0"
+      ],
+      packages=find_packages(),
+      package_data={"": ["templates/**/*.*", "static/**/*.*"]},
+      entry_points={"console_scripts": ["autoreduce-webapp-manage = autoreduce_webapp.manage:main"]})
