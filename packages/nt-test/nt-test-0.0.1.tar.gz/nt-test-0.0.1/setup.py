cb13aad1d3ba27c4171668909c97deb4d2140033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:22 +0000

    Add package nt-test/0.0.1/nt-test-0.0.1.tar.gz

---
+++ packages/nt-test/nt-test-0.0.1.tar.gz/nt-test-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="nt-test",  # Replace with your own username
+    version="0.0.1",
+    author="newstower",
+    author_email="kj2945@gmail.com",
+    description="newstower",
+    # long_description='',
+    # long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    # packages={'nt-crawler'},
+    # package_dir={'': 'nt-crawler/src'},
+    packages=setuptools.find_packages(),
+    # scripts=['nt.py'],
+    entry_points={
+        'console_scripts': ['nt-run=nt_crawler.src.nt:main']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
