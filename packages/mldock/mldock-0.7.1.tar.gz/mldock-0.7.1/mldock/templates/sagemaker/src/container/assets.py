21a5f6132d3fe7eaa652efa2fecabadee83b7ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:20 +0000

    Add package mldock/0.7.1/mldock-0.7.1.tar.gz

---
+++ packages/mldock/mldock-0.7.1.tar.gz/mldock-0.7.1/mldock/templates/sagemaker/src/container/assets.py
@@ -0,0 +1,34 @@
+import logging
+from mldock.platform_helpers.mldock.configuration.environment.sagemaker import SagemakerEnvironment
+from mldock.platform_helpers.mldock.configuration.container import \
+    BaseTrainingContainer, BaseServingContainer
+
+# Set base logger to INFO
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('mldock')
+
+# Instantiate Environment here
+# import to other scripts
+environment = SagemakerEnvironment()
+
+# (TODO)
+# Assess situations and likelihood of whether users should by
+# default create their own or can it be hidden until needed.
+# For example, using just the BaseServingContainer by default
+# USE-CASES:
+#   - special Metric loggers (cleaning up, uploading to separate s3 locatations, etc)
+#   - Environment and importing from here to other scripts
+class TrainingContainer(BaseTrainingContainer):
+    """Implements the base training container, allow a user to override/add/extend any training container setup logic"""
+    pass
+
+# (TODO)
+# Assess situations and likelihood of whether users should by
+# default create their own or can it be hidden until needed.
+# For example, using just the BaseServingContainer by default
+# USE-CASES:
+#   - special Metric loggers (cleaning up, uploading to separate s3 locatations, etc)
+#   - Instantiating Environment and importing from here to other scripts
+class ServingContainer(BaseServingContainer):
+    """Implements the base serving container, allow a user to override/add/extend any serving container setup logic"""
+    pass
