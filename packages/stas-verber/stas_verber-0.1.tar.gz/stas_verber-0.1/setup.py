98935fa32659a28ff3bd85b53fc5175c6e27900c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:19 +0000

    Add package stas-verber/0.1/stas_verber-0.1.tar.gz

---
+++ packages/stas-verber/stas_verber-0.1.tar.gz/stas_verber-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stas_verber',
+    version='0.1',
+    author='Stas Verber',
+    author_email='vrnvstanislav@yandex.ru',
+    description='This  is my first package',
+    long_description=open('README.md').read(),
+    packages=find_packages()
+)
