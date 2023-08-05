cf9d457ef4ca6832440eae59a5db8498212914ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package detorch/0.2.0/detorch-0.2.0-py3.9.egg

---
+++ packages/detorch/detorch-0.2.0-py3.9.egg/detorch/utils.py
@@ -0,0 +1,31 @@
+import math
+from functools import wraps
+
+
+def hook(func):
+    func.before_register = []
+    func.after_register = []
+    def add_hook(after=True):
+        def _add_hook(hook):
+            if after:
+                func.after_register.append(hook)
+            else:
+                func.before_register.append(hook)
+        return _add_hook
+
+    func.add_hook = add_hook
+
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        for hook in func.before_register:
+            hook(*args, **kwargs)
+        ret = func(*args, **kwargs)
+        for hook in func.after_register:
+            hook(*args, **kwargs, ret=ret)
+        return ret
+
+    return wrapped
+
+
+def calculate_cr(size):
+    return math.pow(math.e, -0.5) / (math.log(size) * math.sqrt(2 * math.pi))
