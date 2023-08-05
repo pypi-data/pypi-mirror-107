7be82b24a8740702c6b884ca48e2135911b700a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:19 +0000

    Add package replforks/1.0.0/replforks-1.0.0.tar.gz

---
+++ packages/replforks/replforks-1.0.0.tar.gz/replforks-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='replforks',
+    version='1.0.0',
+    description='A way to detect forks in python.',
+    long_description='I will add.',
+    packages=["replforks"]
+  )
+
