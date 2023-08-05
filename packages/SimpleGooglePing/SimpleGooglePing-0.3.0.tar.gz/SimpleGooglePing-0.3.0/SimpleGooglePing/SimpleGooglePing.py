8e3395e719035d85a9a7832ae681143750863c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:02 +0000

    Add package SimpleGooglePing/0.3.0/SimpleGooglePing-0.3.0.tar.gz

---
+++ packages/SimpleGooglePing/SimpleGooglePing-0.3.0.tar.gz/SimpleGooglePing-0.3.0/SimpleGooglePing/SimpleGooglePing.py
@@ -0,0 +1,15 @@
+import os
+print("HI. I WILL JAB YOUR FACE.")
+os.system("ping www.google.com")
+print("OK")
+
+
+def Ping():
+  import os
+  print("HI. I WILL JAB YOUR FACE.")
+  os.system("ping www.google.com")
+  print("OK")
+
+
+
+
