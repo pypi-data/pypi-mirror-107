eb3afd3054f461e584995957204d3b1d8e67932c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:31 +0000

    Add package FileHandler/1.0.2/FileHandler-1.0.2.tar.gz

---
+++ packages/FileHandler/FileHandler-1.0.2.tar.gz/FileHandler-1.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='FileHandler',
+    version='v1.0.2',
+    url='',
+    license='',
+    author='chienaeae',
+    author_email='chienaeae@gmail.com',
+    description='file processor package',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src.FileHandler"),
+    python_requires=">=3.6",
+    install_requires=["requests==2.25.1"],
+)
