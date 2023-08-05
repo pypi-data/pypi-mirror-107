71b08378328dc6fcd09917c914f82987fd08bae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:10 +0000

    Add package toppics/0.1.0/toppics-0.1.0-py3-none-any.whl

---
+++ packages/toppics/toppics-0.1.0-py3-none-any.whl/toppics/constants.py
@@ -0,0 +1,21 @@
+import re
+from typing import Dict, List, Pattern
+
+# Topic lists
+PYTHON_CLI_TOPICS: List[str] = ["python", "cli"]
+
+TOPICS: Dict[str, List[str]] = {"Python CLI": PYTHON_CLI_TOPICS}
+
+# GitHub
+# More info:
+# - https://github.com/FriendCode/giturlparse.py
+# - https://github.com/nephila/giturlparse
+WEB_URL_PATTERN: Pattern[str] = re.compile(
+    r"https://github.com/(?P<owner>.+)/(?P<repo>.+)"
+)
+
+DEFAULT_ENV_VARIABLE: str = "GITHUB_TOKEN"
+
+# CLI
+TOKEN_HELP: str = "The GitHub personal access token."
+URL_ERROR_MSG: str = "it must be an HTTPS/web URL for a GitHub repo."
