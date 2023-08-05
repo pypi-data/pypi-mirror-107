1538c741b7c130013e7b8f6ed8accf50f2d1e9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:52 +0000

    Add package securex/0.0.1/securex-0.0.1.tar.gz

---
+++ packages/securex/securex-0.0.1.tar.gz/securex-0.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# coding: utf-8
+
+
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+
+    name='securex',
+
+    version='0.0.1',
+
+    author='Tector Pro',
+
+    description='Testing something',
+
+    packages=find_packages()
+
+)
