a73316316df14226f9f862cff91b6f46595043b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:58 +0000

    Add package cmx/0.0.16/cmx-0.0.16-py3-none-any.whl

---
+++ packages/cmx/cmx-0.0.16-py3-none-any.whl/cmx/__init__.py
@@ -0,0 +1,10 @@
+from functional_notations import F
+
+from cmx.backends.markdown import CommonMark
+from . import data
+
+doc = CommonMark()
+md = doc
+# todo: implement this
+csv = F @ doc.csv
+# todo: implement this
