5e80c77a7a3e4fb7e34e11652bf34ddd33338c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:32 +0000

    Add package feffery-antd-components/0.0.1a12/feffery_antd_components-0.0.1a12.tar.gz

---
+++ packages/feffery-antd-components/feffery_antd_components-0.0.1a12.tar.gz/feffery_antd_components-0.0.1a12/README.md
@@ -0,0 +1,20 @@
+# feffery-antd-components
+`Plotly Dash`第三方组件库，将`Antd`中的常用组件带入`Dash`的世界🥳，最新版本：`0.0.1a11`
+
+## 1 最新版本安装方式
+
+```bash
+pip install feffery-antd-components==0.0.1a11
+```
+
+## 2 已实现部件目录
+
+- 日期选择框：`AntdDatePicker`
+- 分割线：`AntdDivider`
+- 按钮：`AntdButton`
+- 下拉选择：`AntdSelect`
+- 树形控件：`AntdTree`
+
+## 3 说明文档
+
+[在线文档地址](http://121.40.46.113:8050/feffery-antd-docs/index)
---
+++ packages/feffery-antd-components/feffery_antd_components-0.0.1a12.tar.gz/feffery_antd_components-0.0.1a12/feffery_antd_components/_imports_.py
@@ -0,0 +1,17 @@
+from .AntdButton import AntdButton
+from .AntdDatePicker import AntdDatePicker
+from .AntdDateRangePicker import AntdDateRangePicker
+from .AntdDivider import AntdDivider
+from .AntdSelect import AntdSelect
+from .AntdTable import AntdTable
+from .AntdTree import AntdTree
+
+__all__ = [
+    "AntdButton",
+    "AntdDatePicker",
+    "AntdDateRangePicker",
+    "AntdDivider",
+    "AntdSelect",
+    "AntdTable",
+    "AntdTree"
+]
