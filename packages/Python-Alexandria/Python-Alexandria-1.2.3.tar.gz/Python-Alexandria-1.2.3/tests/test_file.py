8423faf61b4bccbbff59cfaacf948e7d24871e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:43 +0000

    Add package Python-Alexandria/1.2.3/Python-Alexandria-1.2.3.tar.gz

---
+++ packages/Python-Alexandria/Python-Alexandria-1.2.3.tar.gz/Python-Alexandria-1.2.3/tests/test_file.py
@@ -0,0 +1,15 @@
+import unittest
+
+from Alexandria.file import file_management
+
+
+class Tests():
+
+    def file_management(self):
+        print(file_management.find_file("txt"))
+
+    def file_methods(self):
+        pass
+
+    def parsers(self):
+        pass
