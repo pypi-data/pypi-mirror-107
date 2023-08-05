044d7a3c1227abd78f6cc5feda775bcbe86109cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:59 +0000

    Add package AgroClient/1.0.5/AgroClient-1.0.5.tar.gz

---
+++ packages/AgroClient/AgroClient-1.0.5.tar.gz/AgroClient-1.0.5/agrothon_client/__main__.py
@@ -0,0 +1,13 @@
+from .utils import *
+import multiprocessing
+
+def main():
+    pool = multiprocessing.Pool()
+    sen_result = pool.apply_async(serial_sensor_in)
+    pump_result = pool.apply_async(pump_status)
+    multiprocessing.Process(motion_intruder_detect(), daemon=True).start()
+    sen_result.wait()
+    pump_result.wait()
+
+if __name__ == '__main__':
+    main()
