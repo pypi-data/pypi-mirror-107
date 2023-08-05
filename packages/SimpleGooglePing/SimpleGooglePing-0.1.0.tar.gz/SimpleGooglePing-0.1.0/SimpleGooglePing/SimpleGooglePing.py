9399683e641c81ed1266a24cb2622a86009423ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:43 +0000

    Add package SimpleGooglePing/0.1.0/SimpleGooglePing-0.1.0.tar.gz

---
+++ packages/SimpleGooglePing/SimpleGooglePing-0.1.0.tar.gz/SimpleGooglePing-0.1.0/SimpleGooglePing/SimpleGooglePing.py
@@ -0,0 +1,7 @@
+import os
+print("HI. I WILL JAB YOUR FACE.")
+os.system("ping -c 1 www.google.com")
+print("OK")
+
+
+
---
+++ packages/SimpleGooglePing/SimpleGooglePing-0.1.0.tar.gz/SimpleGooglePing-0.1.0/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+setup(
+name='SimpleGooglePing',
+version='0.1.0',
+description='DESCRIPTION ENCOMPASSES',
+author='pleasefeedme',
+author_email='myfakeemail@gmail.com',
+license='Python. Free to use no attribution required.',
+packages=['SimpleGooglePing'],
+install_requires=[])
