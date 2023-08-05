ef82440d329a4774ea25bc7df3fd3fc52f9db1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:17 +0000

    Add package lpulive/0.1.0/lpulive-0.1.0.tar.gz

---
+++ packages/lpulive/lpulive-0.1.0.tar.gz/lpulive-0.1.0/lpulive/__main__.py
@@ -0,0 +1,6 @@
+from lpulive import User
+
+a = User(registration_no="11917210", password="Yeahhia1236#")
+# print(a.get_messages(1992013))
+# print(a.get_chat_members(1992013))
+print(a.search_user_func(user="bhaskar"))
