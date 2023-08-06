2955b6d5a8123173467ffa345c80db3bf9053b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:38 +0000

    Add package pin-and-string/0.0.1/pin-and-string-0.0.1.tar.gz

---
+++ packages/pin-and-string/pin-and-string-0.0.1.tar.gz/pin-and-string-0.0.1/README.md
@@ -0,0 +1,3 @@
+# pin-and-string: A python module to evaluate and manupilate shapes that can be built by pins and string.
+
+*pin-and-string* is a **Python** package that provides functions and built-in matrices for constructing and manupilating shapes that can be built by pins and string.
---
+++ packages/pin-and-string/pin-and-string-0.0.1.tar.gz/pin-and-string-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+setuptools.setup()
+
+## Can be used instead of static setup.cfg file. Optional.
+
+#import setuptools
+
+#with open("README.md", "r", encoding="utf-8") as fh:
+#    long_description = fh.read()
+
+#setuptools.setup(
+#    name="pin-and-string",
+#    version="0.0.1",
+#    author="Özgür Deniz Akyol",
+#    author_email="dakyol@pm.me",
+#    description="A python module to evaluate and manupilate shapes that can be built by pins and string.",
+#    long_description=long_description,
+#    long_description_content_type="text/markdown",
+#    url="https://github.com/dakyol/pin-and-string",
+#    project_urls={
+#        "Bug Tracker": "https://github.com/dakyol/pin-and-string/issues",
+#    },
+#    classifiers=[
+#        "Programming Language :: Python :: 3",
+#        "License :: OSI Approved :: MIT License",
+#        "Operating System :: OS Independent",
+#    ],
+#    package_dir={"": "src"},
+#    packages=setuptools.find_packages(where="src"),
+#    python_requires=">=3.6",
+#)
