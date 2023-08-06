61b084cb314f2625bf1927dba34d223336762c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:35 +0000

    Add package dpixels/0.0.7/dpixels-0.0.7.tar.gz

---
+++ packages/dpixels/dpixels-0.0.7.tar.gz/dpixels-0.0.7/dpixels/__init__.py
@@ -0,0 +1,17 @@
+from .autodraw import AutoDraw
+from .canvas import Canvas
+from .client import Client
+from .color import Color
+from .exceptions import *  # NOQA
+from .ratelimits import RateLimitedEndpoint, Ratelimits
+
+__version__ = "0.0.7"
+
+__all__ = [
+    "Canvas",
+    "Client",
+    "Color",
+    "Ratelimits",
+    "RateLimitedEndpoint",
+    "AutoDraw",
+]
