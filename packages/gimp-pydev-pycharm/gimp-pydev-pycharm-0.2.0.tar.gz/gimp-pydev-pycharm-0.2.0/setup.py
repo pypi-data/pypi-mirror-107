ddfd1fa25ab3b51ff74dcbf1ec538f6cfe8fd236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:22 +0000

    Add package gimp-pydev-pycharm/0.2.0/gimp-pydev-pycharm-0.2.0.tar.gz

---
+++ packages/gimp-pydev-pycharm/gimp-pydev-pycharm-0.2.0.tar.gz/gimp-pydev-pycharm-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from setuptools import setup
+from setuptools import find_packages
+
+requirements = Path("requirements.txt").read_text().split("\n")
+
+setup(
+    name='gimp-pydev-pycharm',
+    version='0.2.0',
+    packages=find_packages(),
+    url='https://github.com/isman7/gimp-python-development/',
+    license='GNU GPLv3',
+    author='ibenito',
+    author_email='ismaelbenito@protonmail.com',
+    description='A PyDev for PyCharm debugging inside the GIMP python environment',
+    install_requires=requirements,
+    package_data={"gimp.plugins.pydev": ["pydev"]}
+)
