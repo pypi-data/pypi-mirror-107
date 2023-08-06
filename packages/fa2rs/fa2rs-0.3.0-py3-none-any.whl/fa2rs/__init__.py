15010c5907143670d440758ac9101e8c9d2d4887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:19 +0000

    Add package fa2rs/0.3.0/fa2rs-0.3.0-py3-none-any.whl

---
+++ packages/fa2rs/fa2rs-0.3.0-py3-none-any.whl/fa2rs/__init__.py
@@ -0,0 +1,14 @@
+from fa2rs.libfa2rs import *
+
+__all__ = ["__title__", "__summary__", "__uri__", "__version__", "__author__", "__email__", "__license__", "__copyright__"]
+
+__author__ = "Pascal Engélibert <tuxmain@zettascript.org>"
+__copyright__ = "CopyLeft 2020-2021 {0}".format(__author__)
+__license__ = "AGPL 3.0"
+__summary__ = "Bindings to full Rust ForceAtlas2 implementation"
+__title__ = "ForceAtlas2-rs"
+__uri__ = "https://framagit.org/ZettaScript/fa2rs-py"
+__version__ = "0.3.0"
+
+Settings = Settings32
+Layout = Layout32
