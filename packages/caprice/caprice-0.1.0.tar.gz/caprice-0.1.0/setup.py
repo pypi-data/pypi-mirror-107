8a6da02227d5d9c3f79ec31ee0bfa204f8cab14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:14 +0000

    Add package caprice/0.1.0/caprice-0.1.0.tar.gz

---
+++ packages/caprice/caprice-0.1.0.tar.gz/caprice-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='caprice',
+    packages=find_packages(include=['caprice']),
+    version='0.1.0',
+    description='Robust PDF creation library for Python',
+    author='Aaron Elkins',
+    license='GPLv2.0',
+)
