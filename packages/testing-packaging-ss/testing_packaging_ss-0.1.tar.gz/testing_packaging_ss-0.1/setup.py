9370f5cf0a8fb325d12282278676990164c2483e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:07 +0000

    Add package testing-packaging-ss/0.1/testing_packaging_ss-0.1.tar.gz

---
+++ packages/testing-packaging-ss/testing_packaging_ss-0.1.tar.gz/testing_packaging_ss-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='testing_packaging_ss',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/Seemant-tiger/testing_packaging',
+      author='Seemant Singh',
+      author_email='seemant.singh@tigeranalytics.com',
+      license='MIT',
+      packages=['testing_packaging_ss'],
+      zip_safe=False)
