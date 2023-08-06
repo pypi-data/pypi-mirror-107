ba978441a4d92923b1024e2621e914e982a58d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:57 +0000

    Add package eeyore-nlp/0.0.9/eeyore-nlp-0.0.9.tar.gz

---
+++ packages/eeyore-nlp/eeyore-nlp-0.0.9.tar.gz/eeyore-nlp-0.0.9/src/eeyore_nlp/taggers/__init__.py
@@ -0,0 +1,8 @@
+from .abs import TokenTagger, \
+                 TextChunker, \
+                 ContextChunker
+from .mappers import TagMapper, \
+                     IobMapper
+from .chunkers import PhraseChunker, \
+                      PosChunker
+from .scopers import Scoper
