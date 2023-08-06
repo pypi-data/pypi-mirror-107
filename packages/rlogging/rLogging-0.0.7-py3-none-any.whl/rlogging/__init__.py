7a94bd2e74d29e2903487ac74dea5e7da1b7b87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:16 +0000

    Add package rlogging/0.0.7/rLogging-0.0.7-py3-none-any.whl

---
+++ packages/rlogging/rLogging-0.0.7-py3-none-any.whl/rlogging/__init__.py
@@ -0,0 +1,8 @@
+""" Модуль гибкого логирования """
+
+from rlogging import handlers, loggers, printers, service
+from rlogging.main import (get_logger, registration_logger, start_loggers,
+                           stop_loggers)
+
+# alpha release
+__version__ = '0, 0, 4'
