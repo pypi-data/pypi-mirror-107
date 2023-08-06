4b94f3f1680c8f44faee2703849f20256c98f15d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:52 +0000

    Add package Raghav-Chhabra-Proprietary/0.1/Raghav_Chhabra_Proprietary-0.1.tar.gz

---
+++ packages/Raghav-Chhabra-Proprietary/Raghav_Chhabra_Proprietary-0.1.tar.gz/Raghav_Chhabra_Proprietary-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Raghav_Chhabra_Proprietary',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = "Shagoon Malhotra",
+      author_email= "shagoon2895@gmail.com",
+      zip_safe=False)
