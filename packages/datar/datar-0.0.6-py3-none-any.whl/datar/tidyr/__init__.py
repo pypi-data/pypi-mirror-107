adbfe0de3ebffaf3089f80113323862b031f4d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:41 +0000

    Add package datar/0.0.6/datar-0.0.6-py3-none-any.whl

---
+++ packages/datar/datar-0.0.6-py3-none-any.whl/datar/tidyr/__init__.py
@@ -0,0 +1,8 @@
+"""APIs ported from R-tidyr"""
+
+from .funcs import full_seq, nesting
+from .verbs import (
+    pivot_longer, pivot_wider, uncount, replace_na, fill,
+    expand_grid, extract, separate, separate_rows, unite,
+    drop_na, expand
+)
