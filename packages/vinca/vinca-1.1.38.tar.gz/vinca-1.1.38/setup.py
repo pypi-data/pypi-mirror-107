03f03e4aa8134aec0c305d3c500775f01c7784a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:11 +0000

    Add package vinca/1.1.38/vinca-1.1.38.tar.gz

---
+++ packages/vinca/vinca-1.1.38.tar.gz/vinca-1.1.38/setup.py
@@ -0,0 +1,28 @@
+import codecs
+import setuptools
+
+
+setuptools.setup(
+    name="vinca",
+    version="1.1.38",
+    author="Oscar Laird", # your name
+    author_email="olaird25@gmail.com", # your email
+    description="A simple spaced repetition system",
+    long_description=codecs.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="MIT", # license type
+    include_package_data = True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5', # python version
+    install_requires=[ # dependecies package
+        'readchar',
+    ],
+    entry_points = {
+        'console_scripts': ['vinca=vinca.run:main']
+    },
+)
