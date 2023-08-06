867027628b3110f5941098cc83ea58d3911c4845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:45 +0000

    Add package pykcalc/0.1/pykcalc-0.1.tar.gz

---
+++ packages/pykcalc/pykcalc-0.1.tar.gz/pykcalc-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="pykcalc",
+version="0.1",
+description="This is a calculator package",
+long_description = "",
+author="krishna jain",
+packages=['pykcalc'],
+install_requires=[])
