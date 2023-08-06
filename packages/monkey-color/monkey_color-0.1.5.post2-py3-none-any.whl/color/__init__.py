b1ab62df7bbd3cc67dd79ac45a5a8aa5a8b05ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:52 +0000

    Add package monkey-color/0.1.5.post2/monkey_color-0.1.5.post2-py3-none-any.whl

---
+++ packages/monkey-color/monkey_color-0.1.5.post2-py3-none-any.whl/color/__init__.py
@@ -0,0 +1,12 @@
+
+# Author: 猴子
+# Date: 2021-05-23 15:10:22
+# LastEditTime: 2021-05-27 14:32:17
+# FilePath: /code/color/__init__.py
+
+from .color import *
+from .get import *
+
+__copyright__ = "Copyright © 2021 monkey"
+__author__ = "猴子#3807"
+__version__ = "0.1.5-2"
