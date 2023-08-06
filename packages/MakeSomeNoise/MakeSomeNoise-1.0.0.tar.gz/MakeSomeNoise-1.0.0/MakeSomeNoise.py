2bea85c8df367626c7539057c0951dbc5a2e3122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:34 +0000

    Add package MakeSomeNoise/1.0.0/MakeSomeNoise-1.0.0.tar.gz

---
+++ packages/MakeSomeNoise/MakeSomeNoise-1.0.0.tar.gz/MakeSomeNoise-1.0.0/MakeSomeNoise.py
@@ -0,0 +1,5 @@
+def MakeSomeNoise():
+    print("AhAhAh...")
+
+MakeSomeNoise()
+"""fjdkaslf jkldasf jkldasfj"""
---
+++ packages/MakeSomeNoise/MakeSomeNoise-1.0.0.tar.gz/MakeSomeNoise-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'MakeSomeNoise',
+    version = '1.0.0',
+    py_modules = ['MakeSomeNoise'],
+    author = 'Jiang',
+    author_email = '1773947293@qq.com',
+    url = 'http://www.baidu.com',
+    description = 'Yeah',
+    )
