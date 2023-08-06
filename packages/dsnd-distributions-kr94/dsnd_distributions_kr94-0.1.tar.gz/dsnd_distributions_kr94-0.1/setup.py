5e14df3b65dd6b9c38717c8c4637e5518c9c13d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:49 +0000

    Add package dsnd-distributions-kr94/0.1/dsnd_distributions_kr94-0.1.tar.gz

---
+++ packages/dsnd-distributions-kr94/dsnd_distributions_kr94-0.1.tar.gz/dsnd_distributions_kr94-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_kr94',
+      version='0.1',
+      description='Gaussian distributions',
+      author = "Krishnamoorthy Ramanath",
+      zip_safe=False)
