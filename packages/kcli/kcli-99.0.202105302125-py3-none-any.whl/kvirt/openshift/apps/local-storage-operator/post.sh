f86b0d7d9c2d8aa24ba81100ba1016d96fdd0085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:48 +0000

    Add package kcli/99.0.202105302125/kcli-99.0.202105302125-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202105302125-py3-none-any.whl/kvirt/openshift/apps/local-storage-operator/post.sh
@@ -0,0 +1,11 @@
+while true; do
+  oc get sc/{{ localstorage_storageclass }} 2>/dev/null && break;
+  echo "Waiting for the storageclass to be created"
+  sleep 5
+done
+{% if localstorage_default_storageclass %}
+ oc patch storageclass {{ localstorage_storageclass }} -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+{% else %}
+echo Run the following command to make localstorage the default storage class
+echo oc patch storageclass {{ localstorage_storageclass }} -p \'{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}\'
+{% endif %}
