569d489b4537820a38c58663596c70970398edf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:51 +0000

    Add package venture/1.1.0/venture-1.1.0-py3-none-any.whl

---
+++ packages/venture/venture-1.1.0-py3-none-any.whl/venture/icons.py
@@ -0,0 +1,34 @@
+from typing import Dict, Optional
+
+# Uses Nerd Fonts Icons
+__icons: Dict[str, str] = {
+    "directory": "",
+    "default": "",
+    "py": "",
+    "rb": "",
+    "cr": "",
+    "rs": "",
+    "js": "",
+    "c": "ﭰ",
+    "cpp": "ﭱ",
+    "cs": "",
+    "css": "",
+    "go": "ﳑ",
+    "html": "",
+    "php": "",
+    "r": "ﳒ",
+    "swift": "ﯣ",
+    "ts": "ﯤ",
+    "json": "ﬥ ",
+    "zip": "遲",
+    "fish": "",
+    "tsx": "",
+    "jsx": "",
+}
+
+
+def icon(name: str) -> Optional[str]:
+    """Get an icon associated with a filename,
+    returns the default if it doesn't exist"""
+    name = name.lstrip(".")
+    return __icons.get(name) or __icons.get("default")
