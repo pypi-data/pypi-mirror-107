8275571ab65b171169051134f5ef9fa7f3c8c771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:24 +0000

    Add package neurobiba/0.1/neurobiba-0.1.tar.gz

---
+++ packages/neurobiba/neurobiba-0.1.tar.gz/neurobiba-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='neurobiba',
+      version='0.1',
+      description='small collection of functions for neural networks',
+      packages=['neurobiba'],
+      author_email='cumnaamys@gmail.com',
+      zip_safe=False)
