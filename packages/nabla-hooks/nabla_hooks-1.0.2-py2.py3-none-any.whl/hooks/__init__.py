0eab545b01a4d31e12806c27cbb98e3fa78fd261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:00 +0000

    Add package nabla-hooks/1.0.2/nabla_hooks-1.0.2-py2.py3-none-any.whl

---
+++ packages/nabla-hooks/nabla_hooks-1.0.2-py2.py3-none-any.whl/hooks/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    This module allow to customize git hooks.
+"""
+# import hooks.get_jira  # noqa: F401
+
+name = 'nabla_hooks'
+
+__version__ = 'v1.0.2'
