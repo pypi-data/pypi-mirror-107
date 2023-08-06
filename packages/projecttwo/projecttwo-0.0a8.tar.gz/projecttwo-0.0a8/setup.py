302a44220ed9566e2590d37e1fb754a2637d9351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:13 +0000

    Add package projecttwo/0.0a8/projecttwo-0.0a8.tar.gz

---
+++ packages/projecttwo/projecttwo-0.0a8.tar.gz/projecttwo-0.0a8/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='projecttwo',
+    version='0.0a8',
+    packages=[''],
+    url='https://google.com',
+    author='ML',
+    author_email='',
+)
