7d898df56924f56b2aabd46985276fc52e2e3eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:07 +0000

    Add package AnyRobot/1.1.23/AnyRobot-1.1.23.tar.gz

---
+++ packages/AnyRobot/AnyRobot-1.1.23.tar.gz/AnyRobot-1.1.23/aishu/datafaker/profession/entity/CreateTestPort.py
@@ -0,0 +1,52 @@
+
+# !/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from aishu.datafaker.profession.entity.port import date
+from aishu.datafaker.profession.entity.ip import date
+import requests
+import time
+
+class Port(object):
+    def __init__(self):
+        pass
+
+    def httpport(self):
+        url = 'http://{ip}/etl/input'.format(ip=date.getTestHostIP())
+        http_port = date.getEtlPortNew()
+        body = {
+            "community":[],
+            "port":http_port,
+            "agentProtocol":"HTTP",
+            "protocol":"AR-Agent",
+            "ruleName":None,
+            "status":1,
+            "type":None,
+            "tagsID":[],
+            "tags":[],
+            "timezone":"Asia/Shanghai",
+            "charset":"UTF-8"
+        }
+        requests.post(url=url, json= body)
+        time.sleep(1)
+        print(http_port)
+        return http_port
+
+    def syslogPort(self):
+        url_syslog = 'http://{ip}/etl/input'.format(ip=date.getTestHostIP())
+        syslog_port = date.getEtlPortNew()
+        body_syslog = {
+            "community":[],
+            "port": syslog_port,
+            "protocol":"syslog",
+            "ruleName":None,
+            "status":1,
+            "tagsID":[],
+            "tags":[],
+            "timezone":"Asia/Shanghai",
+            "type":"syslog",
+            "charset":"UTF-8"
+        }
+        requests.post(url=url_syslog, json= body_syslog)
+        time.sleep(60)
+        print(syslog_port)
+        return syslog_port
