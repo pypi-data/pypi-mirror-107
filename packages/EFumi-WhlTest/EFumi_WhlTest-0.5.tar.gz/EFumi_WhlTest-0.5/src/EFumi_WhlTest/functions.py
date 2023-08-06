3dd06bf13ddf9258cc66ebeb5ac190e8a254a507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:31 +0000

    Add package EFumi-WhlTest/0.5/EFumi_WhlTest-0.5.tar.gz

---
+++ packages/EFumi-WhlTest/EFumi_WhlTest-0.5.tar.gz/EFumi_WhlTest-0.5/src/EFumi_WhlTest/functions.py
@@ -0,0 +1,19 @@
+import EFumi_WhlTest.variables as v
+
+
+def rotate(mtr):
+    x = v.rotM @ mtr
+    points = [[v.r * x[0][0], v.r * x[1][0]],
+              [v.r * x[0][1], v.r * x[1][1]],
+              [v.r * x[0][2], v.r * x[1][2]],
+              [v.r * x[0][3], v.r * x[1][3]]]
+    return x, points
+
+   
+def run():
+    all_points = []
+    for i in range(v.iterations):
+    # while loop:
+        v.p, point_list = f.rotate(v.p)
+        all_points.append(point_list)
+    return all_points
