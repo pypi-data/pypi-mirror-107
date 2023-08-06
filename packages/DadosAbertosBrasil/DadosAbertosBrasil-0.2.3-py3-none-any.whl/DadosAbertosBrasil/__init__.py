d7b138b4e60f83b8f91e918614f9a2246259fd5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:32 +0000

    Add package DadosAbertosBrasil/0.2.3/DadosAbertosBrasil-0.2.3-py3-none-any.whl

---
+++ packages/DadosAbertosBrasil/DadosAbertosBrasil-0.2.3-py3-none-any.whl/DadosAbertosBrasil/__init__.py
@@ -0,0 +1,58 @@
+'''
+Dados Abertos Brasil é uma iniciativa para facilitar o acesso a dados abertos
+e APIs do governo brasileiro.
+
+É um pacote open-source para Python e Pandas e a forma mais simples de acessar
+dados de instituições como IGBE, IPEA, etc.
+
+Módulos em Desenvolvimento
+--------------------------
+    - DadosAbertosBrasil.ibge
+    - DadosAbertosBrasil.ipea
+    - DadosAbertosBrasil.camara
+    - DadosAbertosBrasil.senado
+    - DadosAbertosBrasil.bacen
+    - DadosAbertosBrasil.favoritos
+
+Sobre
+-----
+    - Página Oficial: https://www.gustavofurtado.com/dab.html
+    - Documentação: https://www.gustavofurtado.com/DadosAbertosBrasil/index.html
+
+Instalação
+----------
+    - pip install DadosAbertosBrasil
+
+Dependências
+------------
+    - Python 3.6 ou superior
+    - pandas
+    - requests
+
+Licença
+-------
+    - MIT
+
+Próximos Passos
+---------------
+    - Conclusão do módulo `senado`;
+    - Desenvolvimento do módulo `transparencia` para coleta de dados do
+      Portal da Transparência do Governo Federal;
+    - Expandir módulo `bacen` com coleta de dados de expectativas de mercado;
+    - Adicionar novos exemplos à documentação;
+    - Adição constante de novas funções no módulo `favoritos`.
+'''
+
+
+
+__version__ = '0.2.3'
+__author__ = 'Gustavo Furtado da Silva'
+
+
+
+from . import bacen
+from . import camara
+from . import ibge
+from . import ipea
+from . import senado
+from .favoritos import *
