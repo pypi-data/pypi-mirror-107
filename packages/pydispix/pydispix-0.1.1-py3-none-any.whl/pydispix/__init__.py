ba8dcd31149bc2467679f7521e0d46d39f31e68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:59 +0000

    Add package pydispix/0.1.1/pydispix-0.1.1-py3-none-any.whl

---
+++ packages/pydispix/pydispix-0.1.1-py3-none-any.whl/pydispix/__init__.py
@@ -0,0 +1,7 @@
+from pydispix.client import Client  # noqa
+from pydispix.canvas import Canvas, Pixel  # noqa
+from pydispix.color import Color, Colour, parse_color, parse_colour  # noqa
+from pydispix.ratelimits import RateLimiter, RateLimitedEndpoint  # noqa
+from pydispix.log import setup_logging
+
+setup_logging()
