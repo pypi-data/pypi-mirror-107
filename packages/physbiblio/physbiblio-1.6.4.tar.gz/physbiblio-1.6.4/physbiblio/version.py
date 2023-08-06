1034c30bdc490b2458335790e78ff767c073f396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:16 +0000

    Add package physbiblio/1.6.4/physbiblio-1.6.4.tar.gz

---
+++ packages/physbiblio/physbiblio-1.6.4.tar.gz/physbiblio-1.6.4/physbiblio/version.py
@@ -0,0 +1,9 @@
+__version__ = "1.6.4"
+__version_date__ = "31/05/2021"
+
+__recent_changes__ = """<br>* fix errors in MacOS<br>
+* fix problem with multiple '@' in bibtex during import<br>
+* increase time separation between some INSPIRE queries<br>
+* small bug fixes and deprecation errors solved<br>
+* test fixes and updates<br>
+"""
