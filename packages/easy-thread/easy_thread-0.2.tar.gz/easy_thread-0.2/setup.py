7e4a96b43b9ac6e05d121851eef9a4aa9ba975ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:22 +0000

    Add package easy-thread/0.2/easy_thread-0.2.tar.gz

---
+++ packages/easy-thread/easy_thread-0.2.tar.gz/easy_thread-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='easy_thread',
+      version='0.2',
+      description='Module to make easy threads. ',
+      packages=['easy_thread'],
+      author_email='vanproskur@gmail.com',
+      zip_safe=False)
