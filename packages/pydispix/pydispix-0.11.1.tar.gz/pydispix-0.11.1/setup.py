165894ebfaba2e54d74a7a75ed2c01bb1ba28238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:11 +0000

    Add package pydispix/0.11.1/pydispix-0.11.1.tar.gz

---
+++ packages/pydispix/pydispix-0.11.1.tar.gz/pydispix-0.11.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pydispix']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.4,<0.5.0',
+ 'matplotlib>=3.4.2,<3.5.0',
+ 'pillow>=8.2.0,<8.3.0',
+ 'requests>=2.25.1,<2.26.0']
+
+setup_kwargs = {
+    'name': 'pydispix',
+    'version': '0.11.1',
+    'description': "API wrapper for python-discord's pixels.",
+    'long_description': None,
+    'author': 'ItsDrike',
+    'author_email': 'itsdrikeofficial@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.0,<3.9.0',
+}
+
+
+setup(**setup_kwargs)
