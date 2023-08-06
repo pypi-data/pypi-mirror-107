739b814a25287ae3967b399d98ce7d9475d32c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:43 +0000

    Add package hata/1.1.82/hata-1.1.82-py3-none-any.whl

---
+++ packages/hata/hata-1.1.82-py3-none-any.whl/hata/ext/slash/components.py
@@ -0,0 +1,4 @@
+__all__ = ('Button', 'ButtonStyle', 'Row', 'Select', 'Option')
+
+from ...discord.interaction import ComponentButton as Button, ComponentRow as Row, ComponentSelect as Select, \
+    ComponentSelectOption as Option, ButtonStyle
