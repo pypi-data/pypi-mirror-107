18a23ee1402ec94473d2183e3f5e3bcf9f20e633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:54 +0000

    Add package informathion/0.2.0/informathion-0.2.0.tar.gz

---
+++ packages/informathion/informathion-0.2.0.tar.gz/informathion-0.2.0/informathion/__init__.py
@@ -0,0 +1,1 @@
+from informathion.OpportunityLoss import OpportunityLoss
---
+++ packages/informathion/informathion-0.2.0.tar.gz/informathion-0.2.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['informathion']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.4.2,<4.0.0',
+ 'numpy>=1.20.3,<2.0.0',
+ 'pandas>=1.2.4,<2.0.0',
+ 'seaborn>=0.11.1,<0.12.0']
+
+setup_kwargs = {
+    'name': 'informathion',
+    'version': '0.2.0',
+    'description': 'Framework for Minimizing Opportunity Loss',
+    'long_description': 'Measurements for Expected Opportunity Loss\n------------------------------------------\n\nTo use (with caution), simply do::\n\n    >>> from informathion import ExpectedOpportunityLoss\n    >>> ExpectedOpportunityLoss(rewards, inputs)',
+    'author': 'migueltorrescosta',
+    'author_email': 'miguelptcosta1995@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/migueltorrescosta/informathion',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
