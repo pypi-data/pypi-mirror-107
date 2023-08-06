b52645fbe295c5ec648c51e76f4e84e66a7a0c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:38 +0000

    Add package allneo/5.0.0/allneo-5.0.0.tar.gz

---
+++ packages/allneo/allneo-5.0.0.tar.gz/allneo-5.0.0/allneo/count_operations.py
@@ -0,0 +1,16 @@
+from neo4j import GraphDatabase
+import sys
+def count_node(uri,user,pwd):
+    try:
+        driver=GraphDatabase.driver(uri=uri,auth=(user,pwd))
+        connect=1
+    except (ValueError,OSError,AuthError) as e:
+        sys.exit("check for hostname , username and password")
+    if(connect==1):
+        session=driver.session()
+        count_query="""match (n) return count(n) as count"""
+        count_result=session.run(count_query)
+        for count in count_result:
+            nodecount=count["count"]
+        return nodecount
+    
