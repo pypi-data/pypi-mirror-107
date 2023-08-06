c52033c5394829830096bc4e8cd87317ac44c784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:16 +0000

    Add package distributions-pete1-test/0.2/distributions_pete1_test-0.2.tar.gz

---
+++ packages/distributions-pete1-test/distributions_pete1_test-0.2.tar.gz/distributions_pete1_test-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_pete1_test',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Pete Tae',
+      email='pete.taecha@gmail.com',
+      zip_safe=False)
