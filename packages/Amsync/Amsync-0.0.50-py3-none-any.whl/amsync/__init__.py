23141ddd6b2d9191077caedaaa7ad26d42038204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:35 +0000

    Add package Amsync/0.0.50/Amsync-0.0.50-py3-none-any.whl

---
+++ packages/Amsync/Amsync-0.0.50-py3-none-any.whl/amsync/__init__.py
@@ -0,0 +1,12 @@
+import sys
+
+from .bot import *
+from .obj import *
+from .image import *
+from .dataclass import *
+from .exceptions import InvalidPythonVersion
+
+__version__ = '0.0.50'
+
+if sys.version_info < (3, 8):
+    raise InvalidPythonVersion(f"Your python {'.'.join(map(str, sys.version_info[:2]))}, use python >= 3.8")
