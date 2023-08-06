d4831c87b8443a5cec3d7f0ee9892c19633f9990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:26 +0000

    Add package fast-machine-learning/0.0.1.14/fast_machine_learning-0.0.1.14-py2.py3-none-any.whl

---
+++ packages/fast-machine-learning/fast_machine_learning-0.0.1.14-py2.py3-none-any.whl/fml/utils/common.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def linspace(start, end, step, dtype=float, decimal=None):
+    
+    data = []
+    while start < end:
+        start = dtype(start)
+        if dtype is float and decimal is not None:
+            start = round(start, decimal)
+        data.append(start)
+        start += step
+    
+    return data
+
+def split_range(_range, split_number):
+    
+    step = int(np.ceil(  len(list(_range)) / split_number  ))
+    
+    for i in range(0, len(_range), step):
+        yield _range[i:i+step]
+
+def split_array(_array, split_number):
+    
+    step = int(np.ceil(  _array.shape[0] / split_number  ))
+    
+    for i in range(0, _array.shape[0], step):
+        yield _array[i:i+step, :]
