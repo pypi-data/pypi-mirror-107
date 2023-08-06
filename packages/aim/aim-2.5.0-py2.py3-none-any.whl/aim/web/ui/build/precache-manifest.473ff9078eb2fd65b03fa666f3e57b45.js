943b15cd27533e9010de2491330c7ad76bd26383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:54 +0000

    Add package aim/2.5.0/aim-2.5.0-py2.py3-none-any.whl

---
+++ packages/aim/aim-2.5.0-py2.py3-none-any.whl/aim/web/ui/build/precache-manifest.473ff9078eb2fd65b03fa666f3e57b45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0deba647f0f4d363ee0670d20700e3",
+    "url": "/static-files/index.html"
+  },
+  {
+    "revision": "2cd57b56f5cd72fe89be",
+    "url": "/static-files/static/css/2.614aab5d.chunk.css"
+  },
+  {
+    "revision": "ffbdc9b3576b607c07db",
+    "url": "/static-files/static/css/main.73911414.chunk.css"
+  },
+  {
+    "revision": "2cd57b56f5cd72fe89be",
+    "url": "/static-files/static/js/2.e8a0e27d.chunk.js"
+  },
+  {
+    "revision": "ffbdc9b3576b607c07db",
+    "url": "/static-files/static/js/main.b21da2ec.chunk.js"
+  },
+  {
+    "revision": "8c4823a1cc9c2dd3bed4",
+    "url": "/static-files/static/js/runtime~main.991fcab5.js"
+  },
+  {
+    "revision": "e564e259c4f6dbb7295018e466b3af5b",
+    "url": "/static-files/static/media/logo-white.e564e259.png"
+  },
+  {
+    "revision": "d9bc6f2975d2f25135960b30aff5dad8",
+    "url": "/static-files/static/media/no_data.d9bc6f29.svg"
+  },
+  {
+    "revision": "7b44de7bc460e8e6b474abe60002a318",
+    "url": "/static-files/static/media/tensorflow-2.7b44de7b.svg"
+  }
+]);
