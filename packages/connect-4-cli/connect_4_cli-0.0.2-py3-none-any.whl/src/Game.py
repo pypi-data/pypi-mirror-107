5911fde8dda73e074f25d432a93b76cb821cda27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:42 +0000

    Add package connect-4-cli/0.0.2/connect_4_cli-0.0.2-py3-none-any.whl

---
+++ packages/connect-4-cli/connect_4_cli-0.0.2-py3-none-any.whl/src/Game.py
@@ -0,0 +1,38 @@
+from src.constants import ValidUserInput as UserInput
+from src.Match import Match
+
+
+class Game():
+    """Logic of connect4 game."""
+
+    def __init__(self) -> None:
+        """Constructor."""
+        self.match = Match()
+
+    def process_input(self, input):
+        """Process user keyboard input after it was read and identified."""
+        if input == UserInput.KEY_R:
+            self._reset_match()
+        elif self._is_match_ongoing():
+            self._process_input_match(input)
+
+    def get_game_board(self):
+        """Return game board as list of Column objects"""
+        return self.match.board.columns
+
+    def _reset_match(self):
+        """Create a new match in default state."""
+        self.match = Match()
+
+    def _is_match_ongoing(self):
+        """Return if match is ongoing or stopped (player won or they tied)."""
+        return self.match.is_being_played()
+
+    def _process_input_match(self, input):
+        """Process user input with ongoing match."""
+        switcher = {
+            UserInput.ARROW_LEFT: self.match.column_previous,
+            UserInput.ARROW_RIGHT: self.match.column_next,
+            UserInput.ENTER: self.match.add_checker
+        }
+        switcher.get(input, lambda: None)()
