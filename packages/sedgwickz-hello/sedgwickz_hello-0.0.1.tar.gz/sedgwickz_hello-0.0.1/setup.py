4c5da59d971e7a3b46b6ee4d79ae65158ad3b176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:48 +0000

    Add package sedgwickz-hello/0.0.1/sedgwickz_hello-0.0.1.tar.gz

---
+++ packages/sedgwickz-hello/sedgwickz_hello-0.0.1.tar.gz/sedgwickz_hello-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="sedgwickz_hello",
+    version="0.0.1",
+    scripts=["hello.py"],
+    author="sedgwickz",
+    author_email="kunnsh@gmail.com",
+    description="This is my first pypi package test",
+    keywords=["aliyun", "oss"],
+    url="https://github.com/sedgwickz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
