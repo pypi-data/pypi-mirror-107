87b0c1b2954c38de203ac2fe07936070e931b88d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:31 +0000

    Add package amxmaintenance/0.0.2/amxmaintenance-0.0.2.tar.gz

---
+++ packages/amxmaintenance/amxmaintenance-0.0.2.tar.gz/amxmaintenance-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amxmaintenance",
+    version="0.0.2",
+    author="Logan Vaughn",
+    # author_email="logantv@gmail.com",
+    description="amx telnet info, firmware checks, logs, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/logantv/amxmaintenance",
+    project_urls={
+        "Bug Tracker": "https://github.com/logantv/amxmaintenance/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+		'amxtelnet',
+		'amxlogs',
+		'exceltoamx',
+		'amxtoexcel',
+        'amxfirmware',
+	],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
