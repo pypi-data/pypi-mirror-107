d6cdb207cc1b3cd62327e32dffe7e8f088503130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:41 +0000

    Add package MYtt-PACKAGE/2.0/MYtt_PACKAGE-2.0.tar.gz

---
+++ packages/MYtt-PACKAGE/MYtt_PACKAGE-2.0.tar.gz/MYtt_PACKAGE-2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(name = "MYtt_PACKAGE",
+      version = "2.0",
+      description = "排序，渲染，自动列宽",
+      author = "tt",
+      py_modules = ["MYtt_PACKAGE.package_2_render",
+                    "MYtt_PACKAGE.package_3_render",
+                    "MYtt_PACKAGE.package_autowidth",
+                    "MYtt_PACKAGE.package_sort"])
+
+
