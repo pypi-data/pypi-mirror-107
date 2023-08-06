ef6789b0cb696d501192151424636f3e2c87f49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:35 +0000

    Add package distpy-nano/0.1/distpy_nano-0.1.tar.gz

---
+++ packages/distpy-nano/distpy_nano-0.1.tar.gz/distpy_nano-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distpy_nano',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distpy_nano'],
+      zip_safe=False)
