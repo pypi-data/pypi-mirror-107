fcad245b3f34147bd6d5d86a09d7cde2e39bcc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:35 +0000

    Add package daspuml-compiler/0.10/daspuml-compiler-0.10.tar.gz

---
+++ packages/daspuml-compiler/daspuml-compiler-0.10.tar.gz/daspuml-compiler-0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+authors = ["Dominik Bober", "Adam Klekowski", "Szymon Duda", "Przemysław Ziaja"]
+
+setup(
+    name='daspuml-compiler',
+    version='0.10',
+    license='MIT',
+    description='TYPE YOUR DESCRIPTION HERE',
+    author="\n".join(authors),
+    author_email='',
+    url='https://gitlab.com/agh-dasp/daspuml-language',
+    keywords=['UML'],
+    install_requires=[
+        'antlr4-python3-runtime',
+        'plantuml'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'dasp=compiler.main:main'
+        ]
+    },
+)
