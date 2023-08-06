9fee294f45629f9b80f5c1ce6d01d9d5b724e99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:52 +0000

    Add package eg2w3-g3wg343/0.0.1/eg2w3%20g3wg343-0.0.1.tar.gz

---
+++ packages/eg2w3-g3wg343/eg2w3%20g3wg343-0.0.1.tar.gz/eg2w3 g3wg343-0.0.1/calc/__init__.py
@@ -0,0 +1,11 @@
+def an(num1, num2):
+    return num1 + num2
+
+def sn(num1, num2):
+    return num1 - num2
+
+def mn(num1, num2):
+    return num1 * num2
+
+def dn(num1, num2):
+    return num1 / num2
