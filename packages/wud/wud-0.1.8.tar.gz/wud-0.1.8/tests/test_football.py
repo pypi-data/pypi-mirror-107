5515d7bd4fca3db16fb17c5f6ef7b140fd6e9513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:29 +0000

    Add package wud/0.1.8/wud-0.1.8.tar.gz

---
+++ packages/wud/wud-0.1.8.tar.gz/wud-0.1.8/tests/test_football.py
@@ -0,0 +1,8 @@
+from wud.football import team_name_revise
+
+
+def test_team_name_revise():
+    assert team_name_revise('Chelsea FC', 'footballapi', 'livescore') == 'Chelsea'
+    assert team_name_revise('Tottenham Hotspur', 'livescore', 'short') == 'Spurs'
+    
+    assert team_name_revise("Nankatsu", 'footballapi', 'livescore') == "Nankatsu"
