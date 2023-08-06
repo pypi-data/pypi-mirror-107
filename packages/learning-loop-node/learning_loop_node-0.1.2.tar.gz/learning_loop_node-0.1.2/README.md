a4ee6b1e4c1d719959cc0bae72031d004463c606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:50 +0000

    Add package learning-loop-node/0.1.2/learning_loop_node-0.1.2.tar.gz

---
+++ packages/learning-loop-node/learning_loop_node-0.1.2.tar.gz/learning_loop_node-0.1.2/README.md
@@ -0,0 +1,3 @@
+# Learning Loop Node
+
+This Python library helps you to write your own Detection Nodes, Training Nodes and Converter Nodes for the Zauberzeug Learning Loop.
---
+++ packages/learning-loop-node/learning_loop_node-0.1.2.tar.gz/learning_loop_node-0.1.2/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['learning_loop_node',
+ 'learning_loop_node.converter',
+ 'learning_loop_node.tests',
+ 'learning_loop_node.trainer',
+ 'learning_loop_node.trainer.tests']
+
+package_data = \
+{'': ['*'], 'learning_loop_node.trainer.tests': ['test_data/*']}
+
+install_requires = \
+['fastapi-socketio>=0.0.6,<0.0.7',
+ 'fastapi-utils>=0.2.1,<0.3.0',
+ 'fastapi>=0.63.0,<0.64.0',
+ 'python-socketio[asyncio_client]>=5.0.4,<6.0.0',
+ 'requests>=2.25.1,<3.0.0',
+ 'simplejson>=3.17.2,<4.0.0',
+ 'uvicorn>=0.13.3,<0.14.0']
+
+setup_kwargs = {
+    'name': 'learning-loop-node',
+    'version': '0.1.2',
+    'description': 'Python Library for Nodes which connect to the Zauberzeug Learning Loop',
+    'long_description': '# Learning Loop Node\n\nThis Python library helps you to write your own Detection Nodes, Training Nodes and Converter Nodes for the Zauberzeug Learning Loop.\n',
+    'author': 'Zauberzeug GmbH',
+    'author_email': 'info@zauberzeug.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/zauberzeug/learning_loop_node',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
