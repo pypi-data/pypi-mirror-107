f5cee6d5800fff3bcc415a89439e51efff39877a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:15 +0000

    Add package crocodile/2.1.0/crocodile-2.1.0.tar.gz

---
+++ packages/crocodile/crocodile-2.1.0.tar.gz/crocodile-2.1.0/myresources/crocodile/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "2.1.0"
+docs = r"https://crocodile.readthedocs.io/en/latest/"
+git = r"https://github.com/thisismygitrepo/crocodile"
+release_notes = """
+
+Turbo cmap is removed as is now supported by matplotlib
+"""
