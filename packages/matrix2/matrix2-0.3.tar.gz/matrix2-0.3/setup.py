16711374fc2c60017cdda1473aae29e8d251fa9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:11 +0000

    Add package matrix2/0.3/matrix2-0.3.tar.gz

---
+++ packages/matrix2/matrix2-0.3.tar.gz/matrix2-0.3/setup.py
@@ -0,0 +1,12 @@
+#setup.py
+from setuptools import setup
+
+setup(
+    name='matrix2',
+    scripts=['matrix2'],
+    version= '0.3',
+    description = 'simple project, simple life',
+	long_description = "just a humble project",
+    author = 'madhavth'
+)
+
