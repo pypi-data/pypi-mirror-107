27cb406d3287487291507bb622c48581c1fd7d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:59 +0000

    Add package retval/1.0.1/retval-1.0.1.tar.gz

---
+++ packages/retval/retval-1.0.1.tar.gz/retval-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+	name='retval',
+	version='1.0.1',
+	description='a module for easy error-handling without exceptions',
+	long_description=read('README.md'),
+	long_description_content_type="text/markdown",
+	url='https://github.com/darkwyrm/retval',
+	project_urls={
+		"Bug Tracker": "https://github.com/darkwyrm/retval/issues"
+	},
+	author='Jon Yoder',
+	author_email='jon@yoder.cloud',
+	license='MIT',
+	py_modules=['retval'],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development",
+		"Programming Language :: Python",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
