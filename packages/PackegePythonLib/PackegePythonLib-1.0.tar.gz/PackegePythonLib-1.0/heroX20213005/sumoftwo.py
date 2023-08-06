f34cb5887f4ec1294722e55611235f7ba74a0cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:39 +0000

    Add package PackegePythonLib/1.0/PackegePythonLib-1.0.tar.gz

---
+++ packages/PackegePythonLib/PackegePythonLib-1.0.tar.gz/PackegePythonLib-1.0/heroX20213005/sumoftwo.py
@@ -0,0 +1,20 @@
+import math
+
+
+class SumTwo:
+	
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+	
+	def sumtwovar(self):
+		"""
+		this function is just a simple function to sum two vairables
+		"""
+		return self.x + self.y
+	
+	def squarootvar(self) -> object:
+		"""
+		this function is just a simple function to sum two vairables
+		"""
+		return math.sqrt(self.x)
---
+++ packages/PackegePythonLib/PackegePythonLib-1.0.tar.gz/PackegePythonLib-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+"""
+this is a setup file for the package
+"""
+setup(name='PackegePythonLib',
+      version='1.0',
+      packages=['heroX20213005'],
+      url='', license='MIT',
+      author='Dr. Mohammad Abdelrahman',
+      author_email='dr.m.abdelrahman@gmail.com',
+      description='This module is to test how to create a custom python package.')
