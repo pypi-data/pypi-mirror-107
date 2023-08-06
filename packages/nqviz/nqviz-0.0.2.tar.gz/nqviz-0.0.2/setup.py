a2bba311fee654ddf311791a167dd68240092994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:55 +0000

    Add package nqviz/0.0.2/nqviz-0.0.2.tar.gz

---
+++ packages/nqviz/nqviz-0.0.2.tar.gz/nqviz-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='nqviz',
+      version='0.0.2',
+      description='N Queen Visualizer',
+      packages=['nqviz'],
+      install_requires=['pygame>=2.0.1'],
+      author = 'Jianming Han',
+      author_email = 'hjma810@126.com',
+      zip_safe=False)
