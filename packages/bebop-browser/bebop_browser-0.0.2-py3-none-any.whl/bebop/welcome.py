85a52ee43990f4584febf31e793c150a4d2cdaa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:18 +0000

    Add package bebop-browser/0.0.2/bebop_browser-0.0.2-py3-none-any.whl

---
+++ packages/bebop-browser/bebop_browser-0.0.2-py3-none-any.whl/bebop/welcome.py
@@ -0,0 +1,17 @@
+"""Welcome page."""
+
+WELCOME_PAGE = """\
+# Bebop
+
+Hi! Welcome to the Bebop browser! 🚀🎶
+
+Press "?" or type ":help" and enter to see the keybinds, commands and more, \
+or visit the link below by pressing its link ID (1). To start browsing \
+right away, press "o", type an URL and press Enter.
+
+=> bebop:help Offline help page
+=> gemini://dece.space/dev/bebop.gmi Online Bebop home
+
+You can configure which page to show up when starting Bebop instead of this \
+one: set your home URL in the "home" key of your configuration file.
+"""
