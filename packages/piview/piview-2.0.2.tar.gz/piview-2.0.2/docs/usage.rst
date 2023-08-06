139d679d51303f5e50404bde3a01c172fbf3ff4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:19 +0000

    Add package piview/2.0.2/piview-2.0.2.tar.gz

---
+++ packages/piview/piview-2.0.2.tar.gz/piview-2.0.2/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use PiView in a project::
+
+    import piview
---
+++ packages/piview/piview-2.0.2.tar.gz/piview-2.0.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Adrian Gould",
+    author_email='adrian.gould@nmtafe.wa.edu.au',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Open Software License 3.0 (OSL-3.0)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="TBA",
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='piview',
+    name='piview',
+    packages=find_packages(include=['piview', 'piview.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/AdyGCode/PiView-AG',
+    version='2.0.2',
+    zip_safe=False,
+)
