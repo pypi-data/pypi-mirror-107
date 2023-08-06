568ba64999b67035ea8c96848a0e84d13c4c5b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:34 +0000

    Add package rubric/0.1.0/rubric-0.1.0.tar.gz

---
+++ packages/rubric/rubric-0.1.0.tar.gz/rubric-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rubric', 'rubric.rsrc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiofiles>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['rubric = rubric:cli_entrypoint']}
+
+setup_kwargs = {
+    'name': 'rubric',
+    'version': '0.1.0',
+    'description': 'Initialize your Python project with all the linting boilerplates you need',
+    'long_description': '',
+    'author': 'rednafi',
+    'author_email': 'redowan.nafi@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/rednafi/rubric',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
