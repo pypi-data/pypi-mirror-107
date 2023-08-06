6911b30a0d57f4cb8ccc1ad1f06b1ba08cc1a92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:14 +0000

    Add package tankobon/6.3.0/tankobon-6.3.0-py3-none-any.whl

---
+++ packages/tankobon/tankobon-6.3.0-py3-none-any.whl/tankobon/cli.py
@@ -0,0 +1,315 @@
+# coding: utf8
+
+import collections
+import functools
+import json
+import logging
+import pathlib
+
+import click
+import coloredlogs  # type: ignore
+
+import fpdf
+import imagesize
+from natsort import natsorted
+
+from . import __version__, core, parsers  # noqa: F401
+from .exceptions import MangaNotFoundError
+
+click.option = functools.partial(click.option, show_default=True)  # type: ignore
+
+_log = logging.getLogger("tankobon")
+VERBOSITY = [
+    getattr(logging, level)
+    for level in ("CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG")
+]
+
+MANIFEST_PATH = "manifest.json"
+A4_WIDTH = 210
+A4_HEIGHT = 297
+
+
+@click.group()
+@click.version_option(__version__)
+@click.option(
+    "-v", "--verbose", "verbosity", help="be more chatty", default=4, count=True
+)
+def cli(verbosity):
+    """Manga browser/downloader."""
+    # set up logger
+    coloredlogs.install(
+        level=VERBOSITY[verbosity - 1],
+        fmt=" %(levelname)-8s :: %(message)s",
+        logger=_log,
+    )
+
+
+def _pprint(_dict):
+
+    for key, value in _dict.items():
+
+        # +2 for the ': ' suffix
+        indent = len(key) + 2
+
+        if isinstance(value, list):
+            value = f"\n{' ' * indent}".join(value)
+
+        click.echo(f"{key}: {value}\n")
+
+
+def _info_table(manga):
+    table = []
+
+    volumes = collections.defaultdict(list)
+
+    for _, chapter in manga.data.items():
+        volumes[chapter.volume].append(chapter)
+
+    table.append("| volume | chapter | title ")
+    table.append("|--------|---------|-------")
+
+    for volume, chapters in volumes.items():
+        for chapter in chapters:
+            table.append(
+                "| {:<6} | {:<7} | {}".format(
+                    volume, chapter.id, chapter.title or "(empty)"
+                )
+            )
+
+    return "\n".join(table), len(volumes), len(manga.data)
+
+
+def _info(manga):
+
+    _pprint(manga.meta.__dict__)
+
+    table, n_vol, n_chapter = _info_table(manga)
+
+    click.echo(table)
+
+    click.echo(
+        f"summary: {n_vol} volume{'s' if n_vol > 1 else ''}, {n_chapter} chapter{'s' if n_chapter > 1 else ''}"
+    )
+
+
+def _load(url, cache):
+    try:
+        return cache.load(url)
+    except MangaNotFoundError:
+        click.echo(
+            f"Manga not found in the cache. Try adding it first with 'tankobon refresh {url}'."
+        )
+        raise click.Abort()
+
+
+@cli.command()
+@click.argument("url")
+@click.option("-c", "--chapter", help="show info only for a specific chapter")
+def info(url, chapter):
+    """Show info on a manga url."""
+
+    with core.Cache() as cache:
+
+        manga = _load(url, cache)
+
+        if url not in cache.index:
+            cache.save(manga)
+
+        if chapter:
+            _pprint(manga.data[chapter].__dict__)
+        else:
+            _info(manga)
+
+
+@cli.command("list")
+def _list():
+    """List all manga in the cache."""
+
+    _pprint({"supported websites": list(core.Manga.registered.keys())})
+
+    click.echo("cached manga:\n")
+
+    with core.Cache() as cache:
+
+        if not cache.index:
+            click.echo("(none)")
+
+        else:
+            for url, data in cache.index.items():
+                metadata = data["metadata"]
+                print(f"{url}: {metadata['title']} ({metadata['_hash']})")
+
+
+@cli.command()
+@click.argument("url")
+@click.option(
+    "-p",
+    "--pages",
+    is_flag=True,
+    help="parse pages for chapters without any pages",
+)
+def refresh(url, pages):
+    """Create/refresh data for a manga by url.
+    You can add manga urls using this command (it will be created if it does not exist).
+    """
+
+    with core.Cache() as cache:
+
+        if url not in cache.index:
+            click.echo("manga dosen't exist, creating")
+            manga = core.Manga.from_url(url)
+
+        else:
+            click.echo("loading existing manga")
+            manga = cache.load(url)
+
+        manga.refresh(pages=pages)
+
+        click.echo("saving changes")
+        cache.save(manga)
+
+
+@cli.command()
+@click.argument("url")
+def delete(url):
+    """Delete a manga by url from the cache."""
+
+    with core.Cache() as cache:
+        cache.delete(url)
+
+
+@cli.command()
+@click.argument("url")
+@click.option(
+    "-p",
+    "--path",
+    type=pathlib.Path,
+    default=".",
+    help="where to download to (must be a folder)",
+)
+@click.option(
+    "-c", "--chapters", help="chapters to download, seperated by ',' (i.e '1,2,3,4')"
+)
+@click.option(
+    "-f",
+    "--force",
+    is_flag=True,
+    help="re-download chapters even if they have already been downloaded",
+)
+def download(url, path, chapters, force):
+    """Download a manga by url."""
+
+    try:
+        with (path / MANIFEST_PATH).open() as f:
+            manifest = json.load(f)
+    except FileNotFoundError:
+        manifest = {}
+
+    with core.Cache() as cache:
+
+        manga = _load(url, cache)
+
+        if chapters is None:
+            if click.confirm(
+                "ALL chapters will be downloaded (this consumes a lot of bandwidth). Are you sure?"
+            ):
+                chapters = list(manga.data)
+        else:
+            chapters = chapters.split(",")
+
+        for cid in chapters:
+            click.echo(f"downloading chapter {cid}")
+
+            if cid in manifest and not force:
+                click.echo(f"chapter {cid} already downloaded, skipping")
+                continue
+
+            chapter_path = path / cid
+            chapter_path.mkdir(exist_ok=True)
+
+            try:
+                images = manga.download(cid, chapter_path)
+            except core.PagesNotFoundError:
+                click.echo(
+                    f"Pages for chapter {cid} not found. Try running 'tankobon refresh -p {url}' first."
+                )
+                raise click.Abort()
+
+            manifest[cid] = natsorted(
+                [str(i.relative_to(chapter_path)) for i in images]
+            )
+
+    with (path / MANIFEST_PATH).open("w") as f:
+        json.dump(manifest, f, indent=4)
+
+
+@cli.command()
+@click.option(
+    "-p",
+    "--path",
+    default=".",
+    type=pathlib.Path,
+    help="manga folder to make a pdf for",
+)
+@click.option(
+    "-c", "--chapters", help="only add specific chapters to the pdf, split by ','"
+)
+@click.option(
+    "-o",
+    "--output",
+    type=pathlib.Path,
+    default="export.pdf",
+    help="where to save the pdf",
+)
+def pdfify(path, chapters, output):
+    """Create a (single) pdf file for several or more chapters of a downloade manga."""
+
+    try:
+        with (path / MANIFEST_PATH).open() as f:
+            manifest = json.load(f)
+    except FileNotFoundError:
+        click.echo(
+            "Can't seem to find the manifest file. Did you download the manga to another folder?"
+        )
+        raise click.Abort()
+
+    if chapters is None:
+        chapters = list(manifest.keys())
+    else:
+        chapters = chapters.split(",")
+
+    document = fpdf.FPDF()
+
+    for cid in natsorted(chapters):
+        click.echo(f"adding chapter {cid}")
+
+        chapter = manifest[cid]
+        total = len(chapter) - 1
+        for page in natsorted(chapter):
+            click.echo(f"adding page {page} of {total}")
+            page_path = path / cid / page
+
+            width, height = imagesize.get(page_path)
+            ratio = min(A4_WIDTH / width, A4_HEIGHT / height)
+
+            document.add_page()
+            document.image(str(page_path), 0, 0, w=width * ratio, h=height * ratio)
+
+    document.output(str(path / output), "F")
+
+
+@cli.command("gui")
+def _gui():
+    """Start the tankobon GUI."""
+
+    try:
+        from .gui import gui
+
+    except ImportError:
+        click.echo(
+            "GUI extension not installed, install with 'pip install tankobon[gui]'."
+        )
+        raise click.Abort()
+
+    else:
+        gui.main()
---
+++ packages/tankobon/tankobon-6.3.0-py3-none-any.whl/tankobon/core.py
@@ -0,0 +1,475 @@
+# coding: utf8
+"""Core functionality of tankobon."""
+
+from __future__ import annotations
+
+import abc
+import concurrent.futures as cfutures
+import hashlib
+import json
+import logging
+import pathlib
+import shutil
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, Generator, List, Optional, Union
+
+import bs4
+import requests
+import fake_useragent as ua
+from natsort import natsorted
+
+from . import utils
+from .exceptions import MangaNotFoundError, PagesNotFoundError, UnknownDomainError
+
+# type hints
+StrList = Optional[List[str]]
+
+_log = logging.getLogger("tankobon")
+
+BS4_PARSER = "html5lib"
+USER_AGENT = ua.UserAgent()
+
+CACHE_PATH = pathlib.Path.home() / ".tankobon"
+CACHE_PATH.mkdir(exist_ok=True)
+
+# index at the root of the cache
+INDEX_FILE = "index.json"
+
+# Each manga has a unique hash name generated using MD5
+# (see Metadata.hash).
+# This hash name is used to create a per-manga directory (to store the manga pages).
+
+
+@dataclass
+class Metadata:
+    """Helper class to store manga metadata.
+
+    Args:
+        url: The url to the manga title page.
+        title: The manga name in English (romanized/translated).
+        alt_titles: A list of alternative names for the manga.
+            i.e in another language, original Japanese name, etc.
+        authors: A list of author names.
+        genres: A list of catagories the manga belongs to.
+            i.e shounen, slice_of_life, etc.
+            Note that the catagories are sanitised using utils.sanitise() on initalisation.
+        desc: The sypnosis (human-readable info) of the manga.
+        cover: The url to the manga cover page (must be an image).
+    """
+
+    url: str
+    title: str = ""
+
+    alt_titles: StrList = None
+    authors: StrList = None
+    genres: StrList = None
+
+    desc: str = ""
+    cover: str = ""
+
+    _hash: str = ""
+
+    def __post_init__(self):
+        if self.genres is not None:
+            self.genres = [utils.sanitize(g.strip()) for g in self.genres]
+
+        self.desc = self.desc.strip()
+
+    def parsed(self):
+        """Check whether the metadata fields has been partially/totally filled."""
+        return any(value for key, value in self.__dict__.items() if key != "url")
+
+    @property
+    def hash(self):
+        if not self._hash:
+            self._hash = hashlib.md5((self.title + self.url).encode()).hexdigest()
+
+        return self._hash
+
+
+@dataclass
+class Chapter:
+    """A manga chapter.
+
+    Args:
+        id: The chapter id as a string (i.e 1, 2, 10a, etc.).
+        url: The chapter url.
+        title: The chapter name.
+        volume: The volume the chapter belongs to.
+        pages: A list of image urls to the chapter pages.
+    """
+
+    id: str
+    url: str
+    title: str = ""
+    volume: str = "0"
+    pages: StrList = None
+
+
+class Manga(abc.ABC):
+    """A manga hosted somewhere online.
+
+    Attributes:
+        data: A map of chapter id to the Chapter object.
+        domain: The name of the manga host website, i.e 'mangadex.org'.
+            This **must** be set in any derived subclasses like so:
+
+            class MyManga(Manga):
+                domain = 'mymanga.com'
+                ...
+
+        hash: A MD5 checksum of the manga title + url.
+            This can be used to uniquely identify manga.
+        meta: The manga metadata as a Metadata object.
+        registered: A map of subclass domain to the subclass itself.
+            Subclasses can then be delegated to depending on a url's domain.
+        session: The requests.Session used to download soups.
+        soup: The BeautifulSoup of the manga title page.
+    """
+
+    # It might seem like a bad idea to have a mutable class attribute,
+    # but it is intended to be shared across all class instances.
+    # This allows subclasses to be registered.
+    registered: Dict[str, Dict] = {}
+
+    def __init__(self, data: Dict[str, Any]):
+        self.meta = Metadata(**data["metadata"])
+
+        self.session = requests.Session()
+        self.session.headers.update(
+            {"Referer": self.meta.url, "User-Agent": USER_AGENT.random}
+        )
+
+        self.soup = self.soup_from_url(self.meta.url)
+
+        if not self.meta.parsed():
+            self.meta = self.metadata()
+
+        self.data: Dict[str, Chapter] = {}
+
+        chapters = data.get("chapters")
+
+        if chapters is not None:
+            self.data = {cid: Chapter(**cdata) for cid, cdata in chapters.items()}
+
+    @classmethod
+    def parser(cls, url: str):
+        """Get the appropiate subclass for the domain in url.
+
+        Args:
+            url: The url to get the subclass for.
+
+        Returns:
+            The subclass that can be used to parse the url.
+
+        Raises:
+            UnknownDomainError, if there is no registered subclass for the url domain.
+        """
+
+        domain = utils.parse_domain(url)
+
+        try:
+            subclass = cls.registered[domain]
+        except KeyError:
+            raise UnknownDomainError(f"no parser found for domain '{domain}'")
+
+        return subclass
+
+    @classmethod
+    def from_url(cls, url: str) -> Manga:
+        """Parse a url into a Manga object.
+        The appropiate subclass will be selected based on the url domain.
+
+        Args:
+            url: The url to parse.
+
+        Returns:
+            The parsed Manga object.
+        """
+
+        return cls.import_dict({"metadata": {"url": url}})
+
+    @classmethod
+    def import_dict(cls, data: Dict[str, Any]) -> Manga:
+        """Import manga data.
+
+        Args:
+            data: The previously exported data from export_dict().
+        """
+
+        subclass = cls.parser(data["metadata"]["url"])
+        return subclass(data)
+
+    def close(self):
+        self.session.close()
+        self.soup.decompose()
+
+    def export_dict(self) -> Dict[str, Any]:
+        """Export the manga data.
+        The dict can be saved to disk and loaded back later using import_dict().
+
+        Returns:
+            The manga data as a dict.
+        """
+
+        return {
+            "metadata": self.meta.__dict__,
+            "chapters": {cid: cdata.__dict__ for cid, cdata in self.data.items()},
+        }
+
+    def refresh(self, pages: bool = False):
+        """Refresh the list of chapters available.
+
+        Args:
+            pages: Whether or not to parse the pages for any new chapters.
+                Defaults to False (may take up a lot of bandwidth for many chapters).
+        """
+
+        self.meta = self.metadata()
+
+        for chapter in self.chapters():
+
+            if chapter.id not in self.data:
+
+                _log.info("manga: adding new chapter %s", chapter.id)
+
+                self.data[chapter.id] = chapter
+
+            else:
+                # take the reference to the existing chapter so assigning pages will work properly.
+                chapter = self.data[chapter.id]
+
+            if pages and chapter.pages is None:
+
+                _log.info("manga: adding pages to chapter %s", chapter.id)
+
+                self.data[chapter.id].pages = self.pages(chapter)
+
+    def select(self, start: str, end: str) -> List[str]:
+        """Select chapter ids from the start id to the end id.
+        The ids are sorted first.
+
+        Args:
+            start: The start chapter id.
+            end: The end chapter id.
+
+        Returns:
+            A list of all chapter ids between start and end (inclusive of start and end).
+        """
+
+        cids = natsorted(self.data.keys())
+        start_index = cids.index(start)
+        end_index = cids.index(end, start_index)
+
+        return cids[start_index : end_index + 1]
+
+    def soup_from_url(self, url: str) -> bs4.BeautifulSoup:
+        """Retreive a url and create a soup using its content."""
+        return bs4.BeautifulSoup(self.session.get(url).text, BS4_PARSER)
+
+    def download(
+        self, cid: str, to: Union[str, pathlib.Path], progress: Callable[[int], None]
+    ) -> List[pathlib.Path]:
+        """Download a chapter's pages to a folder.
+
+        Args:
+            cid: The chapter id to download.
+            to: The folder to download the pages to.
+            progress: A callback function which is called with the page number every time a page is downloaded.
+
+        Returns:
+            A list of absolute paths to the downloaded pages in ascending order
+            (1.png, 2.png, 3.png, etc.)
+
+        Raises:
+            PagesNotFoundError, if the chapter's pages have not been parsed yet.
+            To avoid this, .pages(refresh=True) should be called at least once.
+        """
+
+        to = pathlib.Path(to)
+
+        paths = []
+
+        chapter = self.data[cid]
+
+        if chapter.pages is None:
+            raise PagesNotFoundError(
+                f"chapter {cid} has no pages (did you forget to call .refresh(pages=True)?)"
+            )
+
+        total = len(chapter.pages)
+
+        with cfutures.ThreadPoolExecutor(max_workers=8) as pool:
+            futures = {
+                pool.submit(self.session.get, url): count
+                for count, url in enumerate(chapter.pages)
+            }
+
+            for progress_count, future in enumerate(cfutures.as_completed(futures)):
+                count = futures[future]
+
+                _log.info(f"manga: [{chapter.id}] downloading page {count} of {total}")
+
+                resp = future.result()
+                path = utils.save_response(to / str(count), resp)
+                paths.append(path)
+
+                if progress is not None:
+                    progress(progress_count)
+
+        return paths
+
+    def download_cover(self) -> requests.Response:
+        """Download the manga cover.
+        The manga cover url must be valid.
+
+        Returns:
+            The requests.Response of the manga cover url.
+        """
+
+        return self.session.get(self.meta.cover)
+
+    def total(self) -> int:
+        """Return the total number of pages in this manga.
+        All the chapter pages must have already been parsed.
+        """
+
+        return sum(
+            len(c.pages) if c.pages is not None else 0 for c in self.data.values()
+        )
+
+    @abc.abstractmethod
+    def metadata(self) -> Metadata:
+        """Parse metadata from the manga title page.
+
+        Returns:
+            A Metadata object.
+        """
+
+    @abc.abstractmethod
+    def chapters(self) -> Generator[Chapter, None, None]:
+        """Parse chapter info from the manga title page.
+
+        Yields:
+            A Chapter object for each chapter in the manga.
+        """
+
+    @abc.abstractmethod
+    def pages(self, chapter: Chapter) -> List[str]:
+        """Parse pages (images of the manga) from the manga chapter page.
+
+        Args:
+            chapter: The Chapter object to parse for.
+
+        Returns:
+            A list of urls to the chapter pages (must be images).
+        """
+
+    @property
+    @abc.abstractmethod
+    def domain(self) -> str:
+        pass
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+
+        cls.registered[cls.domain] = cls
+
+    def __getattr__(self, attr):
+        try:
+            return self.meta.__dict__[attr]
+        except KeyError:
+            raise AttributeError
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, t, v, tb):
+        self.close()
+
+
+class Cache:
+    def __init__(self, path: pathlib.Path = CACHE_PATH):
+        self.path = path
+        self.path.mkdir(exist_ok=True)
+
+        self.index_path = self.path / INDEX_FILE
+
+        # The index maps manga urls to their metadata.
+        try:
+            with self.index_path.open() as f:
+                self.index = json.load(f)
+
+        except FileNotFoundError:
+            _log.info("cache: index not found, creating")
+            self.index = {}
+
+    def _hash_name(self, url):
+        return self.index[url]["metadata"]["_hash"]
+
+    def _hash_path(self, url):
+        return self.path / self._hash_name(url)
+
+    def save(self, manga: Manga, cover: bool = False):
+        """Save this manga within the cache.
+
+        Args:
+            manga: The manga object to save.
+            cover: Whether or not to save the cover to the cache (if the cover url exists).
+                Defaults to False.
+        """
+
+        # make sure hash is generated
+        manga.meta.hash
+
+        self.index[manga.url] = manga.export_dict()
+
+        manga_path = self._hash_path(manga.url)
+        manga_path.mkdir(exist_ok=True)
+
+        if cover and manga.meta.cover:
+            utils.save_response(manga_path / "cover", manga.download_cover())
+
+    def load(self, url: str):
+        """Load a manga by url.
+
+        Args:
+            url: The manga url.
+
+        Returns:
+            The Manga object.
+
+        Raises:
+            MangaNotFoundError, if the manga does not exist in the cache.
+        """
+        if url not in self.index:
+            raise MangaNotFoundError(
+                f"{url} does not exist in cache. Did you try to create it? (Use Manga.from_url(url) instead.)"
+            )
+
+        return Manga.import_dict(self.index[url])
+
+    def delete(self, url: str):
+        """Delete a manga from the cache.
+
+        Args:
+            url: The manga url.
+
+        Raises:
+            MangaNotFoundError, if the manga does not exist in the cache.
+        """
+        if url not in self.index:
+            raise MangaNotFoundError(f"can't delete {url}")
+
+        shutil.rmtree(self._hash_path(url))
+        del self.index[url]
+
+    def close(self):
+        with self.index_path.open("w") as f:
+            json.dump(self.index, f)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, t, v, tb):
+        self.close()
