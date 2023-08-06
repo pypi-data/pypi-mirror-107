5bff37e2990bf1e3b9399f8cae2887b1e7843c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:22 +0000

    Add package sat-ws/3.1.5/sat_ws-3.1.5-py3-none-any.whl

---
+++ packages/sat-ws/sat_ws-3.1.5-py3-none-any.whl/tests/test_sat_connector.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from sat.package import Package
+from sat.query import Query
+from sat.sat_connector import SATConnector
+
+
+def test_query(query: Query):
+    assert query.status == 5000
+
+
+def test_verify(sat_connector: SATConnector, query: Query):
+    query.identifier = "6de09b92-e830-4f52-808a-b26900407022"
+    query.verify(sat_connector)
+    return query
+
+
+def test_download(sat_connector: SATConnector, packages: List[Package]):
+    for package in packages:
+        package.download(sat_connector)
+        with open(f"tests/downloads/{package.identifier}.zip", "wb") as f:
+            f.write(package.binary)
