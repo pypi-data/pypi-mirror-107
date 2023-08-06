3c51c45ad686e3ddba1a33c8942a37d04ce66d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:53 +0000

    Add package ez-torch/0.1.1/ez-torch-0.1.1.tar.gz

---
+++ packages/ez-torch/ez-torch-0.1.1.tar.gz/ez-torch-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ez_torch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['torch>=1.8.1,<2.0.0', 'torchvision>=0.9.1,<0.10.0']
+
+setup_kwargs = {
+    'name': 'ez-torch',
+    'version': '0.1.1',
+    'description': 'Utility functions and fluent interface for pytorch tensors and modules',
+    'long_description': None,
+    'author': 'Iliya Zhecev',
+    'author_email': 'iliya.zhechev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
