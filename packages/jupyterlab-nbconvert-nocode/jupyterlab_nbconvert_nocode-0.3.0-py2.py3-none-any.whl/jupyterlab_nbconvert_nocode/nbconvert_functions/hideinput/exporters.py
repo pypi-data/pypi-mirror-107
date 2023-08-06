a2687f5b305dfe65c88e2c8c3a019ce3f03f1071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:25 +0000

    Add package jupyterlab-nbconvert-nocode/0.3.0/jupyterlab_nbconvert_nocode-0.3.0-py2.py3-none-any.whl

---
+++ packages/jupyterlab-nbconvert-nocode/jupyterlab_nbconvert_nocode-0.3.0-py2.py3-none-any.whl/jupyterlab_nbconvert_nocode/nbconvert_functions/hideinput/exporters.py
@@ -0,0 +1,36 @@
+import os
+import os.path
+from nbconvert.exporters.html import HTMLExporter
+from nbconvert.exporters.pdf import PDFExporter
+from .utils import ENV_VARS
+
+
+def export_pdf(nbpath, template=None):
+    from nbconvert.nbconvertapp import NbConvertApp
+
+    if template is None:
+        template = os.environ.get(ENV_VARS["export_pdf"], "hide_code_cells_pdf")
+    NbConvertApp.launch_instance([nbpath, "--template", template, "--to", "pdf"])
+
+
+def export_html(nbpath, template=None):
+    from nbconvert.nbconvertapp import NbConvertApp
+
+    if template is None:
+        template = os.environ.get(ENV_VARS["export_html"], "hide_code_cells_html")
+    NbConvertApp.launch_instance([nbpath, "--template", template, "--to", "html"])
+
+
+class HTMLHideCodeExporter(HTMLExporter):
+    export_from_notebook = "HTML - No Code"
+
+    # exclude_input = True
+    def _template_name_default(self):
+        return "hide_code_cells_html"
+
+
+class PDFHideCodeExporter(PDFExporter):
+    export_from_notebook = "PDF - No Code"
+
+    def _template_file_default(self):
+        return "hide_code_cells_pdf"
