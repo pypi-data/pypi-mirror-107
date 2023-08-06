37a89c23308eb66f9e4e7a12ee70ba874034758c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:52 +0000

    Add package allneo/17.0.0/allneo-17.0.0.tar.gz

---
+++ packages/allneo/allneo-17.0.0.tar.gz/allneo-17.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="allneo",
+    author="RONI DAS",
+    version="17.0.0",
+    author_email="roni@totaltechnology.in",
+    description="complete package for neo4j developed by Roni Das",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=["neo4j"],
+    python_requires=">=3.7",
+)
