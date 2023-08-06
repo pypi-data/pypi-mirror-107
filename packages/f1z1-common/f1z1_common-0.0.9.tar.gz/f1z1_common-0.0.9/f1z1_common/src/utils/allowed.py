852a5f3bdd1fd4aea8ba6aece5acfeddf9355d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:11 +0000

    Add package f1z1-common/0.0.9/f1z1_common-0.0.9.tar.gz

---
+++ packages/f1z1-common/f1z1_common-0.0.9.tar.gz/f1z1_common-0.0.9/f1z1_common/src/utils/allowed.py
@@ -0,0 +1,16 @@
+# @Time     : 2021/5/27
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from typing import Any
+
+from ..validator import is_dict
+
+
+def get_from_allowed(key: Any, allowed_props: dict) -> Any:
+    allowed = {} if not is_dict(allowed_props) else allowed_props
+    result = allowed.get(key)
+    if result is None:
+        raise KeyError(f"not found allowed key from {allowed}")
+    return result
