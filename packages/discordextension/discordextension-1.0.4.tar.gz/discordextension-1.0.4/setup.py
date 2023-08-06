8f7a8c2f2ce900d0682009340520b359bb16a20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:36 +0000

    Add package discordextension/1.0.4/discordextension-1.0.4.tar.gz

---
+++ packages/discordextension/discordextension-1.0.4.tar.gz/discordextension-1.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='discordextension',
+    version='1.0.4',
+    author='Feluxerich',
+    description='Little discord.py extension',
+    packages=find_packages(),
+    include_package_data=True
+)
