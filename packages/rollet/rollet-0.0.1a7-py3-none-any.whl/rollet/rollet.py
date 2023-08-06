b7e68c973b6729faafb2b23a9667d800d69815fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:35 +0000

    Add package rollet/0.0.1a7/rollet-0.0.1a7-py3-none-any.whl

---
+++ packages/rollet/rollet-0.0.1a7-py3-none-any.whl/rollet/rollet.py
@@ -0,0 +1,40 @@
+"""
+Rollet functions
+"""
+
+__all__ = [
+    "get_content"
+]
+
+from datetime import datetime
+from tldextract import extract
+
+from rollet import settings
+from rollet.extractor import (
+    BaseExtractor
+)
+
+def get_content(url):
+    """
+    Pull content from url to formated data
+    url: string url
+    :return: dict of content
+    """
+    content = {
+        'url': url,
+        'status': None,
+        'title': 'blacklisted',
+        'abstract': None,
+        'lang': None,
+        'content_type': None,
+        'date': datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    }
+    domain = extract(url).domain
+    if domain in settings.BLACKLIST: return content
+    f_kwargs = settings.FETCH_KWARGS[settings.TYPES.get(domain, 'default')]
+    extractor = settings.EXTRACTORS.get(domain, BaseExtractor)
+    try:
+        content = extractor(url, **f_kwargs).to_dict()
+    except Exception as e:
+        content['title'] = str(e)
+    return content
