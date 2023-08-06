2bab66690b0c86b726e2b58f8137f57413aa1a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:24 +0000

    Add package MuarAugment/1.0/MuarAugment-1.0.tar.gz

---
+++ packages/MuarAugment/MuarAugment-1.0.tar.gz/MuarAugment-1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='MuarAugment',
+    version='1.0',
+    description='State-of-the-art data augmentation search algorithms in PyTorch',
+    author='Adam Mehdi',
+    author_email='amehdi.25@dartmouth.edu',
+    url='https://github.com/adam-mehdi/MuarAugment',
+    install_requires=['pytorch-lightning', 'kornia'],
+    packages=find_packages(),
+)
+
