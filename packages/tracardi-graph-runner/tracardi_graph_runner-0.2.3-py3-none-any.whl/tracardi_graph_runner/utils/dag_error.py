e85ac4a5556217acab466bd7a266e120b6edd448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:09 +0000

    Add package tracardi-graph-runner/0.2.3/tracardi_graph_runner-0.2.3-py3-none-any.whl

---
+++ packages/tracardi-graph-runner/tracardi_graph_runner-0.2.3-py3-none-any.whl/tracardi_graph_runner/utils/dag_error.py
@@ -0,0 +1,8 @@
+class DagError(ValueError):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+        self.port = kwargs['port'] if 'port' in kwargs else None
+
+
+class DagExecError(ValueError):
+    pass
