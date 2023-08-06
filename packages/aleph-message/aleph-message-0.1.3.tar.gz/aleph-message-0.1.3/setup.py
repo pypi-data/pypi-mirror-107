0c2a6becdb78a0fd15da406dd1e6bd5aa5b41dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:49 +0000

    Add package aleph-message/0.1.3/aleph-message-0.1.3.tar.gz

---
+++ packages/aleph-message/aleph-message-0.1.3.tar.gz/aleph-message-0.1.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Aleph Message - Python library for the Aleph.im message specification
+(c) 2021 OKESO for Aleph.im
+"""
+
+import os
+
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(name='aleph-message',
+      version='0.1.3',
+      description='Aleph.im message specification ',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Hugo Herter',
+      author_email='git@hugoherter.com',
+      url='https://github.com/aleph-im/aleph-message',
+      packages=['aleph_message', 'aleph_message.models'],
+      data_files=[],
+      install_requires=[
+          'pydantic',
+          'typing_extensions',
+      ],
+      package_data={"aleph-message": ["py.typed"]},
+      license='MIT',
+      platform='any',
+      keywords="aleph.im message validation specification",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Programming Language :: Python :: 3',
+                   'Intended Audience :: Developers',
+                   'Topic :: System :: Distributed Computing',
+                   ],
+      )
