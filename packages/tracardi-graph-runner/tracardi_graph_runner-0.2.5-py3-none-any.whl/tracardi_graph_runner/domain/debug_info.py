93081aa7aa12ef3393dae366032a3820925aa928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:30 +0000

    Add package tracardi-graph-runner/0.2.5/tracardi_graph_runner-0.2.5-py3-none-any.whl

---
+++ packages/tracardi-graph-runner/tracardi_graph_runner-0.2.5-py3-none-any.whl/tracardi_graph_runner/domain/debug_info.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from pydantic import BaseModel
+
+from tracardi_graph_runner.domain.debug_node_info import DebugNodeInfo
+from tracardi_graph_runner.domain.init_result import InitResult
+
+
+class DebugInfo(BaseModel):
+    init: InitResult
+    run: List[DebugNodeInfo] = []
