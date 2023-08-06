b57c2e3b3aa8fb023504dcbac00aeaf061ac4074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:43 +0000

    Add package gitcomp/1.0.1/gitcomp-1.0.1-py3-none-any.whl

---
+++ packages/gitcomp/gitcomp-1.0.1-py3-none-any.whl/gitcomp/__init__.py
@@ -0,0 +1,11 @@
+from .gitcomp_core import GitComp
+from .user import User
+from .repository import Repository
+from .ser_de import Writer
+
+__all__ = [
+    'GitComp',
+    'User',
+    'Repository',
+    'Writer'
+]
