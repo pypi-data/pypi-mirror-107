374f53a4d42c66ec12a862a9db2e69c00d2e7df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:19 +0000

    Add package percstat/0.0.0a15/percstat-0.0.0a15.tar.gz

---
+++ packages/percstat/percstat-0.0.0a15.tar.gz/percstat-0.0.0a15/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="percstat",
+    version="0.0.0a15",
+    license='BSD',
+    author="jeong seojin",
+    author_email="seojin04230423@gmail.com",
+    description="korean high school 3rd grade math with python3!",
+    long_description=open('README.md').read(),
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent"
+    ],
+)
