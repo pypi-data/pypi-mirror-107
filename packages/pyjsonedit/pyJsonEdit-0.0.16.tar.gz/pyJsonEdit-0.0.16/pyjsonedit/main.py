92084642a4bf79929542fea4b073f10e5e441f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:18 +0000

    Add package pyjsonedit/0.0.16/pyJsonEdit-0.0.16.tar.gz

---
+++ packages/pyjsonedit/pyJsonEdit-0.0.16.tar.gz/pyJsonEdit-0.0.16/pyjsonedit/main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""main file to see execution"""
+
+from io import StringIO
+from typing import List
+import os
+from pyjsonedit.tokenizer import tokenize
+from pyjsonedit.tree import parse as tree_parse
+from pyjsonedit.tree import JsonNode
+from pyjsonedit.matcher import print_matched
+
+def __get_tokens(json) -> List:
+    tokens=[]
+    if os.path.isfile(json):
+        with open(json) as handle:
+            tokens = list(tokenize(handle))
+    else:
+        with StringIO(json) as handle:
+            tokens = list(tokenize(handle))
+    return tokens
+
+def string_to_tokens(json_str: str) -> List:
+    """
+    python3 -c 'from main import *; print( string_to_tokens("{}") );'
+    """
+    return __get_tokens(json_str)
+
+def string_to_tree(json_str: str) -> JsonNode:
+    """
+    python3 -c 'from main import *; r=string_to_tree("{}"); print(r)'
+    """
+    tokens = __get_tokens(json_str)
+    return tree_parse(tokens)
+
+def string_match_mark(json, pattern, symbol='X', color=None):
+    """mark part of matched json"""
+    tokens = __get_tokens(json)
+    node = tree_parse(tokens)
+    return print_matched(json, node, pattern, symbol, color)
+
+def cli_match_mark(pattern, json, symbol, color, callback=print):
+    """cli method for masking matching parts of json"""
+
+    tokens = []
+    if os.path.isfile(json):
+        with open(json) as handle:
+            json = handle.read()
+
+    with StringIO(json) as handle:
+        tokens = list(tokenize(handle))
+
+        node = tree_parse(tokens)
+        ret = print_matched(json, node, pattern, symbol, color)
+        callback(ret)
