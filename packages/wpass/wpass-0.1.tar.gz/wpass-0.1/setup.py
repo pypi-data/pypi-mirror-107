76853830d5e7404dd3a9de37bd9aaf99ef22e740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:57 +0000

    Add package wpass/0.1/wpass-0.1.tar.gz

---
+++ packages/wpass/wpass-0.1.tar.gz/wpass-0.1/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='wpass',
+    scripts=['wpass'],
+	version= '0.1',
+	description = 'cli tool to find wifi passwords',
+	author = 'madhavth'
+)
