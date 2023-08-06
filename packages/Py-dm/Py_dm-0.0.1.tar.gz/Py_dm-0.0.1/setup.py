cf2ef5394cc296d4d32801e7fa462c4b70e7efa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:00 +0000

    Add package Py-dm/0.0.1/Py_dm-0.0.1.tar.gz

---
+++ packages/Py-dm/Py_dm-0.0.1.tar.gz/Py_dm-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Py_dm',
+      version='0.0.1',
+      description='模拟操作',
+      url='https://github.com/qxt514657/',
+      author='1Q847',
+      author_email='976671673@qq.com',
+      license='MIT',
+      packages=['Py_dm'])
