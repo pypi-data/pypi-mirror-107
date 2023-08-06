f705818d087faf0ecba7b01a45f9bc73557ea66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:27 +0000

    Add package samourai-circuitpython-scratch/0.0.1b2/samourai-circuitpython-scratch-0.0.1b2.tar.gz

---
+++ packages/samourai-circuitpython-scratch/samourai-circuitpython-scratch-0.0.1b2.tar.gz/samourai-circuitpython-scratch-0.0.1b2/docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/scratch_simpletest.py
+    :caption: examples/scratch_simpletest.py
+    :linenos:
---
+++ packages/samourai-circuitpython-scratch/samourai-circuitpython-scratch-0.0.1b2.tar.gz/samourai-circuitpython-scratch-0.0.1b2/samourai_scratch.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 James Carr
+#
+# SPDX-License-Identifier: MIT
+"""
+`samourai_scratch`
+================================================================================
+
+Example library for testing updates to cookiecutter
+
+
+* Author(s): James Carr
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+__version__ = "0.0.1-beta.2"
+__repo__ = "https://github.com/lesamouraipourpre/Samourai_CircuitPython_Scratch.git"
