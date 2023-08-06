d1254f8df4f054883451bde4181a18b2369339ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:27 +0000

    Add package npyx/1.6.2/npyx-1.6.2-py3-none-any.whl

---
+++ packages/npyx/npyx-1.6.2-py3-none-any.whl/npyx/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from .utils import npa, sign, thresh, smooth, zscore
+
+__doc__="""
+
+npyx submodules:
+    npyx.utils
+
+    npyx.io
+
+    npyx.gl
+
+    npyx.spk_t
+
+    npyx.spk_wvf
+
+    npyx.corr
+
+    npyx.plot
+
+    npyx.ml
+
+    npyx.behav
+
+    npyx.stats
+"""
