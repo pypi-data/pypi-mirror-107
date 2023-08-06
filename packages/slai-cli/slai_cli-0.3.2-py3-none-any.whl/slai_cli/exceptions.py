bcd88d0608e328e1de6442436ed167f700501504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:47 +0000

    Add package slai-cli/0.3.2/slai_cli-0.3.2-py3-none-any.whl

---
+++ packages/slai-cli/slai_cli-0.3.2-py3-none-any.whl/slai_cli/exceptions.py
@@ -0,0 +1,26 @@
+class InvalidModelInputException(Exception):
+    pass
+
+
+class InvalidModelOutputException(Exception):
+    pass
+
+
+class ProjectExistsException(Exception):
+    pass
+
+
+class ModelExistsException(Exception):
+    pass
+
+
+class InvalidPathException(Exception):
+    pass
+
+
+class InvalidApiKey(Exception):
+    pass
+
+
+class InvalidHandlerSyntax(Exception):
+    pass
