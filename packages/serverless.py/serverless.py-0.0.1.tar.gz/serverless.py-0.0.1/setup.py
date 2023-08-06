6b9ac3ffeb4b4afc9b2b64b61ea46522ee3a68c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:29 +0000

    Add package serverless.py/0.0.1/serverless.py-0.0.1.tar.gz

---
+++ packages/serverless.py/serverless.py-0.0.1.tar.gz/serverless.py-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="serverless.py",
+    version="0.0.1",
+    author="Matheus Vinícius",
+    author_email="mtwzim@gmail.com",
+    description="A Simple Decorator to create a REST Python API in serverless.py",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mtwzim/python-serverless",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_required=">=3.7.9"
+);
