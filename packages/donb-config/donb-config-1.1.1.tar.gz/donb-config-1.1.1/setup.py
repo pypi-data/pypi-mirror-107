086bfb4d5415b9207f453cf5d8ed730d0b5e1f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:27 +0000

    Add package donb-config/1.1.1/donb-config-1.1.1.tar.gz

---
+++ packages/donb-config/donb-config-1.1.1.tar.gz/donb-config-1.1.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="donb-config",
+    version="1.1.1",
+    author="Don Beberto",
+    author_email="bebert64@gmail.com",
+    description="Creation of config files based either on toml or a table in a database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    package_data={"": ["py.typed"]},
+    packages=setuptools.find_packages(where="."),
+    extras_require={
+        "toml": ["toml"],
+        "database": ["peewee"],
+    }
+)
