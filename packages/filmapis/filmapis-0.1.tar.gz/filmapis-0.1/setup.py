571ad4e018933ec906f7c8886bebfcd65a65202a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:38 +0000

    Add package filmapis/0.1/filmapis-0.1.tar.gz

---
+++ packages/filmapis/filmapis-0.1.tar.gz/filmapis-0.1/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(name='filmapis',
+      version='0.1',
+      description='The funniest joke in the world',
+      long_description=long_description,
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
+      url='https://github.com/WTTeneger/kinopoisk_api',
+      author='Amal Agishev',
+      author_email='swintages@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'markdown',
+          'requests',
+      ],
+      include_package_data=True,
+      zip_safe=False)
