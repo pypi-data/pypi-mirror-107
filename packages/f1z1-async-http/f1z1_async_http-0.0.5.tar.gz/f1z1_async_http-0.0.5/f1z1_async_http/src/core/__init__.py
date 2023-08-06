516ae5d1afc6cff9efb1dede729d67d42e065df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:15 +0000

    Add package f1z1-async-http/0.0.5/f1z1_async_http-0.0.5.tar.gz

---
+++ packages/f1z1-async-http/f1z1_async_http-0.0.5.tar.gz/f1z1_async_http-0.0.5/f1z1_async_http/src/core/__init__.py
@@ -0,0 +1,11 @@
+# @Time     : 2021/5/30
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from ._types_for_py import HttpxClient, HttpxRequest, HttpxResponse, HttpxURL
+from .base import IAdapters, IAsyncHttp, IAsyncHttpFactory, IAsyncHttpManager, MergedHook, MergedResult
+from ._adapters import Adapters, MethodAdapter, OptionsAdapter, URLAdapter
+from ._messages import IMessages, Messages
+from ._client import AsyncHttp
+from ._factory import AsyncHttpFactory
