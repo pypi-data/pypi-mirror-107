c12b581fb79d7efc0be78c876ce37146a68059d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:02 +0000

    Add package dsnd-probability-udacity-sahin/0.1/dsnd_probability_udacity_sahin-0.1.tar.gz

---
+++ packages/dsnd-probability-udacity-sahin/dsnd_probability_udacity_sahin-0.1.tar.gz/dsnd_probability_udacity_sahin-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_udacity_sahin',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_udacity_sahin'],
+      author="Ümit Sahin",
+      author_email='uemitsahin@outlook.de',
+      zip_safe=False)
