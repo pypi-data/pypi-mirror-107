5d2c697b6e1b052591b4dbae8d5dbaaae012630c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:40 +0000

    Add package CV-skeleton-provider/0.1.3/CV_skeleton_provider-0.1.3.tar.gz

---
+++ packages/CV-skeleton-provider/CV_skeleton_provider-0.1.3.tar.gz/CV_skeleton_provider-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name                = 'CV_skeleton_provider',
+    version             = '0.1.3',
+    description         = 'provide skeleton of a person',
+    author              = 'LiiNen',
+    author_email        = 'kjeonghoon065@gmail.com',
+    url                 = 'https://github.com/LiiNen/CV_skeleton_provider',
+    install_requires    =  ['numpy', 'opencv-python'],
+    keywords            = ['Skeleton Provider'],
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
