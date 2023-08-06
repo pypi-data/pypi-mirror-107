a842765bb1f81d52506ab02aa48d5d036873a886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:51 +0000

    Add package scivision/0.1.0/scivision-0.1.0.tar.gz

---
+++ packages/scivision/scivision-0.1.0.tar.gz/scivision-0.1.0/scivision/__init__.py
@@ -0,0 +1,2 @@
+def message():
+	print(u'Hello scivision!')
---
+++ packages/scivision/scivision-0.1.0.tar.gz/scivision-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scivision',
+      version='0.1.0',
+      description='A toolkit for scientific image analysis',
+      url='https://github.com/alan-turing-institute/scivision',
+      author='scivision team',
+      author_email='acocac@gmail.com',
+      license='MIT',
+      packages=['scivision'],
+      zip_safe=False)
