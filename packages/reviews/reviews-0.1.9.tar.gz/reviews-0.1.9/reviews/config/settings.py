3a5388762a65f8efa2a2c09b4298dc70e4282229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:14 +0000

    Add package reviews/0.1.9/reviews-0.1.9.tar.gz

---
+++ packages/reviews/reviews-0.1.9.tar.gz/reviews-0.1.9/reviews/config/settings.py
@@ -0,0 +1,52 @@
+from typing import Dict, List, Tuple
+
+from decouple import Csv, config
+from rich.color import ANSI_COLOR_NAMES
+
+# Github Config
+GITHUB_TOKEN = config("GITHUB_TOKEN", cast=str, default="")
+GITHUB_USER = config("GITHUB_USER", cast=str, default="")
+GITHUB_URL = config("GITHUB_URL", cast=str, default="https://api.github.com")
+DEFAULT_PAGE_SIZE = config("DEFAULT_PAGE_SIZE", cast=int, default=100)
+
+# Application Config
+DELAY_REFRESH = config("DELAY_REFRESH", cast=int, default=60)
+REPOSITORY_CONFIGURATION = config(
+    "REPOSITORY_CONFIGURATION",
+    cast=Csv(),
+    default="apoclyps/reviews",
+)
+LABEL_CONFIGURATION = config(
+    "LABEL_CONFIGURATION",
+    cast=Csv(),
+    default="blocked/orange,docker/blue,security/red,python/green",
+)
+
+
+def get_configuration() -> List[Tuple[str, str]]:
+    """converts a comma seperated list of organizations/repositories into a list
+    of tuples.
+    """
+
+    def _to_tuple(values: List[str]) -> Tuple[str, str]:
+        return (values[0], values[1])
+
+    return [
+        _to_tuple(values=configuration.split(sep="/", maxsplit=1))
+        for configuration in REPOSITORY_CONFIGURATION
+    ]
+
+
+def get_label_colour_map() -> Dict[str, str]:
+    """converts a comma seperated list of organizations/repositories into a list
+    of tuples.
+    """
+
+    def _preproc(label_colour: str) -> List[str]:
+        return label_colour.lower().split(sep="/")
+
+    return {
+        label: f"[{colour}]"
+        for label, colour in map(_preproc, LABEL_CONFIGURATION)
+        if colour in ANSI_COLOR_NAMES
+    }
---
+++ packages/reviews/reviews-0.1.9.tar.gz/reviews-0.1.9/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_namespace_packages, setup
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="reviews",
+    packages=find_namespace_packages(include=["*"]),
+    version="0.1.9",
+    license="MIT",
+    description=(
+        "A terminal UI Dashboard for monitoring requests for code review across "
+        "several Github repositories and pull requests."
+    ),
+    author="Kyle Harrison",
+    author_email="kyle.harrison.dev@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/apoclyps/reviews",
+    download_url="https://pypi.org/project/reviews/",
+    keywords=["Reviews", "pull request review"],
+    install_requires=_requires_from_file("requirements.txt"),
+    entry_points={"console_scripts": ["reviews = reviews.cli.main:main"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Terminals",
+    ],
+)
