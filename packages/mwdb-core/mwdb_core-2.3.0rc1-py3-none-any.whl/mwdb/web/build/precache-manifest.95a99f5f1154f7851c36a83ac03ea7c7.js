ff945595bfa67fc3520f9649084d9a3c3f69abad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:58 +0000

    Add package mwdb-core/2.3.0rc1/mwdb_core-2.3.0rc1-py3-none-any.whl

---
+++ packages/mwdb-core/mwdb_core-2.3.0rc1-py3-none-any.whl/mwdb/web/build/precache-manifest.95a99f5f1154f7851c36a83ac03ea7c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd6ae899cad9fb885ad14878e191bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01c65b9c5e0367108676",
+    "url": "/static/css/2.0e969c87.chunk.css"
+  },
+  {
+    "revision": "1289d5a23ff86104d3ae",
+    "url": "/static/css/main.25283c0c.chunk.css"
+  },
+  {
+    "revision": "01c65b9c5e0367108676",
+    "url": "/static/js/2.fe5d000f.chunk.js"
+  },
+  {
+    "revision": "d8f9aaba88f4c07ed193ce4bf2eebdff",
+    "url": "/static/js/2.fe5d000f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1289d5a23ff86104d3ae",
+    "url": "/static/js/main.f2c504e0.chunk.js"
+  },
+  {
+    "revision": "f89b46cc4b0d8b273726",
+    "url": "/static/js/runtime-main.27f1ea9c.js"
+  },
+  {
+    "revision": "b21df568d6dd40d16a1f30ac4eb81763",
+    "url": "/static/media/logo.b21df568.png"
+  }
+]);
