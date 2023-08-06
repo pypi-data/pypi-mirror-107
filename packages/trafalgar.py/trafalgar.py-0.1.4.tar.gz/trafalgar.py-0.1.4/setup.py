e4c9635228f4db15993f2eaeeb6bc462e1633ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:33 +0000

    Add package trafalgar.py/0.1.4/trafalgar.py-0.1.4.tar.gz

---
+++ packages/trafalgar.py/trafalgar.py-0.1.4.tar.gz/trafalgar.py-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='trafalgar.py',
+    version='0.1.4',
+    description='Trafalgar makes quantitative finance and portfolio analysis faster and easier',
+    py_modules=['trafalgar'],
+    package_dir={'':'src'},
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url='https://github.com/ssantoshp/trafalgar',
+    author = "Santosh Passoubady",
+    author_email = "santoshpassoubady@gmail.com",
+    license='MIT',
+    install_requires=[
+          'numpy',
+          'matplotlib',
+          'pykalman',
+          'seaborn',
+          'scipy',
+          'pandas_datareader',
+          'datetime',
+          'statsmodels',
+          'sklearn',
+          'pyportfolioopt'
+      ],
+)
