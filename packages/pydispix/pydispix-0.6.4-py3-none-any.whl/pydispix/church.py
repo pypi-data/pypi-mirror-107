3a73d9927ed6cb67d3e7c925526bf39dd370aad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:20 +0000

    Add package pydispix/0.6.4/pydispix-0.6.4-py3-none-any.whl

---
+++ packages/pydispix/pydispix-0.6.4-py3-none-any.whl/pydispix/church.py
@@ -0,0 +1,122 @@
+import logging
+from abc import abstractmethod
+from dataclasses import dataclass
+from json.decoder import JSONDecodeError
+
+from pydispix.errors import RateLimitBreached
+from typing import Union
+
+from pydispix.client import Client
+from pydispix.color import Color
+from pydispix.utils import resolve_url_endpoint
+from pydispix.color import parse_color
+
+logger = logging.getLogger("pydispix")
+
+
+@dataclass
+class ChurchTask:
+    x: int
+    y: int
+    color: Union[int, str, tuple[int, int, int], Color]
+
+
+class ChurchClient(Client):
+    def __init__(
+        self,
+        pixel_api_token: str,
+        church_token: str,
+        base_church_url: str,
+        *args,
+        **kwargs
+    ):
+        super().__init__(pixel_api_token, *args, **kwargs)
+
+        if not base_church_url.endswith("/"):
+            base_church_url = base_church_url + "/"
+
+        self.base_church_url = base_church_url
+        self.church_token = church_token
+
+    def resolve_church_endpoint(self, endpoint: str):
+        return resolve_url_endpoint(self.base_church_url, endpoint)
+
+    @abstractmethod
+    def get_task(self, endpoint: str = "get_task") -> ChurchTask:
+        """
+        Get task from the church, this is an abstract method, you'll need
+        to override this to get it to work with your church's specific API.
+        """
+
+    @abstractmethod
+    def submit_task(self, church_task: ChurchTask, endpoint: str = "submit_task") -> bool:
+        """
+        Submit a task to the church, this is an abstract method, you'll need
+        to override this to get it to work with your church's specific API.
+        """
+
+    def run_task(
+        self,
+        submit_endpoint: str = "submit_task",
+        show_progress: bool = False,
+        repeat_on_ratelimit: bool = True,
+    ):
+        """
+        Obtain the Church Task, put new pixel on the canvas and send the `submit_task` request.
+
+        In case we hit initial rate limit from `set_pixel`, the rate limit is waited out,
+        and we proceed with a new task, since the church's API time limit for that task
+        has already likely expired.
+        """
+        # This can't just use the `set_pixel`, because we need to send submit message to the church
+        # before we wait for the rate limits, this is also why we use `make_raw_request` instead
+        # of just using `make_requests` that handles the rate limits for us
+        task = self.get_task()
+        logger.info(f"Running church task: {task}")
+        url = self.resolve_endpoint("set_pixel")
+        try:
+            response = self.make_raw_request(
+                "POST", url,
+                data={
+                    "x": task.x,
+                    "y": task.y,
+                    "rgb": parse_color(task.color)
+                }
+            )
+        except RateLimitBreached as exc:
+            if not repeat_on_ratelimit:
+                raise exc
+
+            try:
+                response_text = exc.response.json()
+                response_text = response_text["data"]
+            except JSONDecodeError:
+                response_text = exc.response.content
+            except KeyError:
+                # If we can't get `data` key from obtained JSON,
+                # just use the pure JSON that's already set.
+                pass
+            logger.warning(f"Hit rate limit, request failed ({response_text})")
+            self.rate_limiter.wait(url, show_progress=show_progress)
+            # There is no point in trying to fullfil this request now, just
+            # rerunning and obtain a new request, the rate limit for set_pixel
+            # is quite big, and would violate the rate limits of the church API.
+            return self.run_task(
+                submit_endpoint, show_progress=show_progress,
+                repeat_on_ratelimit=False
+            )
+        data = response.json()
+        # Log successful pixel placement just like `put_pixel` would
+        logger.info(f"Success: {data['message']}")
+
+        # Submit task before waiting out the pixels API limit, which is quite long
+        # and will most likely lead to violations of the church API response time
+        # limit for the given task.
+        status = self.submit_task(task, endpoint=submit_endpoint)
+        self.rate_limiter.wait(url, show_progress=show_progress)
+        return status
+
+    def run_tasks(self, show_progress: bool = False):
+        """Keep running church tasks forever."""
+        while True:
+            self.run_task(show_progress=show_progress)
---
+++ packages/pydispix/pydispix-0.6.4-py3-none-any.whl/pydispix/churches.py
@@ -0,0 +1,122 @@
+from json.decoder import JSONDecodeError
+import logging
+import random
+from dataclasses import dataclass
+import requests
+
+from pydispix.church import ChurchClient, ChurchTask
+
+logger = logging.getLogger("pydispix")
+
+SQLITE_CHURCH = "https://decorator-factory.su"
+RICK_CHURCH = "https://pixel-tasks.scoder12.repl.co/api"
+
+
+@dataclass
+class RickChurchTask(ChurchTask):
+    project_title: str
+    start: float
+
+
+@dataclass
+class SQLiteChurchTask(ChurchTask):
+    id: int
+    issued_by: str
+
+
+class RickChurchClient(ChurchClient):
+    """Church Client designed to work specifically with rick church"""
+    def __init__(
+        self,
+        pixel_api_token: str,
+        church_token: str,
+        base_church_url: str = RICK_CHURCH,
+        *args,
+        **kwargs
+    ):
+        super().__init__(pixel_api_token, church_token, base_church_url, *args, **kwargs)
+
+    def get_task(self, endpoint: str = "get_task") -> RickChurchTask:
+        url = self.resolve_church_endpoint(endpoint)
+        response = self.make_request("GET", url, params={"key": self.church_token})
+        return RickChurchTask(**response["task"])
+
+    def submit_task(self, church_task: RickChurchTask, endpoint: str = "submit_task") -> dict:
+        url = self.resolve_church_endpoint(endpoint)
+        body = {
+            'project_title': church_task.project_title,
+            'start': church_task.start,
+            'x': church_task.x,
+            'y': church_task.y,
+            'color': church_task.color
+        }
+        return self.make_request("POST", url, data=body, params={"key": self.church_token})
+
+    def run_task(
+        self,
+        submit_endpoint: str = "submit_task",
+        show_progress: bool = False,
+        repeat_on_ratelimit: bool = True,
+    ):
+        try:
+            return super().run_task(
+                submit_endpoint=submit_endpoint,
+                show_progress=show_progress,
+                repeat_on_ratelimit=repeat_on_ratelimit
+            )
+        except requests.HTTPError as exc:
+            # Handle church not accepting the request
+            # this could occur because the church's rate limit has expired,
+            # or because someone managed to overwrite the pixel we were setting
+            # before we submitted the task
+            if exc.response.status_code == 400:
+                try:
+                    detail = exc.response.json()["detail"]
+                except (JSONDecodeError, KeyError):
+                    # If the response isn't json decodable or doesn't contain detail key,
+                    # it isn't from rick church
+                    raise exc
+                err_msg = (
+                    "You did not complete this task properly, or it was fixed before "
+                    "the server could verify it. You have not been credited for this task."
+                )
+                if detail != err_msg:
+                    # If we didn't catch this error message, something else has occured
+                    # or this wasn't an exception from the rick church, don't handle it
+                    raise exc
+                # If this was exception from the church, re-run the whole `run_task` function,
+                # which obtains a new task and runs that one, this one has failed
+                logger.warn(
+                    "Church task failed, got code 400. Either you hit church's rate limit, "
+                    "or someone managed to overwrite this pixel before you submitted church request."
+                )
+                return self.run_task(
+                    submit_endpoint=submit_endpoint,
+                    show_progress=show_progress,
+                    repeat_on_ratelimit=repeat_on_ratelimit
+                )
+
+
+class SQLiteChurchClient(ChurchClient):
+    """Church Client designed to work specifically with rick church"""
+    def __init__(
+            self,
+            pixel_api_token: str,
+            base_church_url: str = SQLITE_CHURCH,
+            *args,
+            **kwargs
+    ):
+        # SQLite Church API is open for everyone, it doesn't need a token
+        church_token = None
+        super().__init__(pixel_api_token, church_token, base_church_url, *args, **kwargs)
+
+    def get_task(self, endpoint: str = "tasks") -> SQLiteChurchTask:
+        url = self.resolve_church_endpoint(endpoint)
+        response = self.make_request("GET", url)
+        task = random.choice(response)
+        return SQLiteChurchTask(**task)
+
+    def submit_task(self, church_task: SQLiteChurchTask, endpoint: str = "submit_task"):
+        url = self.resolve_church_endpoint(endpoint)
+        body = {"task_id": church_task.id}
+        return self.make_request("POST", url, data=body)
