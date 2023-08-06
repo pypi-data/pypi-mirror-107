451f32aed07843e8847388c6b9a54a81f86453ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:56 +0000

    Add package mapfmclient/0.2.5/mapfmclient-0.2.5.tar.gz

---
+++ packages/mapfmclient/mapfmclient-0.2.5.tar.gz/mapfmclient-0.2.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="mapfmclient",
+    version="0.2.5",
+    author="Jonathan D�nszelmann",
+    author_email="jonabent@gmail.com",
+    description="client library for interaction with mapf.nl",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonay2000/mapfm-client",
+    project_urls={
+        "Bug Tracker": "https://github.com/jonay2000/mapf_client/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ packages/mapfmclient/mapfmclient-0.2.5.tar.gz/mapfmclient-0.2.5/src/mapfmclient/solution.py
@@ -0,0 +1,32 @@
+from typing import Union, List, Tuple
+
+
+class Path:
+    def __init__(self):
+        self.route: List[Tuple[int, int]] = []
+
+    @classmethod
+    def from_list(cls, route: List[Tuple[int, int]]) -> "Path":
+        res = cls()
+        res.route = route
+        return res
+
+    def serialize(self) -> dict:
+        return {"route": [{"x": i[0], "y": i[1]} for i in self.route]}
+
+
+class Solution:
+    def __init__(self):
+        self.paths = []
+
+    @classmethod
+    def from_paths(cls, paths: List[Union[Path, List[Tuple[int, int]]]]) -> "Solution":
+        res = cls()
+        res.paths = [i if isinstance(i, Path) else Path.from_list(i) for i in paths]
+        return res
+
+    def add_path(self, path: Path, for_agent_index: Union[int, None] = None):
+        self.paths.append(path)
+
+    def serialize(self) -> dict:
+        return {"paths": [i.serialize() for i in self.paths]}
