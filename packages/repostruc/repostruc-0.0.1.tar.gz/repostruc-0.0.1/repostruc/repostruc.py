2445207f97c9e08895b02f6f210f88e2bcff67e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:33 +0000

    Add package repostruc/0.0.1/repostruc-0.0.1.tar.gz

---
+++ packages/repostruc/repostruc-0.0.1.tar.gz/repostruc-0.0.1/repostruc/repostruc.py
@@ -0,0 +1,29 @@
+from argHandler import GetArgumentParser
+from returnUtils import ReturnHandler
+from treeUtils import TreeHandler
+from pathlib import Path
+
+def main():
+    """
+    Main function which handles argumenst passed,
+    and calls respective functions. 
+
+    """
+    argparse = GetArgumentParser()
+    arguments = argparse.getArguments()
+    cur_dir = Path.cwd()
+    if arguments['--clip']:
+        ReturnHandler.copyToClipboard(TreeHandler.formatTree(cur_dir))
+    elif arguments['-f'] or arguments['--file']:
+        file_path = Path.absolute(Path(arguments['<filepath>'])) if arguments['<filepath>'] else Path('PROJECTINFO.md') 
+        ReturnHandler.saveAsFile(
+            TreeHandler.formatTree(cur_dir),
+            file_path,
+            arguments['--readme']
+            )
+    else:
+        print(argparse.doc)
+
+
+if __name__ == '__main__' :
+    main()
