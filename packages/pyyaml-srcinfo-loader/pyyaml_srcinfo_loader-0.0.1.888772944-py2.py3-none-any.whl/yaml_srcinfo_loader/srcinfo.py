0a503c5b8ba780cdbb51e6b66160e4dda3887cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:31 +0000

    Add package pyyaml-srcinfo-loader/0.0.1.888772944/pyyaml_srcinfo_loader-0.0.1.888772944-py2.py3-none-any.whl

---
+++ packages/pyyaml-srcinfo-loader/pyyaml_srcinfo_loader-0.0.1.888772944-py2.py3-none-any.whl/yaml_srcinfo_loader/srcinfo.py
@@ -0,0 +1,33 @@
+#*****************************************************************************
+#* srcinfo.py
+#*****************************************************************************
+
+class SrcInfo(object):
+    
+    def __init__(self,
+                 filename=None,
+                 lineno=-1,
+                 linepos=-1):
+        self.filename = filename
+        self.lineno   = lineno
+        self.linepos  = linepos
+        
+    def __str__(self):
+        if self.filename is not None:
+            return "%s:%d:%d" % (self.filename,self.lineno,self.linepos)
+        else:
+            return "%d:%d" % (self.lineno,self.linepos)
+        
+class SrcInfoDict(dict):        
+    pass
+
+class SrcInfoList(list):
+    pass
+        
+class SrcInfoInt(int):
+    pass
+    
+class SrcInfoStr(str):
+    pass
+    
+    
