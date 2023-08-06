5d95babf69a3d76af5c4127113df3031bdd304ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:49 +0000

    Add package dotify/0.3.9/dotify-0.3.9-py3-none-any.whl

---
+++ packages/dotify/dotify-0.3.9-py3-none-any.whl/dotify/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    "Dotify",
+    "Album",
+    "Playlist",
+    "Track",
+]
+
+from dotify.dotify import Dotify
+from dotify.models.album import Album
+from dotify.models.playlist import Playlist
+from dotify.models.track import Track
