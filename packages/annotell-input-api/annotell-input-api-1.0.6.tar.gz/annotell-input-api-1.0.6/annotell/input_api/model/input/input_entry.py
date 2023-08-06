ae64df16b5af7009e33087209b80535708a0baa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:15 +0000

    Add package annotell-input-api/1.0.6/annotell-input-api-1.0.6.tar.gz

---
+++ packages/annotell-input-api/annotell-input-api-1.0.6.tar.gz/annotell-input-api-1.0.6/annotell/input_api/model/input/input_entry.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Optional
+from enum import Enum
+from annotell.input_api.model.abstract.abstract_models import Response
+
+
+class InputStatus(str, Enum):
+    Processing = "processing"
+    Created = "created"
+    Failed = "failed"
+    InvalidatedBadContent = "invalidated:broken-input"
+    InvalidatedSlamRerun = "invalidated:slam-rerun"
+    InvalidatedDuplicate = "invalidated:duplicate"
+    InvalidatedIncorrectlyCreated = "invalidated:incorrectly-created"
+
+
+@dataclass
+class Input(Response):
+    uuid: str
+    external_id: str
+    batch: str
+    input_type: str
+    status: InputStatus
+    calibration_id: Optional[str]
+    view_link: Optional[str]
+    error_message: Optional[str]
+
+    @staticmethod
+    def from_json(js: dict):
+        return Input(
+            js["internalId"],
+            js["externalId"],
+            js["batchId"],
+            js["inputType"],
+            js["status"],
+            js.get("calibrationId"),
+            js.get("viewLink"),
+            js.get("errorMessage")
+        )
