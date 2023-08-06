3ec7755002c9efdc399e13e8306deb683b38abc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:48 +0000

    Add package mjoe6-distributions/0.1/mjoe6_distributions-0.1.tar.gz

---
+++ packages/mjoe6-distributions/mjoe6_distributions-0.1.tar.gz/mjoe6_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mjoe6_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mjoe6_distributions'],
+      author='Joe McFadden',
+      author_email='mjoe6@vt.edu',
+      zip_safe=False)
