295467fd34f9eda58c6fc3daf09cd124ad384ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:43 +0000

    Add package fuc/0.8.0/fuc-0.8.0-py3-none-any.whl

---
+++ packages/fuc/fuc-0.8.0-py3-none-any.whl/fuc/api/common.py
@@ -0,0 +1,47 @@
+"""
+The common submodule is used by other fuc submodules such as pyvcf and
+pybed. It also provides many day-to-day actions used in the field of
+bioinformatics.
+"""
+
+import pathlib
+import re
+from difflib import SequenceMatcher
+
+def script_name(fn):
+    """Return the script name."""
+    return pathlib.Path(fn).stem
+
+def fuc_dir():
+    """Return the path to the fuc directory."""
+    return pathlib.Path(__file__).parent.parent.parent.absolute()
+
+def get_similarity(a, b):
+    """Return a value from 0 to 1 representing how similar two strings are."""
+    return SequenceMatcher(None, a, b).ratio()
+
+def is_similar(a, b, threshold=0.9):
+    """Return True if the similarity is equal to or greater than threshold."""
+    return get_similarity(a, b) >= threshold
+
+def get_most_similar(a, l):
+    """Return the most similar string in a list."""
+    s = [get_similarity(a, x) for x in l]
+    m = max(s)
+    i = [i for i, x in enumerate(s) if x == m][0]
+    return l[i]
+
+def sumstat(fp, fn, tp, tn):
+    """Return various important statistics."""
+    d = {
+        'tpr': tp / (tp + fn), # sensitivity, recall, hit rate
+        'tnr': tn / (tn + fp), # specificity, selectivity
+        'ppv': tp / (tp + fp), # precision
+        'npv': tn / (tn + fn),
+        'fnr': fn / (fn + tp), # miss rate
+        'fpr': fp / (fp + tn), # fall-out rate
+        'fdr': fp / (fp + tp),
+        'for': fn / (fn + tn),
+        'acc': (tp + tn) / (tp + tn + fp + fn),
+    }
+    return d
