35442abe6fe98bf86f82379aa695daa082424218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:26 +0000

    Add package DecoratorHelper/1.1/DecoratorHelper-1.1.tar.gz

---
+++ packages/DecoratorHelper/DecoratorHelper-1.1.tar.gz/DecoratorHelper-1.1/README.md
@@ -0,0 +1,2 @@
+DecoratorHelper
+This class is intended for decorating called objects. Adds attributes that store arguments to an object and returns it.
---
+++ packages/DecoratorHelper/DecoratorHelper-1.1.tar.gz/DecoratorHelper-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='DecoratorHelper',
+      version='1.1',
+      description='This class is intended for decorating called objects.',
+      packages=['DecoratorHelper'],
+      author_email='ivandusheko@mail.ru',
+      zip_safe=False)
