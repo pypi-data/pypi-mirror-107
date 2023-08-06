c417c8f5ff570c8c1198cf3fdc16c18d669843de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:30 +0000

    Add package newsdataapi/0.1.1/newsdataapi-0.1.1-py3-none-any.whl

---
+++ packages/newsdataapi/newsdataapi-0.1.1-py3-none-any.whl/newsdataapi/constants.py
@@ -0,0 +1,17 @@
+# All the Newsdata supported API URL
+BASE_URL = 'https://newsdata.io/api/1/'
+
+# Latest News URL 
+NEWS_URL = BASE_URL + 'news'
+
+
+# News Archive URL 
+ARCHIVE_URL = BASE_URL + 'archive'
+
+
+# News Sources URL 
+SOURCES_URL = BASE_URL + 'sources'
+
+
+# Default request timeout is 300 seconds 
+DEFAULT_REQUEST_TIMEOUT = 300
