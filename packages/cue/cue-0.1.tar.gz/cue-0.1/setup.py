747586f54e0f3722c0c60a200eb0511ad9e50119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:48 +0000

    Add package cue/0.1/cue-0.1.tar.gz

---
+++ packages/cue/cue-0.1.tar.gz/cue-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='cue',
+      version='0.1',
+      description='Accounting system for linux',
+      author='Jonas McCallum',
+      author_email='jonasmccallum@gmail.com',
+      url='https://kosciuszko.cloud',
+      packages=['cue'],
+      install_requires=['ruamel.yaml'],
+      extras_require={'testing': ['pytest']},
+      entry_points={"console_scripts": ["cue = cue.cli.bin:cue"]},
+      zip_safe=False)
