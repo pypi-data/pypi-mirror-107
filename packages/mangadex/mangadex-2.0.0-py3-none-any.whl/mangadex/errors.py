386b47640857c4f939c70fd2c21539cbeecb6912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:42 +0000

    Add package mangadex/2.0.0/mangadex-2.0.0-py3-none-any.whl

---
+++ packages/mangadex/mangadex-2.0.0-py3-none-any.whl/mangadex/errors.py
@@ -0,0 +1,28 @@
+class ApiError(Exception):
+    pass
+class ApiClientError(Exception):
+    pass
+
+class MangaError(Exception):
+    pass
+
+class TagError(Exception):
+    pass
+
+class ChapterError(Exception):
+    pass
+
+class AuthorError(Exception):
+    pass
+
+class ScanlationGroupError(Exception):
+    pass
+
+class UserError(Exception):
+    pass
+
+class CustomListError(Exception):
+    pass
+
+class CoverArtError(Exception):
+    pass
