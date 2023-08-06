c999db2b85f19c422b2bb3c9d6176910e2249628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:53 +0000

    Add package dmsales/0.2.2/dmsales-0.2.2-py3-none-any.whl

---
+++ packages/dmsales/dmsales-0.2.2-py3-none-any.whl/dmsales/api_client.py
@@ -0,0 +1,36 @@
+import requests
+import logging
+
+from .endpoints import contacts, project, segment, events, search, person, user, techscopeapi
+
+logger = logging.getLogger(__name__)
+
+class DMSalesAPI(
+    project.ProjectEndpoints,
+    contacts.ContactsEndpoints,
+    segment.SegmentEndpoints,
+    events.EventsEndpoints,
+    search.SearchEndpoints,
+    person.PersonEndpoints,
+    user.UserEndpoints,
+    techscopeapi.TechScopeApiEndpoints
+):
+    
+    api_host = 'https://app.dmsales.com'
+
+    def __init__(self, api_key: str, test: bool=False):
+        '''
+        Main class to manipulate DMSales API with all implemented methods
+
+        :param api_key: dmsales api key https://app.dmsales.com/pl/panel/settings-account?settings=api-configuration
+        :type api_key: str
+        :param test: dmsales api test environment, defaults to False
+        :type test: bool, optional
+        '''
+        self.api_key = api_key
+
+        if test is True:
+            self.api_host = 'http://dmsales.test.dmsales.com:8081'
+
+        self.session = requests.Session()
+        self.session.headers = {'Authorization': f'Bearer {api_key}'}
