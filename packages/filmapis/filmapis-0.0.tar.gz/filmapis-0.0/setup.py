7497580ff4f41d91b5ab2b0112e30b8446940995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:18 +0000

    Add package filmapis/0.0/filmapis-0.0.tar.gz

---
+++ packages/filmapis/filmapis-0.0.tar.gz/filmapis-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='filmapis',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['filmapis'],
+      author_email='swintages@gmail.com',
+      zip_safe=False)
