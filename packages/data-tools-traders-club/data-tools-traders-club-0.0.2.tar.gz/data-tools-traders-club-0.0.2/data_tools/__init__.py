77dc053261719a25ade97fad2b1cc5580bd6cac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:24 +0000

    Add package data-tools-traders-club/0.0.2/data-tools-traders-club-0.0.2.tar.gz

---
+++ packages/data-tools-traders-club/data-tools-traders-club-0.0.2.tar.gz/data-tools-traders-club-0.0.2/data_tools/__init__.py
@@ -0,0 +1,12 @@
+__doc__ = """
+data_tools
+=========
+Package of analytical support tools for data science projects"""
+
+__author__ = """Fabio Caffarello"""
+__version__ = "0.0.2"
+
+# imports
+
+from . import connection
+from . import tools
---
+++ packages/data-tools-traders-club/data-tools-traders-club-0.0.2.tar.gz/data-tools-traders-club-0.0.2/data_tools/connection/mysql/__init__.py
@@ -0,0 +1,7 @@
+__doc__ = """
+data_tools.connection.mysql
+======================
+Module to perform queries in MySQL"""
+
+# imports
+from .api import MySQL
