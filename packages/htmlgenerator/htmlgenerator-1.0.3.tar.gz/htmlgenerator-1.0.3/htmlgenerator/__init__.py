1e528191914d20cc3525a3106f3ec48e313e17a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:32 +0000

    Add package htmlgenerator/1.0.3/htmlgenerator-1.0.3.tar.gz

---
+++ packages/htmlgenerator/htmlgenerator-1.0.3.tar.gz/htmlgenerator-1.0.3/htmlgenerator/__init__.py
@@ -0,0 +1,7 @@
+from .base import *  # noqa
+from .htmltags import *  # noqa
+from .lazy import *  # noqa
+from .safestring import mark_safe  # noqa
+
+__version__ = "1.0.3"
+__DEBUG__ = False  # not sure whether this is a smart idea, but environment variables do not really seem better...
