94e5c96f5e544a1935ee2801ce884a32e870bbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:30 +0000

    Add package trak/0.1.1/trak-0.1.1.tar.gz

---
+++ packages/trak/trak-0.1.1.tar.gz/trak-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="trak",
+    version="0.1.1",
+    author="Greg Pyle",
+    author_email="chaoborid@gmail.com",
+    description="A simple writing-progress tracking tool for plain text files",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license_files=['LICENSE'],
+    download_url="https://github.com/Gambusia/trak",
+    packages=["src", "src.trak"],
+    package_data={'src': ['data/*.dat']},
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Office/Business',
+          'Topic :: Text Editors :: Text Processing',
+          'Topic :: Text Processing :: General',
+          'Topic :: Utilities',
+          'Typing :: Typed',
+          ],
+    entry_points={
+        "console_scripts": [
+            "trak = src.__main__:main"
+        ]
+    },
+)
