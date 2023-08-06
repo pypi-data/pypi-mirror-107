c8afe64a0de167e2320c2715bc05f8a67d55aa38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:59 +0000

    Add package goopylib/1.1.319a26/goopylib-1.1.319a26.tar.gz

---
+++ packages/goopylib/goopylib-1.1.319a26.tar.gz/goopylib-1.1.319a26/goopylib/imports.py
@@ -0,0 +1,10 @@
+from goopylib.styles import *
+from goopylib.util import *
+from goopylib.constants import *
+
+from goopylib.colours import *
+from goopylib.Window import Window
+
+from goopylib.objects.imports import *
+from goopylib.maths.imports import *
+from goopylib.sound.imports import *
