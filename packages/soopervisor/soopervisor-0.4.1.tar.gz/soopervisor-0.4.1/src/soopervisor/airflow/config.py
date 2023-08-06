946f22eb4f45281d79212302f9791fc471b9c45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:55 +0000

    Add package soopervisor/0.4.1/soopervisor-0.4.1.tar.gz

---
+++ packages/soopervisor/soopervisor-0.4.1.tar.gz/soopervisor-0.4.1/src/soopervisor/airflow/config.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from soopervisor import abc
+from soopervisor.enum import Backend
+
+
+class AirflowConfig(abc.AbstractConfig):
+    repository: Optional[str] = None
+
+    @classmethod
+    def get_backend_value(cls):
+        return Backend.airflow.value
+
+    @classmethod
+    def defaults(cls):
+        data = cls(repository='your-repository/name').dict()
+        data['backend'] = cls.get_backend_value()
+        del data['include']
+        return data
