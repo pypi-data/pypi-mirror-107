6d27dad47b08573e248f3416f4156c6c6c5f08c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:29 +0000

    Add package EFumi-WhlTest/0.4/EFumi_WhlTest-0.4-py3-none-any.whl

---
+++ packages/EFumi-WhlTest/EFumi_WhlTest-0.4-py3-none-any.whl/EFumi_WhlTest/__init__.py
@@ -0,0 +1,11 @@
+import EFumi_WhlTest.variables as v
+import EFumi_WhlTest.functions as f
+
+
+def run():
+    for i in range(v.iterations):
+    # while loop:
+        v.p, point_list = f.rotate(v.p)
+        print(point_list)
+
+run()
