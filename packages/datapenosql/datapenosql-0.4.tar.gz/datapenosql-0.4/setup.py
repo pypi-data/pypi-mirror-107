944f51208726a4b4c435288212c2651c16abf550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:59 +0000

    Add package datapenosql/0.4/datapenosql-0.4.tar.gz

---
+++ packages/datapenosql/datapenosql-0.4.tar.gz/datapenosql-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='datapenosql',
+    version='0.4',
+    packages=['dpsql', 'dpsql.table', 'dpsql.table.model', 'dpsql.table.helpers', 'dpsql.table.service'],
+    url='https://github.com/compufreq/datapenosql.git',
+    license='MIT License',
+    author='Alaa Alhorani',
+    author_email='alaa.alhorani@outlook.com',
+    description='DataPeno SQL Query Builder Utility',
+    classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.9",
+            "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.6',
+)
