a548272eb7e2d0344dd46599356584d4087d13eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:31 +0000

    Add package bergen/0.4.10/bergen-0.4.10.tar.gz

---
+++ packages/bergen/bergen-0.4.10.tar.gz/bergen-0.4.10/bergen/wards/gql/aiohttp.py
@@ -0,0 +1,51 @@
+import logging
+from abc import ABC
+
+from bergen.console import console
+from bergen.query import GQL, TypedGQL
+from bergen.wards.gql.base import BaseGraphQLWard, GraphQLException
+from gql.gql import gql
+from gql.transport.aiohttp import AIOHTTPTransport
+from gql.transport.aiohttp import log as aiohttp_logger
+from gql.transport.requests import RequestsHTTPTransport
+
+aiohttp_logger.setLevel(logging.WARNING)
+
+
+class AIOHttpGraphQLWard(BaseGraphQLWard):
+    can_subscribe = False
+
+    async def connect(self):
+        self.async_transport = AIOHTTPTransport(url=self._graphql_endpoint, headers=self._headers)
+        await self.async_transport.connect()
+
+    async def negotiate(self):
+        if self.needs_negotiation:
+            query_node = gql("""
+                mutation Negotiate {
+                    negotiate
+                }
+            """)
+            response = await self.async_transport.execute(query_node, variable_values={})
+            return response.data["negotiate"]
+        else:
+            return None
+    
+
+    async def pass_async(self, the_query: TypedGQL, variables: dict = {}, **kwargs):
+        query_node = gql(the_query.query)
+        try:
+            response = await self.async_transport.execute(query_node, variable_values=variables)
+
+            if response.errors:
+                raise GraphQLException(f"Ward {self._graphql_endpoint}:" + str(response.errors))
+            
+            return the_query.extract(response.data)
+
+        except:
+            console.print_exception(show_locals=True)
+            raise 
+            
+        
+    async def disconnect(self):
+        await self.async_transport.close()
