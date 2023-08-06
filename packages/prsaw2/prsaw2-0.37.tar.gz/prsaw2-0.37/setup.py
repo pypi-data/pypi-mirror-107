792c5ae4c39f4b9c109cc8ed0c2108f6cd2bf00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:45 +0000

    Add package prsaw2/0.37/prsaw2-0.37.tar.gz

---
+++ packages/prsaw2/prsaw2-0.37.tar.gz/prsaw2-0.37/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="prsaw2",
+    version="0.37",
+    author="Flampt",
+    description="Stands for Python random api wrapper version 2.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FlamptX/prsaw2",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='python, api, rsa',
+    package_dir={"": "prsaw2"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
