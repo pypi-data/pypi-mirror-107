94e58dae4bef67627a712f61fb6e5680f56bfa31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:58 +0000

    Add package nanocompore/1.0.4/nanocompore-1.0.4.tar.gz

---
+++ packages/nanocompore/nanocompore-1.0.4.tar.gz/nanocompore-1.0.4/nanocompore/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+from nanocompore.common import get_version
+
+__version__ = get_version()
+
+__description__ = 'Software package that identifies raw signal changes between two conditions from https://github.com/jts/nanopolish resquiggled dRNA-Seq data.'
