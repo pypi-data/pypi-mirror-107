8faa7ab49b7036e9b1619c8c96206f05d3d1ee9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:54 +0000

    Add package moranpycess/1.0.39/moranpycess-1.0.39-py3-none-any.whl

---
+++ packages/moranpycess/moranpycess-1.0.39-py3-none-any.whl/moranpycess/__init__.py
@@ -0,0 +1,26 @@
+"""
+##############################################################################
+#
+#   Init file for the package
+#
+#   AUTHOR: Maciej_Bak
+#   AFFILIATION: University_of_Basel
+#   AFFILIATION: Swiss_Institute_of_Bioinformatics
+#   CONTACT: wsciekly.maciek@gmail.com
+#   CREATED: 20-07-2020
+#   LICENSE: MIT
+#
+##############################################################################
+"""
+
+# imports
+import os
+import sys
+
+# modify PYTHONPATH so that modules can import each other
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+# import distinct classes from modules of this package
+from .MoranProcess import MoranProcess
+from .MoranProcess2D import MoranProcess2D
+from .MoranProcess3D import MoranProcess3D
