c5beeeee3dfa2857ad9fc2485b9cb69025250f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:08 +0000

    Add package f1z1-common/0.0.7/f1z1_common-0.0.7.tar.gz

---
+++ packages/f1z1-common/f1z1_common-0.0.7.tar.gz/f1z1_common-0.0.7/f1z1_common/src/conf/factory.py
@@ -0,0 +1,61 @@
+# @Time     : 2021/5/28
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from functools import partial
+
+from common.f1z1_common.src.conf import AllowedOptions, AbstractConfigReader
+from common.f1z1_common.src.utils import ExtensionNameList, PathTypes
+from common.f1z1_common.src.validator.is_validators import is_iterable, is_list
+
+
+class IFactory(object):
+
+    @classmethod
+    def create(cls, *args, **kwargs) -> AbstractConfigReader:
+        raise NotImplementedError("")
+
+
+class ReaderFactory(IFactory):
+
+    @classmethod
+    def create(cls,
+               subclass: AbstractConfigReader,
+               file: PathTypes,
+               options: AllowedOptions,
+               extension_names: ExtensionNameList = None,
+               **kwargs) -> AbstractConfigReader:
+        f = cls._factory(
+            subclass, file, options, extension_names
+        )
+        return f(**kwargs)
+
+    @classmethod
+    def _factory(cls,
+                 subclass: AbstractConfigReader,
+                 file: PathTypes,
+                 options: AllowedOptions,
+                 extension_names: ExtensionNameList = None):
+        if not cls.is_reader_subclass(subclass):
+            raise ValueError(f"need a AbstractConfigReader subclass, but got {type(subclass).__name__}")
+        klass = subclass
+        return partial(
+            klass,
+            file,
+            options=options,
+            extension_names=cls.to_extension_names(extension_names)
+        )
+
+    @staticmethod
+    def to_extension_names(extension_names: ExtensionNameList = None) -> list:
+        if is_list(extension_names):
+            return extension_names
+        elif is_iterable(extension_names):
+            return list(extension_names)
+        else:
+            return []
+
+    @staticmethod
+    def is_reader_subclass(subclass):
+        return issubclass(subclass, AbstractConfigReader)
