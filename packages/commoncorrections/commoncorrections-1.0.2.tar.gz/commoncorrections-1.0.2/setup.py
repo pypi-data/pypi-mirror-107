4f9fa5733a60a237e2e3166627cf5b7628fa7715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:04 +0000

    Add package commoncorrections/1.0.2/commoncorrections-1.0.2.tar.gz

---
+++ packages/commoncorrections/commoncorrections-1.0.2.tar.gz/commoncorrections-1.0.2/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def read_file(fname):
+    with open(fname, "r") as f:
+        return f.read()
+
+setuptools.setup(
+    name="commoncorrections",
+    version="1.0.2",
+    author="Rob Smith",
+    author_email="robmsmt@gmail.com",
+    description="A small python implementation of common ASR corrections",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robmsmt/CommonCorrections",
+    package_dir={"": "commoncorrections"},
+    packages=setuptools.find_packages(where="commoncorrections"),
+    python_requires=">=3.6",
+    test_suite="tests/*",
+    install_requires=[
+        elem.strip()
+        for elem in read_file("requirements.txt").splitlines()
+        if elem.strip()
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+#python3 -m pip install --upgrade setuptools wheel
+#python3 setup.py sdist bdist_wheel
+#sudo apt install twine -y
+#twine upload dist/*
+
+#one liner
+#rm -rf ./build ./dist ./commoncorrections.egg-info && python3 setup.py sdist bdist_wheel && twine upload dist/*
