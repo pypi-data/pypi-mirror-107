ef32c0095939448f456e68bf9c5dd1f52948f929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:44 +0000

    Add package django-ts-bridge/1.3.2/django_ts_bridge-1.3.2-py3-none-any.whl

---
+++ packages/django-ts-bridge/django_ts_bridge-1.3.2-py3-none-any.whl/django_ts_bridge/settings.py
@@ -0,0 +1,7 @@
+DJANGO_TS_BRIDGE_EXCLUDED_APPS = ['rest_framework', 'wagtail.api.v2']
+DJANGO_TS_BRIDGE_CHOICE_DEFINITION_IMPORT_PATH = 'ts/django/django-model-choices'
+DJANGO_TS_BRIDGE_CHOICE_DEFINITION_OUTPUT_PATH = 'src/ts/django/django-model-choices.ts'
+DJANGO_TS_BRIDGE_MODEL_DEFINITION_OUTPUT_PATH = 'src/ts/@types/django-models.d.ts'
+DJANGO_TS_BRIDGE_URL_OUTPUT_PATH = 'src/django/urls.json'
+DJANGO_TS_BRIDGE_CHOICE_VAR_NAME = 'Choices'
+DJANGO_TS_BRIDGE_URLS_VAR_NAME = 'Urls'
