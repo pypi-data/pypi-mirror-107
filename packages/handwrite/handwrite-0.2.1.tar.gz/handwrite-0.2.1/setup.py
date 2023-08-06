80be7d3155d4e4acdaae4f6d2a304d2fac2fcb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:32 +0000

    Add package handwrite/0.2.1/handwrite-0.2.1.tar.gz

---
+++ packages/handwrite/handwrite-0.2.1.tar.gz/handwrite-0.2.1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="handwrite",
+    version="0.2.1",
+    author="Yash Lamba, Saksham Arora, Aryan Gupta",
+    author_email="yashlamba2000@gmail.com, sakshamarora1001@gmail.com, aryangupta973@gmail.com",
+    description="Convert text to custom handwriting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cod-ed/handwrite",
+    packages=setuptools.find_packages(),
+    install_requires=["opencv-python", "Pillow"],
+    extras_require={
+        "dev": [
+            "pre-commit",
+            "black",
+            "mkdocs",
+            "mkdocs-material",
+            "pymdown-extensions",
+            "mkdocstrings",
+            "pytkdocs[numpy-style]",
+        ]
+    },
+    entry_points={
+        "console_scripts": ["handwrite = handwrite.cli:main"],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
