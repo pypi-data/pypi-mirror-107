625d3cbc08653efb4ebb813d9feb8e811e6a9ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:54 +0000

    Add package mediaiobest1213123123/0.0.6/mediaiobest1213123123-0.0.6.tar.gz

---
+++ packages/mediaiobest1213123123/mediaiobest1213123123-0.0.6.tar.gz/mediaiobest1213123123-0.0.6/setup.py
@@ -0,0 +1,14 @@
+﻿import setuptools
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+setuptools.setup(  
+    name = 'mediaiobest1213123123',  
+    version = '0.0.6',
+    author = 'wucm',  
+    author_email = 'wucm@gmail.com',	
+    description = 'get a chinesename by random',
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
+	url = "http://www.xxx.com",
+    packages=setuptools.find_packages()
+)  
