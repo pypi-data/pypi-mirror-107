3a7e5d64bdc54d0545f7606a94eb456d7c62d763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:14 +0000

    Add package classes-qgs/0.0.1/classes_qgs-0.0.1.tar.gz

---
+++ packages/classes-qgs/classes_qgs-0.0.1.tar.gz/classes_qgs-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="classes_qgs",
+    version="0.0.1",
+    author="quantumgames studios",
+    author_email="ttvquantumgames27@gmail.com",
+    description="A small example package",
+    long_description="idk",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "class"},
+    packages=setuptools.find_packages(where="class"),
+    python_requires=">=3.6",
+)
