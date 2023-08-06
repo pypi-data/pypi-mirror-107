0bae1c9fc347bf6af65a8cc284e015d1e496ed6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:51 +0000

    Add package opps/0.2.16/opps-0.2.16.tar.gz

---
+++ packages/opps/opps-0.2.16.tar.gz/opps-0.2.16/opps/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pkg_resources
+
+pkg_resources.declare_namespace(__name__)
+
+VERSION = (0, 2, 16)
+__version__ = ".".join(map(str, VERSION))
+__status__ = "Development"
+__description__ = u"Open Source Content Management Platform - CMS for the "
+u"magazines, newspappers websites and portals with "
+u"high-traffic, using the Django Framework."
+
+__author__ = u"Thiago Avelino"
+__credits__ = ['Bruno Rocha']
+__email__ = u"avelinorun@gmail.com"
+__license__ = u"MIT"
+__copyright__ = u"Copyright 2014, Thiago Avelino"
