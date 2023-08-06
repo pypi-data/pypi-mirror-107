970cfb62fe9a6dd7966d3a193d35da59b91201ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:52 +0000

    Add package monolens/0.6.2/monolens-0.6.2-py3-none-any.whl

---
+++ packages/monolens/monolens-0.6.2-py3-none-any.whl/monolens/__init__.py
@@ -0,0 +1,30 @@
+from ._version import version as __version__  # noqa
+
+
+def main():
+    from PySide6 import QtWidgets, QtCore
+    from .lens import Lens
+    from .intro import Intro
+    from .util import DEBUG
+    import sys
+    import signal
+
+    QtCore.QCoreApplication.setOrganizationName("Monolens")
+    QtCore.QCoreApplication.setApplicationName("Monolens")
+
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    app = QtWidgets.QApplication(sys.argv)
+    lens = Lens()
+    settings = QtCore.QSettings()
+    if DEBUG:
+        print("settings:")
+        for key in settings.allKeys():
+            value = settings.value(key)
+            print(f"  {key}: {value}")
+    if DEBUG == 2 or settings.value("show_intro", "True") == "True":
+        intro = Intro()
+        intro.show()
+        intro.closed.connect(lens.show)
+    else:
+        lens.show()
+    sys.exit(app.exec())
