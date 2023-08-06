7ddc8ae01c2a41a2f6c586fd41ce517274c8f6d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:27 +0000

    Add package lime-python/2.0.4/lime_python-2.0.4-py3-none-any.whl

---
+++ packages/lime-python/lime_python-2.0.4-py3-none-any.whl/lime_python/protocol/client/command_processor.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+from typing import Awaitable
+
+from ..command import Command
+from ..listeners import CommandListener
+
+
+class CommandProcessor(CommandListener):
+    """Command Processor."""
+
+    @abstractmethod
+    async def process_command_async(
+        self,
+        command: Command,
+        timeout: float
+    ) -> Awaitable[Command]:
+        """Process a Command asynchronously and return the result.
+
+        Args:
+            command (Command): The Command to be processed
+            timeout (float): Timeout to process the Command
+
+        Returns:
+            Command: The result Command
+        """  # noqa: DAR202
+        pass
