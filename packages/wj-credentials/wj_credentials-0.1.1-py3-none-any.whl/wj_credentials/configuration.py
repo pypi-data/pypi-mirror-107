a50e6c4fbec4a83d9f0582f20d24b05e2f2d8e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:54 +0000

    Add package wj-credentials/0.1.1/wj_credentials-0.1.1-py3-none-any.whl

---
+++ packages/wj-credentials/wj_credentials-0.1.1-py3-none-any.whl/wj_credentials/configuration.py
@@ -0,0 +1,70 @@
+import boto3
+import os
+import base64
+from botocore.exceptions import ClientError
+
+# Variables that contains the user credentials to access Twitter API
+# Whale and Jaguar Oficial keys
+access_token1 = os.getenv('WJ_CREDENTIAL_ACCESS_TOKEN1')
+access_token_secret1 = os.getenv('WJ_CREDENTIAL_ACCESS_SECRET1')
+consumer_key1 = os.getenv('WJ_CREDENTIAL_CONSUMER_KEY1')
+consumer_secret1 = os.getenv('WJ_CREDENTIAL_CONSUMER_SECRET1')
+
+
+
+def get_secret(secret_name, region_name):
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name,
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+    try:
+        get_secret_value_response = client.get_secret_value( SecretId=secret_name)
+        return get_secret_value_response
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        else:
+            # Decrypts secret using the associated KMS CMK.
+            # Depending on whether the secret is a string or binary, one of these fields will be populated.
+            if 'SecretString' in get_secret_value_response:
+                secret = get_secret_value_response['SecretString']
+                return secret
+            else:
+                decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+                return decoded_binary_secret
+
+
+def get_db_credentials(region_name, env_mode='DEV'):
+
+    if env_mode.upper() == 'DEV':
+        return get_secret('Whaledb_Credentials', region_name)
+    elif env_mode.upper() == 'PROD':
+        return get_secret('Oceandb_credentials', region_name)
+    else:
+        raise Exception('ENV_MODE invalid. Please set DEV or PROD!')
