1f0cadc1b64e85475cee7f5484b536cfaa69d0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:59 +0000

    Add package bb-logger/0.3.3/bb_logger-0.3.3-py2.py3-none-any.whl

---
+++ packages/bb-logger/bb_logger-0.3.3-py2.py3-none-any.whl/bb_logger/constant.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class LOG_LEVEL(Enum):
+    info = 'INFO'
+    warn = 'WARN'
+    warning = 'WARNING'
+    error = 'ERROR'
+    critical = 'CRITICAL'
+    unkown = 'UNKOWN'
