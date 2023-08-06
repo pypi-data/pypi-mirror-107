c9a5b1a98b3ed0f5880a65672fb7294fc4653454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:00 +0000

    Add package bravaorm/0.0.17/bravaorm-0.0.17.tar.gz

---
+++ packages/bravaorm/bravaorm-0.0.17.tar.gz/bravaorm-0.0.17/test/test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import sys
+import os
+import re
+
+sys.path.append('/Users/robertoneves/Projetos/bravaorm/')
+
+import bravaorm
+from model import *
+
+import time
+import json
+
+def main(arg):
+    start = time.time()
+
+    conn = bravaorm.Connection(db_user="root", db_password="1234", db_host="127.0.0.1", db_port=3306, db_database="gofans", db_charset="utf8mb4", log_level='debug')
+
+    usuariosDirect = conn.directs_usuarios.inner("websockets").where("id_usuario!=47, id_direct=1").all
+
+    print(usuariosDirect.toJSON())
+
+    conn.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
