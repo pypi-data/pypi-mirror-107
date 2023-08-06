fc89c09f9c236b277060dacabb897bec4562d83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:32 +0000

    Add package progression-qc/0.1.1/progression_qc-0.1.1.tar.gz

---
+++ packages/progression-qc/progression_qc-0.1.1.tar.gz/progression_qc-0.1.1/progression_qc/__main__.py
@@ -0,0 +1,28 @@
+from progression_qc.progression_qc import *
+import progression_qc
+
+if __name__ == '__main__':
+    args = traiter_paramètres()
+
+    if args.version:
+        print(f"progression_qc version {progression_qc.__version__}")
+        code_retour=0
+    else :
+
+        try:
+           infos_question = charger_question(args.fichier, get_readers())
+           resultats = valider_schema_yaml_infos_question(infos_question)
+           resultats["infos_question"] = infos_question
+        except Exception as e:
+           resultats = {"erreurs": { args.fichier : e}}
+
+        code_retour = déterminer_code_retour(resultats)
+           
+        if not args.quiet:
+            if args.json:
+                afficher_résultats_json(resultats)
+            else:
+                afficher_résultats(resultats)
+                
+    exit(code_retour)    
+
