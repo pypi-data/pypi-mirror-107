5c36f4a27c6dcd911eed9a45252a944cfcf0ca68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:10 +0000

    Add package djangoldp-joboffer/2.1.1/djangoldp_joboffer-2.1.1-py3-none-any.whl

---
+++ packages/djangoldp-joboffer/djangoldp_joboffer-2.1.1-py3-none-any.whl/djangoldp_joboffer/models.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.db import models
+from djangoldp_conversation.models import Conversation
+
+from djangoldp.models import Model
+from djangoldp_skill.models import Skill
+
+job_fields = ["@id", "author", "title", "description", "creationDate", "skills", "budget", "duration", "earnBusinessProviding", "closingDate", "conversation"]
+if 'djangoldp_community' in settings.DJANGOLDP_PACKAGES:
+    job_fields += ['community']
+
+
+class JobOffer(Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='jobOffers', on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    duration = models.CharField(max_length=255, blank=True, null=True)
+    budget = models.CharField(max_length=255, blank=True, null=True)
+    earnBusinessProviding = models.BooleanField(default=False)
+    skills = models.ManyToManyField(Skill, blank=True)
+    creationDate = models.DateTimeField(auto_now_add=True)
+    closingDate = models.DateField(null=True)
+    conversation = models.ManyToManyField(Conversation, blank=True)
+
+    class Meta:
+        auto_author = 'author'
+        owner_field = 'author'
+        nested_fields = ["skills", "conversation"]
+        anonymous_perms = ["view"]
+        authenticated_perms = ["inherit", "add"]
+        owner_perms = ["inherit", "change", "delete"]
+        container_path = 'job-offers/'
+        serializer_fields = job_fields
+        rdf_type = 'hd:joboffer'
+
+    def __str__(self):
+        try:
+            return '{} -> {} ({})'.format(self.author.urlid, self.title, self.urlid)
+        except:
+            return self.urlid
