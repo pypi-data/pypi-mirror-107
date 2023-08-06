aee5262d956db65ee308f6229e791fa87a9d8eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:26 +0000

    Add package faraday-agent-parameters-types/0.1.11/faraday_agent_parameters_types-0.1.11-py2.py3-none-any.whl

---
+++ packages/faraday-agent-parameters-types/faraday_agent_parameters_types-0.1.11-py2.py3-none-any.whl/faraday_agent_parameters_types/data_types.py
@@ -0,0 +1,41 @@
+from faraday_agent_parameters_types.custom_types import (
+    faraday_integer,
+    faraday_string,
+    faraday_boolean,
+    faraday_list,
+    faraday_int_range,
+    faraday_ip,
+    faraday_float,
+    faraday_url,
+)
+
+DATA_TYPE = {
+    "integer": faraday_integer.FaradayIntegerSchema(),
+    "string": faraday_string.FaradayStringSchema(),
+    "boolean": faraday_boolean.FaradayBooleanSchema(),
+    "list": faraday_list.FaradayListSchema(),
+    "range": faraday_int_range.FaradayRangeSchema(),
+    "ip": faraday_ip.FaradayIPSchema(),
+    "float": faraday_float.FaradayFloatSchema(),
+    "url": faraday_url.FaradayUrlSchema(),
+}
+
+valid_base_types = (
+    "integer",
+    "string",
+    "boolean",
+    "list",
+    "decimal",
+)
+
+# Lookup table to match a data type to a base type
+BASE_TYPE = {
+    "integer": "integer",
+    "string": "string",
+    "boolean": "boolean",
+    "list": "list",
+    "range": "string",
+    "ip": "string",
+    "float": "decimal",
+    "url": "string",
+}
