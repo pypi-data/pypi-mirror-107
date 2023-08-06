37c84805a6c316d38456e4b13d763735a14b3348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:28 +0000

    Add package wfz-fjh/1.0.1/wfz_fjh-1.0.1.tar.gz

---
+++ packages/wfz-fjh/wfz_fjh-1.0.1.tar.gz/wfz_fjh-1.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    reshapedata FJH
+"""
+import platform
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'wfz_fjh',
+    version = '1.0.1',
+    install_requires=[
+        'requests',
+    ],
+    packages=find_packages(),
+    license = 'Apache License',
+    author = 'Reshapedata',
+    author_email = '1160230887@qq.com',
+    url = 'http://www.reshapedata.com',
+    description = 'reshape data type in py language ',
+    keywords = ['reshapedata', 'rdt','pyrdt'],
+    python_requires='>=3.6',
+)
