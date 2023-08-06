ad3e704d8d1efff9b4969bd197c88c8fc459f0a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:41 +0000

    Add package ffq/0.0.2/ffq-0.0.2-py3-none-any.whl

---
+++ packages/ffq/ffq-0.0.2-py3-none-any.whl/tests/test_ffq.py
@@ -0,0 +1,324 @@
+from unittest import mock, TestCase
+from unittest.mock import call
+
+from bs4 import BeautifulSoup
+
+import ffq.ffq as ffq
+from tests.mixins import TestMixin
+
+
+class TestFfq(TestMixin, TestCase):
+
+    def test_parse_run(self):
+        with mock.patch('ffq.ffq.cached_get') as cached_get:
+            with open(self.fastqs_path, 'r') as f:
+                cached_get.return_value = f.read()
+            with open(self.run_path, 'r') as f:
+                soup = BeautifulSoup(f.read(), 'xml')
+
+            self.assertEqual({
+                'accession':
+                    'SRR8426358',
+                'experiment':
+                    'SRX5234128',
+                'study':
+                    'SRP178136',
+                'sample':
+                    'SRS4237519',
+                'title':
+                    'Illumina HiSeq 4000 paired end sequencing; GSM3557675: old_Dropseq_1; Mus musculus; RNA-Seq',
+                'files': [{
+                    'url':
+                        'ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR842/008/SRR8426358/SRR8426358_1.fastq.gz',
+                    'md5':
+                        'be7e88cf6f6fd90f1b1170f1cb367123',
+                    'size':
+                        '5507959060'
+                }, {
+                    'url':
+                        'ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR842/008/SRR8426358/SRR8426358_2.fastq.gz',
+                    'md5':
+                        '2124da22644d876c4caa92ffd9e2402e',
+                    'size':
+                        '7194107512'
+                }]
+            }, ffq.parse_run(soup))
+
+    def test_parse_run_bam(self):
+        with mock.patch('ffq.ffq.cached_get') as cached_get:
+            with open(self.fastqs2_path, 'r') as f1, open(self.bam_path,
+                                                          'r') as f2:
+                cached_get.side_effect = [f1.read(), f2.read()]
+            with open(self.run2_path, 'r') as f:
+                soup = BeautifulSoup(f.read(), 'xml')
+
+            self.assertEqual({
+                'accession':
+                    'SRR6835844',
+                'experiment':
+                    'SRX3791763',
+                'study':
+                    'SRP131661',
+                'sample':
+                    'SRS3044236',
+                'title':
+                    'Illumina NovaSeq 6000 sequencing; GSM3040890: library 10X_P4_0; Mus musculus; RNA-Seq',
+                'files': [{
+                    'url':
+                        'ftp://ftp.sra.ebi.ac.uk/vol1/SRA653/SRA653146/bam/10X_P4_0.bam',
+                    'md5':
+                        '5355fe6a07155026085ce46631268ab1',
+                    'size':
+                        '17093057664'
+                }, {
+                    'url':
+                        'ftp://ftp.sra.ebi.ac.uk/vol1/run/SRR683/SRR6835844/10X_P4_0.bam.bai',
+                    'md5':
+                        'c9396c2596254831470a9138ae86ded7',
+                    'size':
+                        '7163216'
+                }]
+            }, ffq.parse_run(soup))
+
+    def test_parse_sample(self):
+        with open(self.sample_path, 'r') as f:
+            soup = BeautifulSoup(f.read(), 'xml')
+
+        self.assertEqual({
+            'accession': 'SRS4237519',
+            'title': 'old_Dropseq_1',
+            'organism': 'Mus musculus',
+            'attributes': {
+                'source_name': 'Whole lung',
+                'tissue': 'Whole lung',
+                'age': '24 months',
+                'number of cells': '799',
+                'ENA-SPOT-COUNT': '109256158',
+                'ENA-BASE-COUNT': '21984096610',
+                'ENA-FIRST-PUBLIC': '2019-01-11',
+                'ENA-LAST-UPDATE': '2019-01-11'
+            }
+        }, ffq.parse_sample(soup))
+
+    def test_parse_experiment(self):
+        with open(self.experiment_path, 'r') as f:
+            soup = BeautifulSoup(f.read(), 'xml')
+
+        self.assertEqual({
+            'accession':
+                'SRX5234128',
+            'title':
+                'Illumina HiSeq 4000 paired end sequencing; GSM3557675: old_Dropseq_1; Mus musculus; RNA-Seq',
+            'platform':
+                'ILLUMINA',
+            'instrument':
+                'Illumina HiSeq 4000'
+        }, ffq.parse_experiment(soup))
+
+    def test_parse_study(self):
+        with open(self.study_path, 'r') as f:
+            soup = BeautifulSoup(f.read(), 'xml')
+
+        self.assertEqual({
+            'accession':
+                'SRP178136',
+            'title':
+                'Multi-modal analysis of the aging mouse lung at cellular resolution',
+            'abstract': (
+                'A) Whole lung tissue from 24 months (n=7) and 3 months old '
+                '(n=8) mice was dissociated and single-cell mRNAseq libraries '
+                'generated with Drop-Seq. B) Bulk RNA-seq data was generated '
+                'from whole mouse lung tissue of old (n=3) and young (n=3) '
+                'samples. C) Bulk RNA-seq data was generated from flow-sorted '
+                'macrophages from old (n=7) and young (n=5) mice and '
+                'flow-sorted epithelial cells from old (n=4) and young (n=4) '
+                'mice. Overall design: Integration of bulk RNA-seq from whole '
+                'mouse lung tissue and bulk RNA-seq from flow-sorted lung '
+                'macrophages and epithelial cells was used to validate results '
+                'obtained from single cell RNA-seq of whole lung tissue.'
+            )
+        }, ffq.parse_study(soup))
+
+    def test_parse_study_with_run(self):
+        with open(self.study_with_run_path, 'r') as f:
+            soup = BeautifulSoup(f.read(), 'xml')
+            self.assertEqual({
+                'accession':
+                    'SRP096361',
+                'title':
+                    'A Molecular Census of Arcuate Hypothalamus and Median Eminence Cell Types',
+                'abstract': (
+                    'Drop-seq and single cell sequencing of mouse arcuate nucleus and '
+                    'median eminence. Please see below link for searchable cluster-based '
+                    'gene expression. Overall design: Drop-Seq was performed on six separate '
+                    'days using mice in C57BL6/J background at various ages/sex noted. '
+                    'On day 1, Chow_1 replicate was obtained. On day 2, Chow_2 replicate '
+                    'was  obtained. On day 3, Chow_3 replicate was obtained.  On day 4, 10% '
+                    'Diet_1 and HFD_1 replicates were obtained. On day 5, Fast_1 and Refed_1 '
+                    'replicates were obtained. On day 6,  Fast_2, Fast_3, Chow_4, and Chow_5 '
+                    'replicates were obtained'
+                ),
+                'runlist': [
+                    'SRR5164436', 'SRR5164437', 'SRR5164438', 'SRR5164439',
+                    'SRR5164440', 'SRR5164441', 'SRR5164442', 'SRR5164443',
+                    'SRR5164444', 'SRR5164445', 'SRR5164446'
+                ]
+            }, ffq.parse_study_with_run(soup))
+
+    def test_gse_search_json(self):
+        with open(self.gse_search_path, 'r') as f:
+            soup = BeautifulSoup(f.read(), 'html.parser')
+            self.assertEqual({
+                'accession': 'GSE93374',
+                'geo_id': "200093374"
+            }, ffq.parse_gse_search(soup))
+
+    def test_gse_summary_json(self):
+        with open(self.gse_summary_path, 'r') as f:
+            soup = BeautifulSoup(f.read(), 'html.parser')
+            self.assertEqual({'accession': 'SRP096361'},
+                             ffq.parse_gse_summary(soup))
+
+    def test_ffq_gse(self):
+        # Need to figure out how to add for loop test for adding individual runs
+        with mock.patch('ffq.ffq.get_gse_search_json') as get_gse_search_json, \
+            mock.patch('ffq.ffq.parse_gse_search') as parse_gse_search, \
+            mock.patch('ffq.ffq.geo_id_to_srps') as geo_id_to_srps, \
+            mock.patch('ffq.ffq.ffq_study') as ffq_study:
+
+            parse_gse_search.return_value = {
+                'accession': 'GSE1',
+                'geo_id': 'GEOID1'
+            }
+            geo_id_to_srps.return_value = ['SRP1']
+            ffq_study.return_value = {'accession': 'SRP1'}
+
+            self.assertEqual({
+                'accession': 'GSE1',
+                'studies': {
+                    'SRP1': {
+                        'accession': 'SRP1'
+                    }
+                }
+            }, ffq.ffq_gse('GSE1'))
+            get_gse_search_json.assert_called_once_with('GSE1')
+            geo_id_to_srps.assert_called_once_with('GEOID1')
+            ffq_study.assert_called_once_with('SRP1')
+
+    def test_ffq_run(self):
+        with mock.patch('ffq.ffq.get_xml') as get_xml,\
+            mock.patch('ffq.ffq.parse_run') as parse_run,\
+            mock.patch('ffq.ffq.parse_sample') as parse_sample,\
+            mock.patch('ffq.ffq.parse_experiment') as parse_experiment,\
+            mock.patch('ffq.ffq.parse_study') as parse_study:
+
+            run = mock.MagicMock()
+            sample = mock.MagicMock()
+            experiment = mock.MagicMock()
+            study = mock.MagicMock()
+            parse_run.return_value = run
+            parse_sample.return_value = sample
+            parse_experiment.return_value = experiment
+            parse_study.return_value = study
+
+            self.assertEqual(run, ffq.ffq_run('SRR8426358'))
+            self.assertEqual(4, get_xml.call_count)
+            get_xml.assert_has_calls([
+                call('SRR8426358'),
+                call(parse_run()['sample']),
+                call(parse_run()['experiment']),
+                call(parse_run()['study'])
+            ])
+
+    def test_ffq_study(self):
+        with mock.patch('ffq.ffq.get_xml') as get_xml,\
+            mock.patch('ffq.ffq.parse_study_with_run') as parse_study_with_run,\
+            mock.patch('ffq.ffq.ffq_run') as ffq_run:
+
+            study = {'runlist': ['run1', 'run2']}
+            run1 = mock.MagicMock()
+            run2 = mock.MagicMock()
+            parse_study_with_run.return_value = study
+            ffq_run.side_effect = [run1, run2]
+
+            self.assertEqual({'runs': {
+                'run1': run1,
+                'run2': run2
+            }}, ffq.ffq_study('SRP226764'))
+            get_xml.assert_called_once_with('SRP226764')
+            self.assertEqual(2, ffq_run.call_count)
+            ffq_run.assert_has_calls([call('run1'), call('run2')])
+
+    def test_ffq_doi(self):
+        with mock.patch('ffq.ffq.get_doi') as get_doi,\
+            mock.patch('ffq.ffq.search_ena_title') as search_ena_title,\
+            mock.patch('ffq.ffq.ffq_study') as ffq_study:
+
+            get_doi.return_value = {'title': ['title']}
+            search_ena_title.return_value = ['SRP1']
+            self.assertEqual([ffq_study.return_value], ffq.ffq_doi('doi'))
+            get_doi.assert_called_once_with('doi')
+            search_ena_title.assert_called_once_with('title')
+            ffq_study.assert_called_once_with('SRP1')
+
+    def test_ffq_doi_no_title(self):
+        with mock.patch('ffq.ffq.get_doi') as get_doi,\
+            mock.patch('ffq.ffq.search_ena_title') as search_ena_title,\
+            mock.patch('ffq.ffq.ncbi_search') as ncbi_search,\
+            mock.patch('ffq.ffq.ncbi_link') as ncbi_link,\
+            mock.patch('ffq.ffq.geo_ids_to_gses') as geo_ids_to_gses,\
+            mock.patch('ffq.ffq.ffq_gse') as ffq_gse:
+
+            get_doi.return_value = {'title': ['title']}
+            search_ena_title.return_value = []
+            ncbi_search.return_value = ['PMID1']
+            ncbi_link.return_value = ['GEOID1']
+            geo_ids_to_gses.return_value = ['GSE1']
+            self.assertEqual([ffq_gse.return_value], ffq.ffq_doi('doi'))
+            get_doi.assert_called_once_with('doi')
+            search_ena_title.assert_called_once_with('title')
+            ncbi_search.assert_called_once_with('pubmed', 'doi')
+            ncbi_link.assert_called_once_with('pubmed', 'gds', 'PMID1')
+            geo_ids_to_gses.assert_called_once_with(['GEOID1'])
+            ffq_gse.assert_called_once_with('GSE1')
+
+    def test_ffq_doi_no_geo(self):
+        with mock.patch('ffq.ffq.get_doi') as get_doi,\
+            mock.patch('ffq.ffq.search_ena_title') as search_ena_title,\
+            mock.patch('ffq.ffq.ncbi_search') as ncbi_search,\
+            mock.patch('ffq.ffq.ncbi_link') as ncbi_link,\
+            mock.patch('ffq.ffq.sra_ids_to_srrs') as sra_ids_to_srrs,\
+            mock.patch('ffq.ffq.ffq_run') as ffq_run:
+
+            get_doi.return_value = {'title': ['title']}
+            search_ena_title.return_value = []
+            ncbi_search.return_value = ['PMID1']
+            ncbi_link.side_effect = [[], ['SRA1']]
+            sra_ids_to_srrs.return_value = ['SRR1']
+            ffq_run.return_value = {
+                'accession': 'SRR1',
+                'study': {
+                    'accession': 'SRP1'
+                }
+            }
+            self.assertEqual([{
+                'accession': 'SRP1',
+                'runs': {
+                    'SRR1': {
+                        'accession': 'SRR1',
+                        'study': {
+                            'accession': 'SRP1'
+                        }
+                    }
+                }
+            }], ffq.ffq_doi('doi'))
+            get_doi.assert_called_once_with('doi')
+            search_ena_title.assert_called_once_with('title')
+            ncbi_search.assert_called_once_with('pubmed', 'doi')
+            self.assertEqual(2, ncbi_link.call_count)
+            ncbi_link.assert_has_calls([
+                call('pubmed', 'gds', 'PMID1'),
+                call('pubmed', 'sra', 'PMID1'),
+            ])
+            sra_ids_to_srrs.assert_called_once_with(['SRA1'])
+            ffq_run.assert_called_once_with('SRR1')
---
+++ packages/ffq/ffq-0.0.2-py3-none-any.whl/tests/test_utils.py
@@ -0,0 +1,230 @@
+from unittest import mock, TestCase
+from unittest.mock import call
+
+from bs4 import BeautifulSoup
+
+import ffq.utils as utils
+from ffq.config import (
+    CROSSREF_URL,
+    ENA_URL,
+    ENA_SEARCH_URL,
+    GSE_SEARCH_URL,
+    GSE_SEARCH_TERMS,
+    GSE_SUMMARY_URL,
+    GSE_SUMMARY_TERMS,
+    NCBI_FETCH_URL,
+    NCBI_LINK_URL,
+    NCBI_SEARCH_URL,
+    NCBI_SUMMARY_URL,
+)
+
+
+class TestUtils(TestCase):
+
+    def test_cached_get(self):
+        with mock.patch('ffq.utils.requests') as requests:
+            self.assertEqual(requests.get().text, utils.cached_get())
+
+    def test_get_xml(self):
+        with mock.patch('ffq.utils.cached_get') as cached_get:
+            cached_get.return_value = """
+            <TAGS>
+                <VALUE1>value1</VALUE1>
+                <VALUE2>value2</VALUE2>
+            </TAGS>
+            """
+            result = utils.get_xml('accession')
+            cached_get.assert_called_once_with(f'{ENA_URL}/accession/')
+            self.assertTrue(isinstance(result, BeautifulSoup))
+
+    def test_get_gse_search_json(self):
+        with mock.patch('ffq.utils.cached_get') as cached_get:
+            cached_get.return_value = """
+            {
+                {key1:value1},
+                {key2:value2}
+            }
+            """
+            result = utils.get_gse_search_json('accession')
+            cached_get.assert_called_once_with(
+                f'{GSE_SEARCH_URL}accession{GSE_SEARCH_TERMS}'
+            )
+            self.assertTrue(isinstance(result, BeautifulSoup))
+
+    def test_get_gse_summary_json(self):
+        with mock.patch('ffq.utils.cached_get') as cached_get:
+            cached_get.return_value = """
+            {
+                {key1:value1},
+                {key2:value2}
+            }
+            """
+            result = utils.get_gse_summary_json('accession')
+            cached_get.assert_called_once_with(
+                f'{GSE_SUMMARY_URL}accession{GSE_SUMMARY_TERMS}'
+            )
+            self.assertTrue(isinstance(result, BeautifulSoup))
+
+    def test_parse_tsv(self):
+        s = 'header1\theader2\theader3\nvalue1\tvalue2\tvalue3'
+        self.assertEqual([{
+            'header1': 'value1',
+            'header2': 'value2',
+            'header3': 'value3'
+        }], utils.parse_tsv(s))
+
+    def test_get_doi(self):
+        with mock.patch('ffq.utils.cached_get') as cached_get:
+            cached_get.return_value = """{
+                "message": {"key": "value"}
+            }"""
+            result = utils.get_doi('doi')
+            cached_get.assert_called_once_with(f'{CROSSREF_URL}/doi')
+            self.assertEqual({'key': 'value'}, result)
+
+    def test_search_ena_title(self):
+        with mock.patch('ffq.utils.requests.get') as get:
+            get.return_value.text = 'study_accession\tsecondary_study_accession\nPRJNA579178\tSRP226764\n'
+            self.assertEqual(['SRP226764'], utils.search_ena_title('title'))
+            get.assert_called_once_with(
+                ENA_SEARCH_URL,
+                params={
+                    'result': 'study',
+                    'limit': 0,
+                    'query': 'study_title="title"',
+                    'fields': 'secondary_study_accession',
+                }
+            )
+
+    def test_ncbi_summary(self):
+        with mock.patch('ffq.utils.requests.get') as get:
+            get.return_value.json.return_value = {
+                'result': {
+                    'uids': ['id1', 'id2'],
+                    'id1': 'summary1',
+                    'id2': 'summary2'
+                }
+            }
+            self.assertEqual({
+                'id1': 'summary1',
+                'id2': 'summary2',
+            }, utils.ncbi_summary('db', 'id'))
+            get.assert_called_once_with(
+                NCBI_SUMMARY_URL,
+                params={
+                    'db': 'db',
+                    'id': 'id',
+                    'retmode': 'json',
+                    'retmax': 10000,
+                }
+            )
+
+    def test_ncbi_search(self):
+        with mock.patch('ffq.utils.requests.get') as get:
+            get.return_value.json.return_value = {
+                'esearchresult': {
+                    'idlist': ['id1', 'id2']
+                }
+            }
+            self.assertEqual(['id1', 'id2'], utils.ncbi_search('db', 'term'))
+            get.assert_called_once_with(
+                NCBI_SEARCH_URL,
+                params={
+                    'db': 'db',
+                    'term': 'term',
+                    'retmode': 'json',
+                    'retmax': 100000
+                }
+            )
+
+    def test_ncbi_link(self):
+        with mock.patch('ffq.utils.requests.get') as get:
+            get.return_value.json.return_value = {
+                'linksets': [{
+                    'linksetdbs': [{
+                        'links': ['id1', 'id2']
+                    }]
+                }]
+            }
+            self.assertEqual(['id1', 'id2'],
+                             utils.ncbi_link('origin', 'destination', 'id'))
+            get.assert_called_once_with(
+                NCBI_LINK_URL,
+                params={
+                    'dbfrom': 'origin',
+                    'db': 'destination',
+                    'id': 'id',
+                    'retmode': 'json',
+                }
+            )
+
+    def test_geo_id_to_srps(self):
+        with mock.patch('ffq.utils.ncbi_summary') as ncbi_summary:
+            ncbi_summary.return_value = {
+                'id': {
+                    'extrelations': [{
+                        'relationtype': 'SRA',
+                        'targetobject': 'SRP1'
+                    }]
+                }
+            }
+            self.assertEqual(['SRP1'], utils.geo_id_to_srps('id'))
+            ncbi_summary.assert_called_once_with('gds', 'id')
+
+    def test_geo_id_to_srps_bioproject(self):
+        with mock.patch('ffq.utils.ncbi_summary') as ncbi_summary,\
+            mock.patch('ffq.utils.ncbi_search') as ncbi_search,\
+            mock.patch('ffq.utils.ncbi_link') as ncbi_link:
+            ncbi_summary.side_effect = [{
+                'id': {
+                    'bioproject': 'PRJNA1'
+                }
+            }, {
+                'SRA1': 'Study acc="SRP1"',
+                'SRA2': 'Study acc="SRP1"'
+            }]
+            ncbi_search.return_value = ['BIOPROJECT1']
+            ncbi_link.return_value = ['SRA1', 'SRA2']
+            self.assertEqual(['SRP1'], utils.geo_id_to_srps('id'))
+            self.assertEqual(2, ncbi_summary.call_count)
+            ncbi_summary.assert_has_calls([
+                call('gds', 'id'), call('sra', 'SRA1,SRA2')
+            ])
+            ncbi_search.assert_called_once_with('bioproject', 'PRJNA1[PRJA]')
+            ncbi_link.assert_called_once_with(
+                'bioproject', 'sra', 'BIOPROJECT1'
+            )
+
+    def test_geo_ids_to_gses(self):
+        with mock.patch('ffq.utils.requests.get') as get:
+            get.return_value.text = 'Series\t\tAccession: GSE1\tSeries\t\tAccession: GSE2\t'
+            self.assertEqual(['GSE1', 'GSE2'],
+                             utils.geo_ids_to_gses(['id1', 'id2']))
+            get.assert_called_once_with(
+                NCBI_FETCH_URL, params={
+                    'db': 'gds',
+                    'id': 'id1,id2',
+                }
+            )
+
+    def test_sra_ids_to_srrs(self):
+        with mock.patch('ffq.utils.requests.get') as get:
+            get.return_value.text = 'Run acc="SRR1" Run acc="SRR2"'
+            self.assertEqual(['SRR1', 'SRR2'],
+                             utils.sra_ids_to_srrs(['id1', 'id2']))
+            get.assert_called_once_with(
+                NCBI_SUMMARY_URL, params={
+                    'db': 'sra',
+                    'id': 'id1,id2',
+                }
+            )
+
+    def test_parse_run_range(self):
+        text = 'SRR10-SRR13'
+        self.assertEqual(['SRR10', 'SRR11', 'SRR12', 'SRR13'],
+                         utils.parse_run_range(text))
+
+    def test_parse_run_range_leading_zero(self):
+        text = 'SRR01-SRR05'
+        self.assertEqual(['SRR01', 'SRR02', 'SRR03', 'SRR04', 'SRR05'],
+                         utils.parse_run_range(text))
