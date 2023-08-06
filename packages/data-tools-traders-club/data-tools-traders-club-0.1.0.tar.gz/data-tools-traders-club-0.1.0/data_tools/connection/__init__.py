68e42bfc8136514e8c2b6d72a6cc86dffbf0f5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:13 +0000

    Add package data-tools-traders-club/0.1.0/data-tools-traders-club-0.1.0.tar.gz

---
+++ packages/data-tools-traders-club/data-tools-traders-club-0.1.0.tar.gz/data-tools-traders-club-0.1.0/data_tools/connection/__init__.py
@@ -0,0 +1,8 @@
+__doc__ = """
+data_tools.connection
+=================
+Package to perform queries in different databases"""
+
+# imports
+from . import mysql
+from . import mongodb
---
+++ packages/data-tools-traders-club/data-tools-traders-club-0.1.0.tar.gz/data-tools-traders-club-0.1.0/data_tools/tools/__init__.py
@@ -0,0 +1,7 @@
+__doc__ = """
+data_tools.tools
+=================
+Package with different functions to support data science projects"""
+
+# imports
+from .api import DataCleaning
