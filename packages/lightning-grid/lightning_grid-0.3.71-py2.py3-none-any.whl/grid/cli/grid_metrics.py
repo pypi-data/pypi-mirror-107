bd5e847eee558cdd86e71bb9cf865f5a388dfd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:53 +0000

    Add package lightning-grid/0.3.71/lightning_grid-0.3.71-py2.py3-none-any.whl

---
+++ packages/lightning-grid/lightning_grid-0.3.71-py2.py3-none-any.whl/grid/cli/grid_metrics.py
@@ -0,0 +1,16 @@
+import click
+
+from grid.client import Grid
+
+
+@click.command()
+@click.argument('experiment', type=str, nargs=1)
+@click.argument('metric', type=str, nargs=1)
+@click.option('--n_lines', default=10, type=int, help='Number of metrics lines (from the end) to return.')
+def metrics(experiment: str, metric: str, n_lines: int) -> None:
+    """Gets metric values from an experiment."""
+    # Instantiate client.
+    client = Grid()
+
+    # Call client.
+    client.experiment_metrics(experiment_name=experiment, metric=metric, n_lines=n_lines)
