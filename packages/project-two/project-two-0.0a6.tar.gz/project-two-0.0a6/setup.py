fded0d47590767a66a3fcf8a95d70909566d16e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:09 +0000

    Add package project-two/0.0a6/project-two-0.0a6.tar.gz

---
+++ packages/project-two/project-two-0.0a6.tar.gz/project-two-0.0a6/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='project-two',
+    version='0.0a6',
+    packages=[''],
+    url='https://google.com',
+    author='ML',
+    author_email='',
+)
