604cf1146d6bc790bf0d26e37f6208df6db40a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:03 +0000

    Add package autonlp/0.2.7/autonlp-0.2.7.tar.gz

---
+++ packages/autonlp/autonlp-0.2.7.tar.gz/autonlp-0.2.7/src/autonlp/tasks.py
@@ -0,0 +1,8 @@
+TASKS = {
+    "binary_classification": 1,
+    "multi_class_classification": 2,
+    "entity_extraction": 4,
+    "summarization": 8,
+    "single_column_regression": 10,
+    "speech_recognition": 11,
+}
