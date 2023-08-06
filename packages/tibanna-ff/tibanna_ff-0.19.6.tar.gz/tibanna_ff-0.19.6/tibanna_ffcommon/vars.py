900f8effe7f8723560ec9ff36d28ab52cc1a1435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:42 +0000

    Add package tibanna-ff/0.19.6/tibanna_ff-0.19.6.tar.gz

---
+++ packages/tibanna-ff/tibanna_ff-0.19.6.tar.gz/tibanna_ff-0.19.6/tibanna_ffcommon/vars.py
@@ -0,0 +1,19 @@
+from tibanna.vars import *
+import os
+
+S3_ENCRYPT_KEY = os.environ.get("S3_ENCRYPT_KEY", '')
+DEV_SUFFIX = 'pre'
+
+RUN_TASK_LAMBDA_NAME = 'run_task'
+CHECK_TASK_LAMBDA_NAME = 'check_task'
+UPDATE_COST_LAMBDA_NAME = 'update_cost'
+
+
+def BUCKET_NAME(env, filetype):
+    if filetype in ['FileFastq', 'FileReference']:
+        bucket_type = 'files'
+    else:
+        bucket_type = 'wfoutput'
+    if env in ['data', 'staging', 'fourfront-green', 'fourfront-blue']:
+        env = 'fourfront-webprod'
+    return 'elasticbeanstalk-%s-%s' % (env, bucket_type)
