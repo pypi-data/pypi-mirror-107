a678b4136a6460019424fc82837d702c7b54aa6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:40 +0000

    Add package django-redissearch/0.0.1/django-redissearch-0.0.1.tar.gz

---
+++ packages/django-redissearch/django-redissearch-0.0.1.tar.gz/django-redissearch-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+  name="django-redissearch",
+  packages=["django_redissearch"],
+  version="0.0.1",
+  license="mit",
+  description="lib to implement full text search ",
+  author="abtin moheban",
+  author_email="abtin@riseup.net",
+
+  keywords=[""],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+  ],
+)
