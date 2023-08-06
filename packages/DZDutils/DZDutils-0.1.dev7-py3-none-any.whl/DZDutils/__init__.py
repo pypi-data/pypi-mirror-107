3ce1ca355f39f81080e813d095214614af049deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:33 +0000

    Add package DZDutils/0.1.dev7/DZDutils-0.1.dev7-py3-none-any.whl

---
+++ packages/DZDutils/DZDutils-0.1.dev7-py3-none-any.whl/DZDutils/__init__.py
@@ -0,0 +1,3 @@
+print(
+    "Warning [ DZDUtils ]: The DZD Python Repository moved to https://git.connect.dzd-ev.de/dzdpythonmodules. Please update you dependencies"
+)
---
+++ packages/DZDutils/DZDutils-0.1.dev7-py3-none-any.whl/DZDutils/inspect/__init__.py
@@ -0,0 +1,4 @@
+print(
+    "Warning [ DZDUtils ]: The DZD Python Repository moved to https://git.connect.dzd-ev.de/dzdpythonmodules. Please update you dependencies"
+)
+from DZDutils.inspect.object2html import object2html
