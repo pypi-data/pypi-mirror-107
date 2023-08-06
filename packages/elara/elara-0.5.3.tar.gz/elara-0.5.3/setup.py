f59fd04b3e94d0737069a81e2542e07a74c140d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:22 +0000

    Add package elara/0.5.3/elara-0.5.3.tar.gz

---
+++ packages/elara/elara-0.5.3.tar.gz/elara-0.5.3/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+with open("README.rst") as f:
+    long_description = f.read()
+
+setup(
+    name="elara",
+    packages=["elara"],
+    version="0.5.3",
+    license="three-clause BSD",
+    description="Elara DB is an easy to use, lightweight NoSQL database written for python that can also be used as a fast in-memory cache for JSON-serializable data. Includes various methods and features to manipulate data structures in-memory, protect database files and export data.",
+    long_description=long_description,
+    author="Saurabh Pujari",
+    author_email="saurabhpuj99@gmail.com",
+    url="https://github.com/saurabh0719/elara",
+    keywords=[
+        "database",
+        "key-value",
+        "storage",
+        "file storage",
+        "key-value database",
+        "nosql",
+        "nosql database",
+        "cache",
+        "in-memory cache",
+        "file cache",
+    ],
+    install_requires=["cryptography", "msgpack", "safer"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+    ],
+)
