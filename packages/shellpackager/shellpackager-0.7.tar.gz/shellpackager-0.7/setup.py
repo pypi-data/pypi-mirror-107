6da61b010b1654028bde86ca4a636532eb027c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:28 +0000

    Add package shellpackager/0.7/shellpackager-0.7.tar.gz

---
+++ packages/shellpackager/shellpackager-0.7.tar.gz/shellpackager-0.7/setup.py
@@ -0,0 +1,18 @@
+#setup.py
+from setuptools import setup
+
+f= open("version", "r")
+version=f.read()
+
+setup(
+    name='shellpackager',
+    scripts=['shellpackager'],
+    version= version,
+    description = 'simple project, simple life',
+    long_description = 'cli tool to create pypi project from shell scripts',
+      install_requires=[
+          'twine',
+          'setuptools'
+      ],
+    author = 'madhavth'
+)
