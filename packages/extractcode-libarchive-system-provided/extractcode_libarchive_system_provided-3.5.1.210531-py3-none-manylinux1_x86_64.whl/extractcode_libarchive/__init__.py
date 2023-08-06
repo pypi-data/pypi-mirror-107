6b1abb2ba964358c709c105e3b3f97a7c265dd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:39 +0000

    Add package extractcode-libarchive-system-provided/3.5.1.210531/extractcode_libarchive_system_provided-3.5.1.210531-py3-none-manylinux1_x86_64.whl

---
+++ packages/extractcode-libarchive-system-provided/extractcode_libarchive_system_provided-3.5.1.210531-py3-none-manylinux1_x86_64.whl/extractcode_libarchive/__init__.py
@@ -0,0 +1,54 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# ScanCode is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/scancode-toolkit for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+import platform
+from os import environ
+from os import path
+
+from plugincode.location_provider import LocationProviderPlugin
+
+
+class LibarchivePaths(LocationProviderPlugin):
+    def get_locations(self):
+        """
+        Return a mapping of {location key: location} providing the installation
+        locations of the libarchive shared library as installed on various Linux
+        distros or on FreeBSD.
+        """
+        lib_archive = environ.get('EXTRACTCODE_LIBARCHIVE_PATH')
+        if not lib_archive:
+            system_arch = platform.machine()
+            mainstream_system = platform.system().lower()
+            if mainstream_system == 'linux':
+                distribution = platform.linux_distribution()[0].lower()
+                debian_based_distro = ['ubuntu', 'mint', 'debian']
+                rpm_based_distro = ['fedora', 'redhat']
+
+                if distribution in debian_based_distro:
+                    lib_dir = '/usr/lib/'+system_arch+'-linux-gnu'
+
+                elif distribution in rpm_based_distro:
+                    lib_dir = '/usr/lib64'
+
+                else:
+                    raise Exception('Unsupported system: {}'.format(distribution))
+
+            elif mainstream_system == 'freebsd':
+                if path.isdir('/usr/local/'):
+                    lib_dir = '/usr/local/lib'
+                else:
+                    lib_dir = '/usr/lib'
+            lib_archive = path.join(lib_dir, 'libarchive.so')
+        else:
+            lib_dir = path.dirname(lib_archive)
+
+        locations = {
+            'extractcode.libarchive.dll': lib_archive,
+        }
+        return locations
