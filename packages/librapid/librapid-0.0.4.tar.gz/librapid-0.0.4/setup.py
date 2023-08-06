056de267220de2b42f4debfc93bf9647fd570ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:38 +0000

    Add package librapid/0.0.4/librapid-0.0.4.tar.gz

---
+++ packages/librapid/librapid-0.0.4.tar.gz/librapid-0.0.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+# Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11 import get_cmake_dir
+
+import os
+import sys
+
+# Load the version number from VERSION.hpp
+version_file = open("VERSION.hpp", "r")
+__version__ = version_file.readlines()[1].split()[2].replace("\"", "")
+version_file.close()
+
+ext_modules = [
+    Pybind11Extension("librapid",
+        ["librapid/pybind_librapid.cpp"],
+        define_macros = [('LIBRAPID_BUILD', 1)],
+        include_dirs=[os.getcwd()]
+        )
+]
+
+setup(
+    name="librapid",
+    version=__version__,
+    author="Toby Davis",
+    author_email="pencilcaseman@gmail.com",
+    url="https://github.com/Pencilcaseman/librapid",
+    description="A fast math and neural network library for Python and C++",
+    long_description="",
+    ext_modules=ext_modules,
+    extras_require={"test": "pytest"},
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False
+)
