103a6f86288e7d13d9ba7b5c64bc951acce8add1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:13 +0000

    Add package symbl/1.0.3/symbl-1.0.3-py3-none-any.whl

---
+++ packages/symbl/symbl-1.0.3-py3-none-any.whl/symbl/init.py
@@ -0,0 +1,54 @@
+from symbl_rest import AuthenticationApi, ConversationsApi, JobsApi, ApiClient, Configuration
+
+from symbl.AsyncApi import AsyncApi
+
+class init():
+
+    '''
+        It will return an object of ApiClient with a prefilled token. 
+    '''
+
+    def __init__(self, app_id=None, app_secret=None, refresh_token=False):
+
+        if app_id is None or len(app_id) == 0:
+            raise ValueError('app_id is required')
+        
+        if app_secret is None or len(app_secret) == 0:
+            raise ValueError('app_secret is required')
+
+        configuration = Configuration()
+
+        body={
+            'type': 'application', 
+            'appId': app_id, 
+            'appSecret': app_secret
+            }
+
+        authenticationResponse = AuthenticationApi().generate_token(body)
+        configuration.api_key['x-api-key'] = authenticationResponse.access_token
+
+        self.api_client = ApiClient(configuration)
+        self.async_api = AsyncApi(self.api_client)
+        self.conversations_api = ConversationsApi(self.api_client)
+    
+
+    def getAsyncApiClient(self):
+
+        if self.api_client is None:
+            raise ValueError('Please initialize sdk with correct app_id and app_secret.')
+        
+        return AsyncApi(self.api_client)
+
+    def getConversationsApiClient(self):
+        
+        if self.api_client is None:
+            raise ValueError('Please initialize sdk with correct app_id and app_secret.')
+        
+        return ConversationsApi(self.api_client)
+
+    def getJobsApiClient(self):
+
+        if self.api_client is None:
+            raise 'Please initialize sdk with correct app_id and app_secret.'
+        
+        return JobsApi(self.api_client)
