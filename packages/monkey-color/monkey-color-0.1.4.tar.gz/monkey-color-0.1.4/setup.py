0a1b08e2d9ec132d573b2d4d78b267af5f5a4b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:51 +0000

    Add package monkey-color/0.1.4/monkey-color-0.1.4.tar.gz

---
+++ packages/monkey-color/monkey-color-0.1.4.tar.gz/monkey-color-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='monkey-color',
+    version='0.1.4',
+    author='monkey',
+    author_email='a102009102009@gmail.com',
+    url='https://github.com/a3510377/color',
+    description=u'有色文字',
+    packages=['color'],
+    install_requires=[],
+    readme="README.md",
+)
