3b77c482ad95e398aeb21ca5ab49393995382fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:34 +0000

    Add package fastformers/0.0.50/fastformers-0.0.50-py3-none-any.whl

---
+++ packages/fastformers/fastformers-0.0.50-py3-none-any.whl/fastformers/multiprocess_api/configs.py
@@ -0,0 +1,17 @@
+class Config:
+    host = '127.0.0.1'
+
+    end_delimiter = b'DELIM'
+
+    socket_timeout = 20
+
+    server_message_size = 8192
+
+    client_message_size = 65536
+
+    error_prefix = b'ERROR'
+
+    message_id_len = 24
+
+    request_signal = b'\x00'
+    request_cancelled = b'\x01'
