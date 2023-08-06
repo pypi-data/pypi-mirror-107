71f79ecd2d979cd1e2fb696f418161413ae881d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:26 +0000

    Add package ndistbdist/0.3/ndistbdist-0.3.tar.gz

---
+++ packages/ndistbdist/ndistbdist-0.3.tar.gz/ndistbdist-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ndistbdist',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=['ndistbdist'],
+      author=['Garvit Kataria'],
+      zip_safe=False)
