f9163c6d67cf4ae0af6aa23461f3575fc22fddef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:55 +0000

    Add package orka-vector-api/0.0.6/orka_vector_api-0.0.6.tar.gz

---
+++ packages/orka-vector-api/orka_vector_api-0.0.6.tar.gz/orka_vector_api-0.0.6/orka_vector_api/enums/status.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Status(Enum):
+    INIT = 'INIT'
+    RUNNING = 'RUNNING'
+    CREATED = 'CREATED'
+    ERROR = 'ERROR'
+    TIMEOUT = 'TIMEOUT'
+    BBOX_TOO_BIG = 'BBOX_TOO_BIG'
+    BBOX_INVALID = 'BBOX_INVALID'
+    NO_THREADS_AVAILABLE = 'NO_THREADS_AVAILABLE'
