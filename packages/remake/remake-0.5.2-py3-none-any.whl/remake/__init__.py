b7d44d9ac41232bc6577b804cbc455244775e8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:30 +0000

    Add package remake/0.5.2/remake-0.5.2-py3-none-any.whl

---
+++ packages/remake/remake-0.5.2-py3-none-any.whl/remake/__init__.py
@@ -0,0 +1,7 @@
+from remake.task import Task
+from remake.task_control import TaskControl
+from remake.flags import RemakeOn
+from remake.util import fmtp
+from remake.task_rule import TaskRule
+from remake.task_query_set import TaskQuerySet
+from remake.remake_base import Remake
