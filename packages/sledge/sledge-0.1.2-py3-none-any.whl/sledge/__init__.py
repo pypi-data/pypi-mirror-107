3c3e5c7b44bf9752f541d00dc4fd9b624cafa9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:44 +0000

    Add package sledge/0.1.2/sledge-0.1.2-py3-none-any.whl

---
+++ packages/sledge/sledge-0.1.2-py3-none-any.whl/sledge/__init__.py
@@ -0,0 +1,10 @@
+"""
+Python package `sledge` semantic evaluation of clustering results.
+"""
+
+def sledge_descriptors(X, labels):
+    pass
+
+
+def sledge_score(X, labels, aggregation='harmonic', return_descriptors=False):
+    pass
