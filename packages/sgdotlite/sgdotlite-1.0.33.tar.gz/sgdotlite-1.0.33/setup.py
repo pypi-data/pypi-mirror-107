b9ee939addc959fe7797083bcea8d000cced6172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:43 +0000

    Add package sgdotlite/1.0.33/sgdotlite-1.0.33.tar.gz

---
+++ packages/sgdotlite/sgdotlite-1.0.33.tar.gz/sgdotlite-1.0.33/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="sgdotlite",
+    version='1.0.33',
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(
+        exclude=["tests", "sgdotlite/data", "sgdotlite/examples"]),
+    install_requires=Path("requirements.txt").read_text().split("\n")[:-1]
+)
