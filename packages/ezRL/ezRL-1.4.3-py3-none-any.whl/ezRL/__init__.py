6255361ec50ae68a5a70f25b3fc3eff3b35c20d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:27 +0000

    Add package ezRL/1.4.3/ezRL-1.4.3-py3-none-any.whl

---
+++ packages/ezRL/ezRL-1.4.3-py3-none-any.whl/ezRL/__init__.py
@@ -0,0 +1,13 @@
+
+import sys
+from sout import sout
+# 共通部品 [utils.py]
+from .parts.utils import show_img, TileArt
+# デバッグ用のプレーヤー [debug_players.py]
+from .parts.debug_players import RandomPlayer, HumanPlayer
+# Deep Q Network AI [DQN_Agent]
+from .parts.DQN_Agent import DQN_Agent
+# Catcherゲーム [catcher_game]
+from .parts import catcher_game
+# # 紅白ゲーム [rw_game]
+# from .parts import rw_game
