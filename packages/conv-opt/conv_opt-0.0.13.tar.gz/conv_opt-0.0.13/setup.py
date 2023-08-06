d56253efde1495264850042472e0dd7c65c7b16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:50 +0000

    Add package conv-opt/0.0.13/conv_opt-0.0.13.tar.gz

---
+++ packages/conv-opt/conv_opt-0.0.13.tar.gz/conv_opt-0.0.13/setup.py
@@ -0,0 +1,50 @@
+import re
+import setuptools
+import subprocess
+import sys
+try:
+    result = subprocess.run(
+        [sys.executable, "-m", "pip", "show", "pkg_utils"],
+        check=True, capture_output=True)
+    match = re.search(r'\nVersion: (.*?)\n', result.stdout.decode(), re.DOTALL)
+    assert match and tuple(match.group(1).split('.')) >= ('0', '0', '5')
+except (subprocess.CalledProcessError, AssertionError):
+    subprocess.run(
+        [sys.executable, "-m", "pip", "install", "-U", "pkg_utils"],
+        check=True)
+import os
+import pkg_utils
+
+name = 'conv_opt'
+dirname = os.path.dirname(__file__)
+
+# get package metadata
+md = pkg_utils.get_package_metadata(dirname, name)
+
+# install package
+setuptools.setup(
+    name=name,
+    version=md.version,
+    description="High-level Python package for solving linear and quadratic optimization problems using multiple solvers",
+    long_description=md.long_description,
+    url="https://github.com/KarrLab/" + name,
+    download_url='https://github.com/KarrLab/' + name,
+    author="Karr Lab",
+    author_email="info@karrlab.org",
+    license="MIT",
+    keywords='convex optimization, linear programming, quadratic programming',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    install_requires=md.install_requires,
+    extras_require=md.extras_require,
+    tests_require=md.tests_require,
+    dependency_links=md.dependency_links,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+)
