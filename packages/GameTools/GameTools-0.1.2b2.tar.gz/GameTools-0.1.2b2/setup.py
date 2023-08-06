7f0b722d852179bfb56510475264d6ce5d3d0327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:33 +0000

    Add package GameTools/0.1.2b2/GameTools-0.1.2b2.tar.gz

---
+++ packages/GameTools/GameTools-0.1.2b2.tar.gz/GameTools-0.1.2b2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GameTools",
+    version="0.1.2b2",
+    author="Spidertyler2005",
+    author_email="spidertyler1122@gmail.com",
+    description="A bunch of tools for making games in Pygame and other libraries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spidertyler2005/GameTools",
+    project_urls={
+        "Bug Tracker": "https://github.com/spidertyler2005/GameTools/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src/"},
+    packages=["GameTools"],
+    install_requires=["pygame"],
+    python_requires=">=3.8",
+)
