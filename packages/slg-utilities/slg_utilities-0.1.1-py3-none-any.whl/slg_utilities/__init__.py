8f458fee17a515c86e21d03cd0fc75853fafd236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:30 +0000

    Add package slg-utilities/0.1.1/slg_utilities-0.1.1-py3-none-any.whl

---
+++ packages/slg-utilities/slg_utilities-0.1.1-py3-none-any.whl/slg_utilities/__init__.py
@@ -0,0 +1,7 @@
+
+from .email_handler import EmailHandler
+from .file_operations import FileOperations
+from .scraping_engine import Scraper
+from .mongo_engine import MongoEngine
+from .image_processor import ImageHandler, ImgProcessorJS
+from .table_helpers import create_table_from_objects
