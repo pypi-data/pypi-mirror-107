444cfdaf7601058796e616af56960ecfabf1d6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:26 +0000

    Add package eth-ape/0.1.0a8/eth_ape-0.1.0a8-py3-none-any.whl

---
+++ packages/eth-ape/eth_ape-0.1.0a8-py3-none-any.whl/ape/api/compiler.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import List, Set
+
+from ape.types import ContractType
+
+
+class CompilerAPI(ABC):
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        ...
+
+    @abstractmethod
+    def get_versions(self, all_paths: List[Path]) -> Set[str]:
+        ...
+
+    @abstractmethod
+    def compile(self, contract_filepaths: List[Path]) -> List[ContractType]:
+        """
+        Compile the source given `pkg_manifest`.
+        All compiler plugins must implement this function.
+        """
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} {self.name}>"
+
+    def __str__(self) -> str:
+        return f"{self.name}>"
