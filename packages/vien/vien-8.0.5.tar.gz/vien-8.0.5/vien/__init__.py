1090ba4321ef8aa42ad9529c6199f293a9e2acd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:13 +0000

    Add package vien/8.0.5/vien-8.0.5.tar.gz

---
+++ packages/vien/vien-8.0.5.tar.gz/vien-8.0.5/vien/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2021 Artëm IG <github.com/rtmigo>
+# SPDX-License-Identifier: BSD-3-Clause
+
+from ._constants import __version__, __license__, __copyright__
+from ._common import is_posix
+from ._main import main_entry_point
+
