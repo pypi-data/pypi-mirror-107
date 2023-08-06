170dcdca66e5ecfdafff5b991026d8dc1cae33a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:33 +0000

    Add package Covid-Data-Interface/1/Covid%20Data%20Interface-1.tar.gz

---
+++ packages/Covid-Data-Interface/Covid%20Data%20Interface-1.tar.gz/Covid Data Interface-1/States/__init__.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+"""
+Save state COVID data to pandas dataframe
+"""
+url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
+df = pd.read_csv(url)
+df.head(5)
+
+
+def getDF(): # return whole US States dataframe
+	return (df) 
+
+def getLength(): # return number of rows in DF
+	return len(df) 
+
+def getState(rowIndex): # return the state name for a given index
+	return df.iloc[rowIndex]["state"]	
+
+def getDate(rowIndex): # return the data of a certain entry
+		return df.iloc[rowIndex]["date"]
+
+def getDeathsToDate(rowIndex): #return total deaths in a certain state up to the desired date
+	return df.iloc[rowIndex]["deaths"]
+
+def getDailyDeathCount(rowIndex): # get number of deaths that occured on a specific day 
+	state = getState(rowIndex) 
+	for i in range(rowIndex - 1,-1,-1):
+		if getState(i) == state:
+			return getDeathsToDate(rowIndex) - getDeathsToDate(i)
+	return getDeathsToDate(rowIndex) 	
+
+def getCasesToDate(rowIndex): #return total cases in a certain state up to the desired date
+		return df.iloc[rowIndex]["cases"]
+
+def getDailyCaseCount(rowIndex): # get number of cases that occured on a specific day 
+	state = getState(rowIndex) 
+	for i in range(rowIndex - 1,-1,-1):
+		if getState(i) == state:
+			return getCasesToDate(rowIndex) - getCasesToDate(i)
+	return getCasesToDate(rowIndex)
+
+def getDateIndex(date):
+	return df[df["date"]==date].index.values
+
+def getStateIndex(state):
+	return df[df["state"]==state].index.values 
---
+++ packages/Covid-Data-Interface/Covid%20Data%20Interface-1.tar.gz/Covid Data Interface-1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Covid Data Interface',
+      version='1',
+      description='Covid Data Interface for learning Python',
+      url='http://github.com/storborg/funniest',
+      author='Neil Aylor and Devon Schwartz',
+      author_email='neilaylor@hotmail.com',
+      license='MIT',
+      packages=['States'],
+      zip_safe=False)
