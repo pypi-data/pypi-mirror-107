86c9aa37416aefea6d1f247127434b102ede75a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:59 +0000

    Add package easy-functions/2.18.60/easy_functions-2.18.60.tar.gz

---
+++ packages/easy-functions/easy_functions-2.18.60.tar.gz/easy_functions-2.18.60/setup.py
@@ -0,0 +1,20 @@
+﻿from setuptools import setup
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+    name = 'easy_functions',
+    version = '2.18.60',
+    py_modules = ['easy_functions'],
+    author = 'Jerry0940',
+    author_email = 'j13816180940@139.com',
+    license = 'MIT',
+    description = '一个简单小巧但实用的模块',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    classifiers = [
+        "Programming Language :: Python :: 3"
+    ]
+)
