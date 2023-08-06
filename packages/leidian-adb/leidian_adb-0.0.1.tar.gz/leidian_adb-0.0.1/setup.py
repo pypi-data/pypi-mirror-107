ac00898bdadb594f30b879ce83051da14d286323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:40 +0000

    Add package leidian-adb/0.0.1/leidian_adb-0.0.1.tar.gz

---
+++ packages/leidian-adb/leidian_adb-0.0.1.tar.gz/leidian_adb-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='leidian_adb',
+      version='0.0.1',
+      description='雷电模块',
+      url='https://github.com/qxt514657/LdAdb.py.git',
+      author='1Q847',
+      author_email='976671673@qq.com',
+      license='MIT',
+      packages=['leidian'])
