ba32efe085b00d17c4bd76b7433facff34ce4db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:55 +0000

    Add package hijri-converter/2.1.2/hijri-converter-2.1.2.tar.gz

---
+++ packages/hijri-converter/hijri-converter-2.1.2.tar.gz/hijri-converter-2.1.2/setup.py
@@ -0,0 +1,10 @@
+"""Setup.py entry point for package
+
+Configuration is handled by setuptools through setup.cfg
+https://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
