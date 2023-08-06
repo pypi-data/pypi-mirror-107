6f435b4f7b0ab7bcd5ebb23db85f86772c4a58f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:19 +0000

    Add package movado/0.3.7/Movado-0.3.7-py3-none-any.whl

---
+++ packages/movado/Movado-0.3.7-py3-none-any.whl/movado/estimator.py
@@ -0,0 +1,18 @@
+import abc
+from abc import abstractmethod
+from typing import List
+from pathlib import Path
+
+
+class Estimator(abc.ABC):
+    @abstractmethod
+    def predict(self, X: List[float]) -> List[float]:
+        pass
+
+    @abstractmethod
+    def train(self, X: List[float], y: List[float]) -> None:
+        pass
+
+    @abstractmethod
+    def get_error(self) -> float:
+        pass
