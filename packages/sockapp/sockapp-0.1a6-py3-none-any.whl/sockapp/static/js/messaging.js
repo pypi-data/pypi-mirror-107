e8438e3f5b76f5daacdbd2ce032d099cf5820cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:18 +0000

    Add package sockapp/0.1a6/sockapp-0.1a6-py3-none-any.whl

---
+++ packages/sockapp/sockapp-0.1a6-py3-none-any.whl/sockapp/static/js/messaging.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $("#send-message-nav").addClass("selected-nav-button");
+    $("#send-file-nav").removeClass("selected-nav-button");
+
+    $("#send").click(function() {
+        var recv_ip = $("#recv_ip").val();
+        var send_msg = $("#send_msg").val();
+
+        if(recv_ip && send_msg) {
+            $.ajax({
+                url: "/send-message",
+                type: "post",
+                dataType: "json",
+                data: {"recv_ip": recv_ip, "send_msg": send_msg},
+                success: function(result) {
+                    Swal.fire({
+                        icon: result.icon,
+                        title: result.title,
+                        text: result.status,
+                    });
+                }
+            });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "Both receiver IP and message are required!",
+            });
+        }
+    });
+
+    $("#receive").click(function() {
+        $("#receive").addClass("selected-button");
+        $.ajax({
+            url: "/receive-message",
+            type: "post",
+            dataType: "json",
+            success: function(result) {
+                Swal.fire({
+                    icon: result.icon,
+                    title: result.title,
+                    text: result.status,
+                });
+                $("#receive").removeClass("selected-button");
+                $("#recvd_msg").html(result.message);
+            }
+        });
+    });
+});
