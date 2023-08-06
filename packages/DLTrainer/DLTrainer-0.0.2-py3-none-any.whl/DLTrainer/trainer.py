07a3a8c174602c3b7722b1dcd357b17aa265a180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:04 +0000

    Add package DLTrainer/0.0.2/DLTrainer-0.0.2-py3-none-any.whl

---
+++ packages/DLTrainer/DLTrainer-0.0.2-py3-none-any.whl/DLTrainer/trainer.py
@@ -0,0 +1,32 @@
+from trainer_utils import train_setup
+
+
+class Trainer:
+    def __init__(self):
+        # set up training config
+
+        # set up dataset and vocab
+
+        # initialize the model
+
+        # create optimizer
+
+
+        pass
+
+    def _save_model(self):
+        pass
+
+    def train(self):
+        pass
+
+    def eval(self):
+        pass
+
+
+def main():
+    args, logger = train_setup()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/DLTrainer/DLTrainer-0.0.2-py3-none-any.whl/DLTrainer/trainer_utils.py
@@ -0,0 +1,159 @@
+import argparse
+import logging
+import numpy as np
+import os
+import random
+
+import torch
+
+
+def set_seed(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
+
+
+def parse_input_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--data_dir", type=str, default="data",
+                        help="The path to the data folder")
+    parser.add_argument("--save_dir", type=str, default="save",
+                        help="The path where the model and log will be saved.")
+    parser.add_argument("--run_name", type=str, default='baseline',
+                        help="Name of your model training run.")
+    parser.add_argument('--recompute-features', action='store_true',
+                        help="Whether to recompute dataset features if they exist")
+    parser.add_argument('--pretrained-checkpoint', type=str, default='',
+                        help="Directory of pretrained model")
+
+    parser.add_argument("--do_train", action="store_true",
+                        help="Whether to run training")
+    parser.add_argument("--do_eval", action="store_true",
+                        help="Whether to run eval")
+    parser.add_argument("--no_eval_during_training", action="store_false",
+                        help="Whether to skip evaluation during training")
+
+    parser.add_argument("--num_train_epochs", default=1.0, type=float,
+                        help="Total number of training epochs to perform.")
+    parser.add_argument("--train_batch_size", default=16, type=int,
+                        help="Batch size per GPU/CPU for training.")
+    parser.add_argument("--eval_batch_size", default=32, type=int,
+                        help="Batch size per GPU/CPU for evaluation.")
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
+                        help="Number of updates steps to accumulate before performing a backward/update pass.")
+    parser.add_argument("--learning_rate", default=5e-5, type=float,
+                        help="The initial learning rate for Adam.")
+    parser.add_argument("--weight_decay", default=0.0, type=float,
+                        help="Weight deay if we apply some.")
+    parser.add_argument("--adam_epsilon", default=1e-8, type=float,
+                        help="Epsilon for Adam optimizer.")
+    parser.add_argument("--max_grad_norm", default=1.0, type=float,
+                        help="Max gradient norm.")
+    parser.add_argument("--max_steps", default=-1, type=int,
+                        help="If > 0: set total number of training steps to perform. Override num_train_epochs.")
+    parser.add_argument("--warmup_steps", default=0, type=int,
+                        help="Linear warmup over warmup_steps.")
+
+    parser.add_argument('--logging_steps', type=int, default=1000,
+                        help="Log every X updates steps.")
+    parser.add_argument('--eval_steps', type=int, default=5000,
+                        help="Save checkpoint every X updates steps.")
+    parser.add_argument("--no_cuda", action='store_true',
+                        help="Avoid using CUDA when available")
+    parser.add_argument('--overwrite_output_dir', action='store_true',
+                        help="Overwrite the content of the output directory")
+    parser.add_argument('--overwrite_cache', action='store_true',
+                        help="Overwrite the cached training and evaluation sets")
+    parser.add_argument('--seed', type=int, default=42,
+                        help="random seed for initialization")
+
+    parser.add_argument('--fp16', action='store_true',
+                        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit")
+    parser.add_argument('--fp16_opt_level', type=str, default='O1',
+                        help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
+                             "See details at https://nvidia.github.io/apex/amp.html")
+    parser.add_argument("--local_rank", type=int, default=-1,
+                        help="For distributed training: local_rank")
+    parser.add_argument("--node_index", type=int, default=-1,
+                        help="node index if multi-node running")
+    parser.add_argument("--num_gpus", type=int, default=-1,
+                        help="num of gpus")
+
+    parser.add_argument('--log_file', type=str, default='')
+    parser.add_argument('--tensorboard_dir', type=str)
+
+    parser.add_argument('--tiny_train', action='store_true',
+                        help="trains model on small training dataset")
+
+    args = parser.parse_args()
+    return args
+
+
+def train_setup(args):
+    args = parse_input_arguments()
+    pool = None
+
+    if os.path.exists(args.output_dir) and os.listdir(
+            args.output_dir) and args.do_train and not args.overwrite_output_dir:
+        raise ValueError(
+            "Output directory ({}) already exists and is not empty. Use --overwrite_output_dir to overcome.".format(
+                args.output_dir))
+
+    # logger
+    logger = logging.getLogger(__name__)
+    logger.info("local_rank: %d, node_index: %d, num_gpus: %d" %
+                (args.local_rank, args.node_index, args.num_gpus))
+    # Setup CUDA, GPU & distributed training
+    if args.local_rank == -1 or args.no_cuda:
+        device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+        args.n_gpu = torch.cuda.device_count()
+    else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+        torch.cuda.set_device(args.local_rank)
+        device = torch.device("cuda", args.local_rank)
+        torch.distributed.init_process_group(backend='nccl')
+        args.local_rank += args.node_index * args.num_gpus
+        args.n_gpu = 1
+    args.device = device
+    # args.batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
+
+    # Setup logging
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                        datefmt='%m/%d/%Y %H:%M:%S',
+                        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+    logger.warning("""Process rank: %s, device: %s, n_gpu: %s, 
+                      distributed training: %s, 16-bits training: %s, world size: %s""",
+                   args.local_rank, device, args.n_gpu, bool(args.local_rank != -1), args.fp16,
+                   torch.distributed.get_world_size() if args.local_rank != -1 else 1)
+
+    # FileHandler
+    fh = logging.FileHandler(args.log_file)
+    logger.addHandler(fh)
+
+    # Set seed
+    set_seed(args)
+
+    # Barrier to make sure only the first process in distributed training download model & vocab
+    if args.local_rank not in [-1, 0]:
+        torch.distributed.barrier()
+
+    args.start_epoch = 0
+    args.start_step = 0
+    checkpoint_last = os.path.join(args.output_dir, 'checkpoint-last')
+    if args.do_train and os.path.exists(checkpoint_last) and os.listdir(checkpoint_last):
+        args.pretrain_dir = os.path.join(checkpoint_last)
+        args.config_name = os.path.join(checkpoint_last, 'config.json')
+        idx_file = os.path.join(checkpoint_last, 'idx_file.txt')
+        with open(idx_file, encoding='utf-8') as idxf:
+            args.start_epoch = int(idxf.readlines()[0].strip()) + 1
+
+        step_file = os.path.join(checkpoint_last, 'step_file.txt')
+        if os.path.exists(step_file):
+            with open(step_file, encoding='utf-8') as stepf:
+                args.start_step = int(stepf.readlines()[0].strip())
+
+        logger.info("reload model from {}, resume from {} steps".format(checkpoint_last, args.start_step))
+
+    return args, logger
