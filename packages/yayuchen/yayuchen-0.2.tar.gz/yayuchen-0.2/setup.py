7ac983ce02eedc5189c5a7f125635abb8c403e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:37 +0000

    Add package yayuchen/0.2/yayuchen-0.2.tar.gz

---
+++ packages/yayuchen/yayuchen-0.2.tar.gz/yayuchen-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='yayuchen',
+      author ='Ya Yu Chen',
+      author_email ='a725761@gmail.com',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['yayuchen'],
+      zip_safe=False)
