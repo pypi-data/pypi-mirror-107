295222c290173a883dfb289614cc88820b25a9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:53 +0000

    Add package pySecDec/1.4.5a1.dev6/pySecDec-1.4.5a1.dev6-py3-none-manylinux2010_x86_64.whl

---
+++ packages/pySecDec/pySecDec-1.4.5a1.dev6-py3-none-manylinux2010_x86_64.whl/pySecDec/loop_integral/__init__.py
@@ -0,0 +1,38 @@
+"""
+Loop Integral
+-------------
+
+This module defines routines to Feynman parametrize a
+loop integral and build a c++ package that numerically
+integrates over the sector decomposed integrand.
+
+Feynman Parametrization
+~~~~~~~~~~~~~~~~~~~~~~~
+
+Routines to Feynman parametrize a loop integral.
+
+.. autoclass:: pySecDec.loop_integral.LoopIntegral
+.. autoclass:: pySecDec.loop_integral.LoopIntegralFromGraph
+.. autoclass:: pySecDec.loop_integral.LoopIntegralFromPropagators
+
+Loop Package
+~~~~~~~~~~~~
+
+This module contains the function that generates a c++ package.
+
+.. autofunction:: pySecDec.loop_integral.loop_package
+
+Drawing Feynman Diagrams
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+Use the following function to draw Feynman diagrams.
+
+.. autofunction:: pySecDec.loop_integral.draw.plot_diagram
+
+"""
+
+from .common import LoopIntegral
+from .from_graph import LoopIntegralFromGraph
+from .from_propagators import LoopIntegralFromPropagators
+from .loop_package import loop_package
+from .draw import plot_diagram
