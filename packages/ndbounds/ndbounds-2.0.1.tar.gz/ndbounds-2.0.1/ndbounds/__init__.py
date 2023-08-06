aca0888831b94450e33dc8a5b1064de1c0c78d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:30 +0000

    Add package ndbounds/2.0.1/ndbounds-2.0.1.tar.gz

---
+++ packages/ndbounds/ndbounds-2.0.1.tar.gz/ndbounds-2.0.1/ndbounds/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2021 Matthias Nadig
+
+from ._api import as_bounds_se
+from ._api import as_bounds_cw
+from ._api import raise_if_not_ndbounds
+
+__version__ = '2.0.1'
+__author__ = 'Matthias Nadig'
