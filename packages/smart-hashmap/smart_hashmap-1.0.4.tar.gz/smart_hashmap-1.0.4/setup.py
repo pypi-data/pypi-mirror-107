dc81a549d5ce675393275964a855df0e34602941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:38 +0000

    Add package smart-hashmap/1.0.4/smart_hashmap-1.0.4.tar.gz

---
+++ packages/smart-hashmap/smart_hashmap-1.0.4.tar.gz/smart_hashmap-1.0.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import setuptools
+
+module_info = {
+    "name": "smart_hashmap",
+    "version": "1.0.4",
+    "description": "Wrapper for hash map based storage systems",
+    "author": "Yury Sokov aka. Yurzs",
+    "author_email": "yurzs+smart_hashmap@icloud.com",
+    "packages": setuptools.find_packages(exclude=("tests", "tests.*", "*.tests")),
+    "license": "MIT",
+    "keywords": ["cache", "indexes"],
+    "url": "https://github.com/yurzs/smart_hashmap",
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+}
+
+with open("README.rst") as long_description_file:
+    module_info["long_description"] = long_description_file.read()
+
+setuptools.setup(**module_info)
