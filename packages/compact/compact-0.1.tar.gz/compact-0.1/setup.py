d78e1c3f2b1ccf7e9099f0104b93cb290cee31d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:23 +0000

    Add package compact/0.1/compact-0.1.tar.gz

---
+++ packages/compact/compact-0.1.tar.gz/compact-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='compact',
+      version='0.1',
+      description='PHP compact for Python',
+      url='http://github.com/robinvandernoord/compact',
+      author='Robin van der Noord',
+      author_email='robinvandernoord@gmail.com',
+      license='MIT',
+      packages=['compact'],
+      zip_safe=False)
