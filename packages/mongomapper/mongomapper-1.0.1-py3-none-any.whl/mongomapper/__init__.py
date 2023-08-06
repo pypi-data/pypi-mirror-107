89528b095ca2fc04ae8168eac601f3e2d9f83e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:30 +0000

    Add package mongomapper/1.0.1/mongomapper-1.0.1-py3-none-any.whl

---
+++ packages/mongomapper/mongomapper-1.0.1-py3-none-any.whl/mongomapper/__init__.py
@@ -0,0 +1,24 @@
+from bson.codec_options import TypeRegistry, CodecOptions
+
+from .utils import classproperty
+
+from .base import BaseSchema, db
+from .reference import Reference
+from .query import Query
+
+from .encoders import DateEncoder
+
+type_registry = TypeRegistry([DateEncoder()])
+
+class Schema(BaseSchema):
+  @classproperty
+  def collection(cls):
+    return db.get_collection(cls.__collection_name__, codec_options=CodecOptions(type_registry=type_registry))
+
+  @property
+  def reference(self):
+    return Reference(self.__class__)(self._id)
+  
+  @classproperty
+  def query(self):
+    return Query(model=self)
