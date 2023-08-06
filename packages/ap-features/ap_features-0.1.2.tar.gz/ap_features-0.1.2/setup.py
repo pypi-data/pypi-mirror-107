557eb7e34b59865b38d8dd6d8149a3d65153e583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:51 +0000

    Add package ap-features/0.1.2/ap_features-0.1.2.tar.gz

---
+++ packages/ap-features/ap_features-0.1.2.tar.gz/ap_features-0.1.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+import sys
+
+from setuptools import Extension, setup
+
+extra_compile_args = ["-fopenmp"]
+if sys.platform == "darwin":
+    extra_compile_args = ["-Xclang", "-fopenmp"]
+
+
+requirements = ["numpy", "numba", "tqdm"]
+
+setup(
+    ext_modules=[
+        Extension(
+            "ap_features.cost_terms",
+            ["src/cost_terms.c"],
+            extra_compile_args=extra_compile_args,
+            extra_link_args=["-lgomp"],
+        )
+    ],
+    version="0.1.2",
+)
