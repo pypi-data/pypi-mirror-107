041be1e64285472ba7954f81abb31379c4aa6c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:22 +0000

    Add package compoundinterest/0.0.1/compoundinterest-0.0.1.tar.gz

---
+++ packages/compoundinterest/compoundinterest-0.0.1.tar.gz/compoundinterest-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def readme() -> str:
+    with open(r'README.md') as f:
+        README = f.read()
+    return README
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Intended Audience :: Financial and Insurance Industry',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='compoundinterest',
+    version='0.0.1',
+    description='Module of compound interest factors which includes single payment, uniform payment series, and arithmetic gradient calculations',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/zaafirrahman/compoundinterest',
+    author='Zaafirrahman',
+    author_email='zaafir123rahman@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['compoundinterest', 'engineeringeconomics'],
+    packages=find_packages(),
+    install_requires=['']
+)
