cf4cfac92fa4c9492b4d761561f08b009a93853a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:52 +0000

    Add package reviewer2/0.9.20/reviewer2-0.9.20.tar.gz

---
+++ packages/reviewer2/reviewer2-0.9.20.tar.gz/reviewer2-0.9.20/reviewer2/__main__.py
@@ -0,0 +1,41 @@
+from flask import Flask, Response, send_from_directory
+from flask_cors import CORS
+import os
+import pkg_resources
+
+from reviewer2 import args
+from reviewer2.main_list import main_list_handler
+from reviewer2.data_page import data_page_handler
+from reviewer2.save import save_form_handler
+
+
+def send_file(path):
+    print(f"Sending {args.directory} {path}")
+    if path.startswith("favicon"):
+        return Response(pkg_resources.resource_stream('reviewer2', 'icons/favicon.png'), mimetype='image/png')
+
+    return send_from_directory(args.directory, path, as_attachment=True)
+
+
+app = Flask(__name__)
+CORS(app)
+
+app.url_map.strict_slashes = False
+
+app.add_url_rule('/', view_func=main_list_handler, methods=['GET'])
+app.add_url_rule('/page', view_func=data_page_handler, methods=['POST', 'GET'])
+app.add_url_rule('/save', view_func=save_form_handler, methods=['POST'])
+app.add_url_rule('/<path:path>', view_func=send_file, methods=['GET'])
+
+os.environ["WERKZEUG_RUN_MAIN"] = "true"
+
+host = os.environ.get('HOST', args.host)
+port = int(os.environ.get('PORT', args.port))
+if args.verbose:
+    print(f"Connecting to {host}:{port}")
+
+app.run(
+    debug=args.dev_mode,
+    host=host,
+    port=port)
+
