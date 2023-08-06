449797f91409c50413f70171bf9b666bf1dc8d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:40 +0000

    Add package chs/3.0.0/chs-3.0.0.tar.gz

---
+++ packages/chs/chs-3.0.0.tar.gz/chs-3.0.0/README.md
@@ -0,0 +1,75 @@
+# ♗ chs
+
+> Play chess against the Stockfish engine in your terminal.
+
+<img src="https://travis-ci.org/nickzuber/chs.svg?branch=master" /> <img src="https://img.shields.io/badge/project-active-brightgreen.svg" /> <img src="https://img.shields.io/badge/status-stable-brightgreen.svg" /> <img src="https://img.shields.io/pypi/dm/chs.svg?color=yellow" /> <img src="https://img.shields.io/pypi/format/chs.svg" /> <img src="https://img.shields.io/badge/state-released-brightgreen.svg" /> <img src="https://img.shields.io/badge/license-MIT%20Licence-blue.svg" />
+
+<img src="https://user-images.githubusercontent.com/10540865/119232802-80c34700-baf4-11eb-9fed-af558575ae4e.png" />
+
+### Table of Contents
+
+- [Installation](#installation)
+  - [Pip](#pip)
+  - [Arch Linux](#arch-linux)
+- [Usage](#usage)
+  - [How to start playing](#how-to-start-playing)
+  - [How to play](#how-to-play)
+- [License](#license)
+
+## Installation
+
+#### Pip
+
+This package is available via PyPi.
+
+```
+$ python3 -m pip install chs
+```
+
+#### Arch Linux
+
+There is a [chs-git](https://aur.archlinux.org/packages/chs-git/) package in the Arch User Repository, which you can install with an AUR helper:
+
+```
+$ yay -S chs-git || paru -S chs-git
+```
+
+## Usage
+
+To play against the default level 1 (easiest) version of the Stockfish engine, just run `chs` command.
+
+### How to start playing
+
+```
+$ chs
+```
+
+To see all possible options, use the help command.
+
+```
+$ chs help
+```
+
+To play as the black pieces, use the `--play-black` flag.
+
+```
+$ chs --play-black
+```
+
+You can also specify the level of the engine if you want to tweak the difficulty.
+
+```
+$ chs --level=8
+```
+
+### How to play
+
+There are a few things you can do while playing:
+
+- Make moves using valid algebraic notation (e.g. `Nf3`, `e4`, etc.).
+- Take back your last move by playing `back` instead of a valid move.
+- Get a hint from the engine by playing `hint` instead of a valid move.
+
+## License
+
+This software is free to use under the MIT License. See [this reference](https://opensource.org/licenses/MIT) for license text and copyright information.
---
+++ packages/chs/chs-3.0.0.tar.gz/chs-3.0.0/chs/__main__.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import chess
+import sys
+import os
+from chs.utils.core import Colors, Levels
+from chs.client.runner import Client
+
+
+def get_version():
+  file_path = os.path.dirname(os.path.abspath(__file__))
+  version_file = open(os.path.join(file_path, 'VERSION'), 'r')
+  return version_file.read().rstrip()
+
+def is_help_command(arg):
+  return (
+    arg == 'help' or
+    arg == '--h' or
+    arg == '--help' or
+    arg == '-help' or
+    arg == '-h'
+  )
+
+def is_version_command(arg):
+  return (
+    arg == 'version' or
+    arg == '--v' or
+    arg == '--version' or
+    arg == '-version' or
+    arg == '-v'
+  )
+
+def get_level_from_args(args):
+  lvl = [arg for arg in args if "--level" in arg]
+  if lvl:
+    try:
+      num = int(lvl[0].split('=')[1])
+      return Levels.level_of_int(num)
+    except:
+      return Levels.ONE
+  return Levels.ONE
+
+def get_player_from_args(args):
+  player = [arg for arg in args if "--play-black" in arg]
+  if player:
+    return chess.BLACK
+  return chess.WHITE
+
+def main():
+  if len(sys.argv) > 1 and is_help_command(sys.argv[1]):
+    print('Usage: chs [COMMAND] [FLAGS]\n')
+    print('Valid values for [COMMAND]')
+    print('  help         Print all the possible usage information')
+    print('  version      Print the current version')
+    print('\nValid values for [FLAGS]')
+    print('  --play-black   Play the game with the black pieces')
+    print('  --level=[LVL]  Start a game with the given difficulty level')
+    print('\nValid values for [LVL]')
+    print('  1     The least difficult setting')
+    print('  2..7  Increasing difficulty')
+    print('  8     The most difficult setting')
+    print('')
+    print('How to play: Your move: [MOVE]\n')
+    print('Valid values for [MOVE]:')
+    print('        Make moves using valid alegraic notation (e.g. Nf3, e4, etc.)')
+    print('  back  Take back your last move')
+    print('  hint  Get a hint from the engine')
+    print('')
+  elif len(sys.argv) > 1 and is_version_command(sys.argv[1]):
+    print('Running chs {}v{}{}\n'.format(Colors.BOLD, get_version(), Colors.RESET))
+  else:
+    try:
+      level = get_level_from_args(sys.argv)
+      play_as = get_player_from_args(sys.argv)
+    except:
+      level = Levels.ONE
+      play_as = chess.WHITE
+    client = Client(level, play_as)
+    client.run()
+
+def run():
+  try:
+    main()
+  except Exception as exception:
+    print(Colors.RED + '\nUncaught error "{}", exiting the app.\n'.format(
+      exception.__class__.__name__
+    ))
