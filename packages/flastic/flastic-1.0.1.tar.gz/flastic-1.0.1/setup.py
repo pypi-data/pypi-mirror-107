6b78cd8a90bc5e9da113686ecf634b4050e7c51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:36 +0000

    Add package flastic/1.0.1/flastic-1.0.1.tar.gz

---
+++ packages/flastic/flastic-1.0.1.tar.gz/flastic-1.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="flastic",
+    version="1.0.1",
+    license="MIT",
+    author="raravel",
+    scripts=["./flastic"],
+    author_email="youn@tree-some.dev",
+    description="development static server",
+    packages=setuptools.find_packages(),
+)
