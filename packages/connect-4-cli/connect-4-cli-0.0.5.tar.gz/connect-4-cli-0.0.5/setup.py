30d7a4843937d1987c8d95eb17f25adfe3a29514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:10 +0000

    Add package connect-4-cli/0.0.5/connect-4-cli-0.0.5.tar.gz

---
+++ packages/connect-4-cli/connect-4-cli-0.0.5.tar.gz/connect-4-cli-0.0.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="connect-4-cli",
+    version="0.0.5",
+    author="Germán Mené Santa Olaya",
+    author_email="german.mene@gmail.com",
+    description="A CLI implementation of the classic connect 4 game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gmso/connect-4-cli",
+    packages = setuptools.find_packages(include = ['src*',]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Games/Entertainment :: Board Games"
+    ],
+    entry_points={
+        "console_scripts": [
+            "connect-4-cli = src.App:main"
+        ]
+    },
+
+)
---
+++ packages/connect-4-cli/connect-4-cli-0.0.5.tar.gz/connect-4-cli-0.0.5/src/PlayerInput.py
@@ -0,0 +1,63 @@
+from src.constants import ValidUserInput as UserInput
+try:
+    from pynput import keyboard
+except ImportError:
+    class pyinput():
+        keyboard = 0
+
+
+class PlayerInput():
+    """Detect and process player input from keyboard"""
+
+    def __init__(self) -> None:
+        """Constructor."""
+        self._reset_object_vars()
+
+    def wait_for_user_input(self, key_overwrite=None):
+        """Wait for user input. Return tuple with key and flag
+        to make other modules update"""
+
+        self._reset_object_vars()
+
+        # Collect events until released
+        if key_overwrite:
+            self._on_press(key_overwrite, key_overwrite)
+        else:
+            with keyboard.Listener(on_press=self._on_press) as listener:  # pragma: no cover
+                listener.join()  # pragma: no cover
+
+        return (self.key_press_detected, self.key_pressed)
+
+    def _on_press(self, key, key_override=None):
+        """Process user input and update object variables accordingly."""
+
+        self.key_press_detected = True
+
+        if key_override:
+            self.key_pressed = key_override
+            return False
+
+        if "char" in dir(key):  # pragma: no cover
+            # handle alphanumeric keys
+            switcher = {
+                "r": UserInput.KEY_R,
+                "q": UserInput.KEY_Q
+            }
+            self.key_pressed = switcher.get(key.char, UserInput.OTHER)
+        else:  # pragma: no cover
+            # handle special keys
+            switcher = {
+                keyboard.Key.left: UserInput.ARROW_LEFT,
+                keyboard.Key.right: UserInput.ARROW_RIGHT,
+                keyboard.Key.enter: UserInput.ENTER,
+            }
+            self.key_pressed = switcher.get(key, UserInput.OTHER)
+
+        # stop listener and exit loop
+        return False
+
+    def _reset_object_vars(self):
+        """Reset object variables back to default values"""
+
+        self.key_press_detected = False
+        self.key_pressed = UserInput.OTHER
