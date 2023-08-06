e92edf1e04599278ec0e3dad18f9e7bed55d31d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:09 +0000

    Add package f1z1-common/0.1.8/f1z1_common-0.1.8-py3-none-any.whl

---
+++ packages/f1z1-common/f1z1_common-0.1.8-py3-none-any.whl/f1z1_common/src/conf/api.py
@@ -0,0 +1,24 @@
+# @Time     : 2021/5/30
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from .reader import IniReader, JsonReader
+from .builder import ReaderBuilder
+
+from ..utils import Encoding, PathTypes
+
+
+def create_ini_reader(file: PathTypes,
+                      encoding: Encoding = None) -> IniReader:
+    builder = ReaderBuilder(file, encoding)
+    builder.add_ext_name(".int")
+    return builder.build()
+
+
+def create_json_reader(file: PathTypes,
+                       encoding: Encoding = Encoding.UTF_8) -> JsonReader:
+    builder = ReaderBuilder(file, encoding)
+    builder.set_reader(JsonReader)
+    builder.add_ext_name(".json")
+    return builder.build()
