7daa288d60e254fa2a6654a5cc9bb8c39885f414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:17 +0000

    Add package django-browsable-router/0.0.5/django-browsable-router-0.0.5.tar.gz

---
+++ packages/django-browsable-router/django-browsable-router-0.0.5.tar.gz/django-browsable-router-0.0.5/browsable_router/__init__.py
@@ -0,0 +1,8 @@
+"""Brousable router and views for django rest framework."""
+
+from .router import APIRouter
+from .views import BaseAPIView
+from .mixins import GetMixin, PostMixin, PutMixin, PatchMixin, DeleteMixin
+from .meta import APIMetadata, SerializerAsOutputMetadata
+from .serializers import BaseAPISerializer
+from .permissions import BlockSchemaAccess
