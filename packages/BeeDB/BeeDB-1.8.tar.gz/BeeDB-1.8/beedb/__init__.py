2c89a1c29ca61fc06cfe048cbcea788b989958c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:40 +0000

    Add package BeeDB/1.8/BeeDB-1.8.tar.gz

---
+++ packages/BeeDB/BeeDB-1.8.tar.gz/BeeDB-1.8/beedb/__init__.py
@@ -0,0 +1,108 @@
+r"""BeeDB it is an easier way to make a simple database it my first package and 
+i very proude of it, if you wanna contact me there are discord : PyFox#3890  SORRY FOR MY BAD GRAMMER"""
+
+
+import os
+import json
+
+
+def set(obj:str,data,file="db.json"):
+    if file in os.listdir(os.getcwd()):
+        pass
+    else:
+        with open(os.path.join(os.getcwd(), file), 'w') as fp:
+            fp.write("{}")
+
+
+    #set
+    with open(file,'r') as database:
+        db = json.load(database)
+
+    db[obj] = {}
+    db[obj] = data
+
+    with open(file,'w') as database:
+        json.dump(db, database,indent=4)
+
+
+def add(obj:str,data,file="db.json"):
+    with open(file,'r') as database:
+         db = json.load(database)
+
+    obj = obj.replace('.',' ')
+    obj = obj.split()
+
+    if obj[0] == "file":
+        db[obj[1]] = data
+
+    if obj[0] in db:
+        db[obj[0]][obj[1]] = data
+
+    with open(file,'w') as database:
+        json.dump(db, database,indent=4)
+
+
+def push(obj:str,data,file="db.json"):
+    with open(file,'r') as database:
+        db = json.load(database)
+
+    obj = obj.replace('.',' ')
+    obj = obj.split()
+
+    if obj[0] in db:
+        if obj[1] not in db[obj[0]]:
+            db[obj[0]][obj[1]] = [data]
+        elif obj[1] in db[obj[0]]:
+            db[obj[0]][obj[1]].append(data)
+
+    with open(file,'w') as database:
+        json.dump(db, database,indent=4)
+
+
+def get(obj:str,file="db.json"):
+    with open(file,'r') as database:
+        db = json.load(database)
+
+
+    obj = obj.replace('.',' ')
+    obj = obj.split()
+
+    if len(obj) == 1:
+        return db[obj[0]]
+
+    if obj[0] in db:
+        return db[obj[0]][obj[1]]
+
+
+def delete(obj:str,file="db.json"):
+    with open(file,'r') as database:
+        db = json.load(database)
+
+
+    obj = obj.replace('.',' ')
+    obj = obj.split()
+
+    if len(obj) == 1:
+        del db[obj[0]]
+
+    if obj[0] in db:
+        del db[obj[0]][obj[1]]
+
+
+    with open(file,'w') as database:
+        json.dump(db, database,indent=4)
+
+
+def all(obj:str,file="db.json"):
+    with open(file,'r') as database:
+        db = json.load(database)
+
+
+    return db[obj]
+
+
+def allfile(file="db.json"):
+    with open(file,'r') as database:
+        db = json.load(database)
+
+    return db
