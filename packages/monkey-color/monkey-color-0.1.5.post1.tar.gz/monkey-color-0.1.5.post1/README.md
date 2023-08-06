509f6d0b408dfff3d651ef6f4b2b6a15ba22408c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:53 +0000

    Add package monkey-color/0.1.5.post1/monkey-color-0.1.5.post1.tar.gz

---
+++ packages/monkey-color/monkey-color-0.1.5.post1.tar.gz/monkey-color-0.1.5.post1/README.md
@@ -0,0 +1,24 @@
+# monkey_color
+
+[![PyPI version](https://badge.fury.io/py/monkey_color.svg)](https://badge.fury.io/py/monkey_color)
+
+## 安裝說明
+
+使用`pip`或其他 PyPi 軟件包進行安裝
+
+```
+pip install monkey_color
+```
+
+## monkey_color 的打印功能
+
+您可以試試：
+
+```python
+from color import color
+
+
+print(color("[rgb(255, 0, 0)]hello[yellow]red[ansi(blue)]blue[ansi(56)]test[/]"))
+```
+
+![Hello World](https://raw.githubusercontent.com/a3510377/color/main/github/print.png)
---
+++ packages/monkey-color/monkey-color-0.1.5.post1.tar.gz/monkey-color-0.1.5.post1/color/get.py
@@ -0,0 +1,58 @@
+
+# Author: 猴子
+# Date: 2021-05-23 15:09:28
+# LastEditTime: 2021-05-24 12:15:42
+# FilePath: /code/color/get.py
+__all__ = [
+    "background",
+    "txt",
+]
+
+
+class main(object):
+    def __init__(self):
+        for name in dir(self):
+            if not name.startswith('_'):
+                setattr(self, name, f"\033[{getattr(self, name)}m")
+
+
+class txt(main):
+    # 黑色
+    black = 30
+    # 红色
+    red = 31
+    # 绿色
+    green = 32
+    # 黃色
+    yellow = 33
+    # 蓝色
+    blue = 34
+    # 紫红色
+    Fuchsia = 35
+    # 青蓝色
+    cyan = 36
+    # 白色
+    white = 37
+
+
+class background(main):
+    # 黑色
+    black = 40
+    # 红色
+    red = 41
+    # 绿色
+    green = 42
+    # 黃色
+    yellow = 43
+    # 蓝色
+    blue = 44
+    # 紫红色
+    Fuchsia = 45
+    # 青蓝色
+    cyan = 46
+    # 白色
+    white = 47
+
+
+txt = txt()
+background = background()
