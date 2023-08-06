5feba1dc74b188ccaa39771ad59c0e8e1cbeba9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:45 +0000

    Add package spiffyplots/0.6.1/spiffyplots-0.6.1.tar.gz

---
+++ packages/spiffyplots/spiffyplots-0.6.1.tar.gz/spiffyplots-0.6.1/spiffyplots/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for SpiffyPlots."""
+
+__author__ = """Julian Rossbroich"""
+__email__ = "julian.rossbroich@fmi.ch"
+__version__ = "0.6.1"
+
+from .multipanel import MultiPanel
+from .lineplots import multiline
