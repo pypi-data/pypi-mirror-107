1ed456b905db89740c00239c06e5809886277214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:18 +0000

    Add package BenchTrack/1.0.1/BenchTrack-1.0.1-py3-none-any.whl

---
+++ packages/BenchTrack/BenchTrack-1.0.1-py3-none-any.whl/src/site/pelicanconf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+# Completed
+
+
+AUTHOR = 'BenchTrack'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# à paramétrer
+THEME= 'theme'
+LOAD_CONTENT_CACHE = False
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+"""
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+"""
+
+THEME_STATIC_DIR = 'static'
+
+DISPLAY_PAGES_ON_MENU = True
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
