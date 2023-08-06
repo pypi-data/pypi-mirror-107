60d03f0d1ba9016fc07a160b65def7c1b417084b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:12 +0000

    Add package lpulive/0.2.0/lpulive-0.2.0.tar.gz

---
+++ packages/lpulive/lpulive-0.2.0.tar.gz/lpulive-0.2.0/lpulive/__main__.py
@@ -0,0 +1,7 @@
+from lpulive import User
+
+a = User(registration_no="11917210", password="Yeahhia1236#")
+# print(a.get_messages(1992013))
+# print(a.get_chat_members(1992013))
+# print(a.get_messages(chat_id=199201, msg_thread=False))
+print(a.get_message_threads(1991112, "36nje1uvhl.1ewef614sdfsdf925747748"))
