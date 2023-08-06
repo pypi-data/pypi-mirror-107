ccf2a2c65d9af0ce2ca6172eb64df03342b9078e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:09 +0000

    Add package digsig/1.215.1/digsig-1.215.1-py3-none-any.whl

---
+++ packages/digsig/digsig-1.215.1-py3-none-any.whl/digsig/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import *
+from .auto import *
+from .auto import PrivateKey as PrivateKeyAuto
+from .auto import PublicKey as PublicKeyAuto
+
+__version__ = '1.215.1'
