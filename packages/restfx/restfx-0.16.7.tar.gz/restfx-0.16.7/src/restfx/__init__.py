335949637b638492df58f448408fbf5879195365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:20 +0000

    Add package restfx/0.16.7/restfx-0.16.7.tar.gz

---
+++ packages/restfx/restfx-0.16.7.tar.gz/restfx-0.16.7/src/restfx/__init__.py
@@ -0,0 +1,14 @@
+from .app import App
+from .routes.decorator import route
+from .routes.parameter_interface import IParam
+from .routes.validator import Validator
+
+# 设置一个简短的别名 for Validator
+val = Validator
+
+__all__ = [
+    'App',
+    'route',
+    'val',
+    'IParam'
+]
