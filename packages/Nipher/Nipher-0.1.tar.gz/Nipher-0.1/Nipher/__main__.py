35ac75a5b4604681bfe8b9e5ceb287174e0f8407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:59 +0000

    Add package Nipher/0.1/Nipher-0.1.tar.gz

---
+++ packages/Nipher/Nipher-0.1.tar.gz/Nipher-0.1/Nipher/__main__.py
@@ -0,0 +1,10 @@
+from Nipher.nipher import Nipher
+
+
+def main():
+    a = Nipher()
+    a.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/Nipher/Nipher-0.1.tar.gz/Nipher-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='Nipher',
+    packages=['Nipher'],
+    version='0.1',
+    license='MIT',
+    description='IP lookup tool to avoid manual check.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Pedro Huang',
+    author_email='justhuangpedro@gmail.com',
+    entry_points={
+            'console_scripts': [
+                'nipher = Nipher.__main__:main'
+            ]
+        },
+    url='https://github.com/Slyrack/Nipher',
+    download_url='https://github.com/Slyrack/Nipher/archive/refs/tags/v0.1.tar.gz',
+    keywords=['Nipher', 'IP Lookup', 'IP Geolocation'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
