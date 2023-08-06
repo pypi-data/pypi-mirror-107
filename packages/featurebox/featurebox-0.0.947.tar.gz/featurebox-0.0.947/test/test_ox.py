491b340078a42913634ec776d538ffdf5ee70085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:58 +0000

    Add package featurebox/0.0.947/featurebox-0.0.947.tar.gz

---
+++ packages/featurebox/featurebox-0.0.947.tar.gz/featurebox-0.0.947/test/test_ox.py
@@ -0,0 +1,31 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+from pymatgen.core import Structure
+
+from featurebox.data.check_data import CheckElements
+from featurebox.featurizers.atom.mapper import AtomTableMap, AtomJsonMap, AtomPymatgenPropMap, BinaryMap
+from featurebox.featurizers.base_transform import ConverterCat
+from featurebox.featurizers.envir.environment import BaseDesGet
+
+
+class TestGraph(unittest.TestCase):
+    def setUp(self) -> None:
+        self.st = Structure.from_file("674718.cif")
+        self.st2 = [Structure.from_file("674718.cif"),Structure.from_file("674718.cif")]
+
+    def test_convert(self):
+        atm = AtomTableMap()
+        a = atm.convert(self.st)
+        print(a)
+
+    def test_convert2(self):
+        atm = AtomJsonMap(search_tp="name")
+        re = atm.convert(self.st)
+
+    def test_tra(self):
+        atm = AtomJsonMap(search_tp="name")
+        re = atm.transform(self.st2)
+
+
