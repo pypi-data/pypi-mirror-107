4b4b66a1e7ada47e0a31d34de1dd6f00c7348fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:34 +0000

    Add package automaton-linux/0.1/automaton-linux-0.1.tar.gz

---
+++ packages/automaton-linux/automaton-linux-0.1.tar.gz/automaton-linux-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+    name='automaton-linux',
+    packages=["automaton"],
+    version='0.1',
+    license='MIT',
+    description='An automation library for Linux using Uinput.',
+    author='Abdul Muiz Iqbal',
+    author_email='parkermuiz0@gmail.com',
+    url='https://github.com/Abdul-Muiz-Iqbal/Automaton/',
+    download_url='https://github.com/Abdul-Muiz-Iqbal/Automaton/archive/refs/tags/0.3.tar.gz',
+    keywords=['automation', 'linux', 'uinput'],
+    install_requires=[
+        'evdev',
+        'pgi',
+        'zenipy'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
