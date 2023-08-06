eb2fdf84194d2a843540acee5c2d97af6cc734a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:53 +0000

    Add package vulcan-athena/0.2.1/vulcan_athena-0.2.1.tar.gz

---
+++ packages/vulcan-athena/vulcan_athena-0.2.1.tar.gz/vulcan_athena-0.2.1/vulcan_athena/weather_reporting/setup.py
@@ -0,0 +1,9 @@
+"""
+This file is for setting up the global variables used throughout the project
+Customized folder paths and details to be initialized here
+"""
+
+api_key = "0c42f7f6b53b244c78a418f4f181282a"
+melbourne_id = "2158177"
+geelong_id = "2165798"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
