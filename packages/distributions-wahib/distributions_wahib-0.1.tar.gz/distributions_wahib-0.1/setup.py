a1e20bb7b41263420228f3a73573bfc3e1272b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:18 +0000

    Add package distributions-wahib/0.1/distributions_wahib-0.1.tar.gz

---
+++ packages/distributions-wahib/distributions_wahib-0.1.tar.gz/distributions_wahib-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_wahib',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
