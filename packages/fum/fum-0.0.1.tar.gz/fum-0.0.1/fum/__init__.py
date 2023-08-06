8b110eeb10bdbf69b08b47023d967074416667f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:09 +0000

    Add package fum/0.0.1/fum-0.0.1.tar.gz

---
+++ packages/fum/fum-0.0.1.tar.gz/fum-0.0.1/fum/__init__.py
@@ -0,0 +1,1 @@
+print("hello from fum package ty good bye")
---
+++ packages/fum/fum-0.0.1.tar.gz/fum-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fum',
+    version='0.0.1',
+    packages=['fum'],
+    url='',
+    license='',
+    author='madhavth',
+    author_email='',
+    description='something not meant to be'
+)
