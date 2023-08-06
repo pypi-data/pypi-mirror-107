643fef951b4c73ecef3b8fdbec4d36c6112d0135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:06 +0000

    Add package pymarko/0.1.0/pymarko-0.1.0.tar.gz

---
+++ packages/pymarko/pymarko-0.1.0.tar.gz/pymarko-0.1.0/pymarko/__init__.py
@@ -0,0 +1,15 @@
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2018 Kevin Walchko
+# see LICENSE for full details
+##############################################
+
+try:
+    from importlib.metadata import version # type: ignore
+except ImportError:
+    from importlib_metadata import version # type: ignore
+
+
+__author__ = "Kevin Walchko"
+__license__ = "MIT"
+__version__ = version("pymarko")
