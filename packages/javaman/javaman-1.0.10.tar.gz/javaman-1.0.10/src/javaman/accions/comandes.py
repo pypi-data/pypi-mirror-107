3b31a43ae70e03f9ae59bac177a322ccc8c3a9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:54 +0000

    Add package javaman/1.0.10/javaman-1.0.10.tar.gz

---
+++ packages/javaman/javaman-1.0.10.tar.gz/javaman-1.0.10/src/javaman/accions/comandes.py
@@ -0,0 +1,46 @@
+from javaman.connexio import JManCon
+
+
+class Comandes:
+    _url_portal_web_comanda = '/portals_web/{numcom}/comanda'
+    _url_pendent_servir = '/comandes/{id_com}/pendent_servir'
+    _url_bloqueig = '/comandes/{id_com}/magatzem_retingut'
+    _url_comencar = '/comandes/{id_com}/iniciar'
+    _url_cancelar = '/comandes/{id_com}/cancelar'
+    _url_albarans = '/comandes/{id_com}/albarans/iniciats'
+
+    def __init__(self, con: JManCon):
+        self._con = con
+
+    def crear(self, portal_web_token: str, comanda: dict):
+        tmp_url = Comandes._url_portal_web_comanda.format(numcom=portal_web_token)
+        req = self._con.post(url=tmp_url, data=comanda)
+        return req.json()
+
+    def bloquejar(self, comanda_id: int):
+        req = self._con.post(url=Comandes._url_bloqueig.format(id_com=comanda_id), data=None)
+        return req.json()
+
+    def pendent_servir(self, comanda_id: int):
+        req = self._con.post(url=Comandes._url_pendent_servir.format(id_com=comanda_id), data=None)
+        return req.json()
+
+    def comencar(self, comanda_id: int):
+        req = self._con.post(url=Comandes._url_comencar.format(id_com=comanda_id), data=None)
+        return req.json()
+
+    def albarans(self, comanda_id: int):
+        req = self._con.get(url=Comandes._url_albarans.format(id_com=comanda_id))
+        return req.json()
+
+    def assignar_usuari(self, comanda_id: int, emplat_id: int):
+        req = self._con.post(url='/comandes/' + str(comanda_id) + '/assignar', data={'id': emplat_id})
+        return req.json()
+
+    def pausar(self, comanda_id: int):
+        req = self._con.post(url='/comandes/' + str(comanda_id) + '/pausar', data=None)
+        return req.json()
+
+    def cancelar_albara(self, albara_id: int):
+        req = self._con.get(url=Comandes._url_cancelar.format(id_com=albara_id))
+        return req.json()
---
+++ packages/javaman/javaman-1.0.10.tar.gz/javaman-1.0.10/src/javaman/accions/portals_web.py
@@ -0,0 +1,13 @@
+from javaman.connexio import JManCon
+
+
+class PortalsWeb:
+    _url_portal = '/portals_web/{num_portal}'
+
+    def __init__(self, con: JManCon):
+        self._con = con
+
+    def get_portal_web(self, portal_web_id: int):
+        tmp_url = PortalsWeb._url_portal.format(num_portal=portal_web_id)
+        req = self._con.get(url=tmp_url)
+        return req.json()
